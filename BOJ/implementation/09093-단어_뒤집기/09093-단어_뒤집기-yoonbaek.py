50c69c63a27018f020842c5dfc85e52a1d06a212
YoonBaek <royal729@naver.com>
2021-07-27 00:49:23 +0900

    code: Solve BOJ 9093 단어뒤집기 (YoonBaek)

--- BOJ/implementation/09093-단어_뒤집기/09093-단어_뒤집기-yoonbaek.py
+++ BOJ/implementation/09093-단어_뒤집기/09093-단어_뒤집기-yoonbaek.py
@@ -3,8 +3,10 @@ import sys
 
 def solve() :
     words = sys.stdin.readline()
+    # 전체 뒤집기
     reverse = words[::-1]
     
+    # 공백별로 나누고 순서 바꾸기
     print(" ".join(reverse.split()[::-1]))
 
 if __name__ == '__main__':

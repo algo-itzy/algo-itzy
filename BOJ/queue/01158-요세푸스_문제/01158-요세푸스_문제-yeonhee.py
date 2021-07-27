6080cae17fb5527f6e014d09c4d1c9de525d0e59
hing9u <yeonheechae425@gmail.com>
2021-07-27 23:27:25 +0900

    fix: Delete unnecessary sentence

--- BOJ/queue/01158-요세푸스_문제/01158-요세푸스_문제-yeonhee.py
+++ BOJ/queue/01158-요세푸스_문제/01158-요세푸스_문제-yeonhee.py
@@ -10,8 +10,7 @@ idx = 0
 while numbers:  # numbers에 값이 존재하지 않을때 까지
     # 인덱스를 이용해 numbers에서 pop(idx) 하게되면 다음 인덱스는 K-1을 더한 값이 됩니다.
     idx += K-1 
-    if idx >= len(numbers):  # numbers의 길이를 초과한 경우 len(numbers)로 나눈 나머지
-        idx %= len(numbers)
+    idx %= len(numbers)  # numbers의 길이를 초과한 경우 len(numbers)로 나눈 나머지
     josephus.append(numbers.pop(idx))
     
 print(f"<{', '.join(map(str, josephus))}>")

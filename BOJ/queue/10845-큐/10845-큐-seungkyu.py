5a3044de7656e36279aadc8295c69a371b64e597
ed-kyu <seungkyukim.dev@gmail.com>
2021-07-27 23:07:52 +0900

    code: Solve boj 10845 큐 (seungkyu)

---
+++ BOJ/queue/10845-큐/10845-큐-seungkyu.py
@@ -0,0 +1,43 @@
+from collections import deque
+import sys
+
+# 입출력 속도 sys.stdin.readline > raw_input() > input()
+n = int(sys.stdin.readline())
+# len함수는 O(n) 시간 복잡도를 가질 것 같아 size 변수를 만들어봄
+size = 0
+# 큐  => 파이썬 deque 활용
+# deque.append(), deque.popleft()
+queue = deque()
+for _ in range(n):
+    # input() 사용시 시간 초과됨
+    command = sys.stdin.readline().split()
+    # push 일때만 command[1]이 존재
+    if command[0] == 'push':
+        queue.append(command[1])
+        size += 1
+    elif command[0] == 'pop':
+        if size == 0:
+            print(-1)
+        else:
+            # deque.popleft() => 제일 앞의 원소가 빠져나옴
+            print(queue.popleft())
+            size -= 1
+    elif command[0] == 'front':
+        if size == 0:
+            print(-1)
+        else:
+            # 제일 앞 원소 인덱싱으로 쉽게 접근
+            print(queue[0])
+    elif command[0] == 'back':
+        if size == 0:
+            print(-1)
+        else:
+            # 제일 뒤 원소 -1 인덱싱으로 쉽게 접근
+            print(queue[-1])
+    elif command[0] == 'size':
+        print(size)
+    elif command[0] == 'empty':
+        if size == 0:
+            print(1)
+        else:
+            print(0)

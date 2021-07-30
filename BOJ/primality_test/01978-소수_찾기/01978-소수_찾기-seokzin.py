cacb114eb5e8cc737e864e8688cd800b4003a5df
seokzin <seokzin95@gmail.com>
2021-07-30 18:43:47 +0900

    code: Solve boj 01978 소수 찾기 (seokzin)

--- BOJ/primality_test/01978-소수_찾기/01978-소수_찾기-seokzin.py
+++ BOJ/primality_test/01978-소수_찾기/01978-소수_찾기-seokzin.py
@@ -1,6 +1,23 @@
+n = int(input())
+nums = list(map(int,input().split()))
+prime = 0 # 소수 개수
 
+for num in nums:
+  if num == 1:
+    continue
 
+  elif num == 2:
+    prime += 1
+    continue
 
-"""
-git commit -m "code: Solve boj 01978 소수 찾기 (seokzin)"
-"""
+  else:  # num > 2
+    for x in range(2, num):
+      if num % x == 0:
+        break
+    else:
+      prime += 1  # for문 무사 통과시 -> 소수다
+  
+print(prime)
+
+# for-else 문을 써보고싶었음
+# 근데 for-else 쓸 상황이면 대부분 함수가 더 좋다

e28be76100192549e0e27b31f81ac06026eebe7f
ed-kyu <seungkyukim.dev@gmail.com>
2021-07-30 23:52:05 +0900

    code: Solve boj 01259 펠린드롬수 (seungkyu)

--- BOJ/implementation/01259-펠린드롬수/01259-펠린드롬수-seungkyu.py
+++ BOJ/implementation/01259-펠린드롬수/01259-펠린드롬수-seungkyu.py
@@ -1,6 +1,20 @@
+import sys
 
+def solve() :
+    input = sys.stdin.readline
 
+    while 1 :
+        word = str(input().rstrip()) # readline으로 읽으면 개행까지 읽어옴 주의
 
-"""
-git commit -m "code: Solve boj 01259 펠린드롬수 (seungkyu)"
-"""
+        if word == '0' :
+            break
+        # 단어 뒤집는 행동 간단하게 word[::-1]로 구현 가능
+        print('yes') if word == word[::-1] else print('no')
+
+        # if word == word[::-1] :
+        #     print('yes')
+        # else :
+        #     print('no')
+
+if __name__ == '__main__' :
+    solve()

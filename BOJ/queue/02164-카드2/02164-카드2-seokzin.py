0b315a13d7f1a754f68a22c5579e0587fdf14d8b
seokzin <seokzin95@gmail.com>
2021-07-30 18:56:13 +0900

    code: Solve boj 02164 카드2 (seokzin)

--- BOJ/queue/02164-카드2/02164-카드2-seokzin.py
+++ BOJ/queue/02164-카드2/02164-카드2-seokzin.py
@@ -1,6 +1,13 @@
+from collections import deque
 
+n = int(input())
 
+deq = deque(i for i in range(1, n+1))
 
-"""
-git commit -m "code: Solve boj 02164 카드2 (seokzin)"
-"""
+for _ in range(n-1):
+  deq.popleft()
+  deq.append(deq.popleft())
+
+print(deq[0])
+
+# 어차피 n-1번 반복하기 때문에 while len(deq) > 1 보다 for(n-1)이 성능 좋다

ecb24459615cd07f1fff19ee5b5f92a25f6f1e3a
hing9u <yeonheechae425@gmail.com>
2021-07-31 00:09:59 +0900

    code: Solve boj 01259 펠린드롬수 (yeonhee)

--- BOJ/implementation/01259-펠린드롬수/01259-펠린드롬수-yeonhee.py
+++ BOJ/implementation/01259-펠린드롬수/01259-펠린드롬수-yeonhee.py
@@ -1,6 +1,11 @@
-
+while True:
+    data = input().strip()
+    if data == '0':
+        break
+    print('yes' if data == data[::-1] else 'no')
 
 
 """
 git commit -m "code: Solve boj 01259 펠린드롬수 (yeonhee)"
+신박한 풀이를 고민했는데 신박한게 안나오네요..
 """

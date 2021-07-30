c15ec784329e114fe87316c85257872486300a98
hing9u <yeonheechae425@gmail.com>
2021-07-31 00:44:56 +0900

    code: Solve boj 01920 수 찾기 (yeonhee)

---
+++ BOJ/binary_search/01920-수_찾기/01920-수_찾기-yeonhee.py
@@ -0,0 +1,42 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+sorted_list = sorted(list(map(int, input().split())))
+M = int(input())
+numbers = list(map(int, input().split()))
+
+def binary_search(element):
+    start_idx = 0
+    end_idx = len(sorted_list) - 1
+
+    while start_idx <= end_idx:
+        mid_point = (start_idx + end_idx) // 2
+        if sorted_list[mid_point] == element:
+            return 1
+        elif sorted_list[mid_point] > element:
+            end_idx = mid_point - 1
+        else:
+            start_idx = mid_point + 1
+
+    return 0
+
+for number in numbers:
+    print(binary_search(number))
+
+
+"""
+이분 탐색 : 탐색 범위를 반씩 제외시키며 찾는 알고리즘으로, 정렬된 리스트를 전제로 한다.
+제가 맡은 부분이라 제 취향을 내려놓고 정석을 풀이해봤습니다.
+"""
+
+# 꼼수 풀이
+
+# N = input()
+# A = set(input().split())
+# M = input()
+# numbers = input().split()
+
+# for number in numbers:
+#     print(1 if number in A else 0)

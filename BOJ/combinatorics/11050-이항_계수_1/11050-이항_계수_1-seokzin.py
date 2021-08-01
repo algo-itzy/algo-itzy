9d142f2e52f3c85d876c6b690867849fd69f078d
seokzin <seokzin95@gmail.com>
2021-08-02 01:15:43 +0900

    code: Solve boj 11050 이항 계수 1 (seokzin)

--- BOJ/combinatorics/11050-이항_계수_1/11050-이항_계수_1-seokzin.py
+++ BOJ/combinatorics/11050-이항_계수_1/11050-이항_계수_1-seokzin.py
@@ -1,6 +1,10 @@
+from math import factorial as f
+n, k = map(int, input().split())
 
+print(f(n) // (f(k) * f(n-k)))
 
+# 쉬워서 숏코딩 연습..
 
 """
-git commit -m "code: Solve boj 00000 문제 이름 (seokzin)"
+git commit -m "code: Solve boj 11050 이항 계수 1 (seokzin)"
 """

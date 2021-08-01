da6a2b25d2be1539c3de35b46f598ac0f9cd2a5c
seokzin <seokzin95@gmail.com>
2021-08-02 01:12:25 +0900

    code: Solve boj 10814 나이순 정렬 (seokzin)

--- BOJ/sorting/10814-나이순_정렬/10814-나이순_정렬-seokzin.py
+++ BOJ/sorting/10814-나이순_정렬/10814-나이순_정렬-seokzin.py
@@ -1,6 +1,21 @@
+import sys
+input = sys.stdin.readline
 
+n = int(input())
 
+members = []
+
+for _ in range(n):
+  age, name = map(str, input().split())
+  members.append((int(age), name))
+
+members.sort(key=lambda x: x[0]) 
+
+for member in members:
+  print(*member)
+
+# write()는 readline()에 비해 극적인 성능차이가 없다고 생각합니다
 
 """
-git commit -m "code: Solve boj 00000 문제 이름 (seokzin)"
+git commit -m "code: Solve boj 10814 나이순 정렬 (seokzin)"
 """

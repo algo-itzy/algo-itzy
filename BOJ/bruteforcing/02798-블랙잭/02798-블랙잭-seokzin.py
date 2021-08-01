79065bece1af5cd0b5a16b091aa4be4dcafd75f4
seokzin <seokzin95@gmail.com>
2021-08-02 01:06:40 +0900

    code: Solve boj 02798 블랙잭 (seokzin)

--- BOJ/bruteforcing/02798-블랙잭/02798-블랙잭-seokzin.py
+++ BOJ/bruteforcing/02798-블랙잭/02798-블랙잭-seokzin.py
@@ -1,6 +1,21 @@
+n, m = map(int, input().split())
 
+cards = list(map(int, input().split()))
 
+res = 0
+
+for i in range(n):
+  for j in range(i+1, n):
+    for k in range(j+1, n):
+      picks = cards[i] + cards[j] + cards[k]
+
+      if picks <= m:
+        res = max(picks, res)
+
+print(res)
+
+# 3중 for문 보자마자 순열이 생각났지만 모듈을 지양했음
 
 """
-git commit -m "code: Solve boj 00000 문제 이름 (seokzin)"
+git commit -m "code: Solve boj 02798 블랙잭 (seokzin)"
 """

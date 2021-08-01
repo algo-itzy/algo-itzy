fc8694ee053d4fec960f0135d86dfb8e669006ad
seokzin <seokzin95@gmail.com>
2021-08-02 01:16:34 +0900

    code: Solve boj 11650 좌표 정렬하기 (seokzin)

--- BOJ/sorting/11650-좌표_정렬하기/11650-좌표_정렬하기-seokzin.py
+++ BOJ/sorting/11650-좌표_정렬하기/11650-좌표_정렬하기-seokzin.py
@@ -1,6 +1,22 @@
+import sys
+input = sys.stdin.readline
 
+n = int(input())
 
+dots = []
+
+for _ in range(n):
+  dot = list(map(int, input().split()))
+  dots.append(dot)
+
+dots.sort(key=lambda x: (x[0], x[1]))  # x정렬 후 y정렬
+# dots.sort()
+
+for dot in dots:
+  print(*dot)
+
+# 사실 sort()로 해도 x정렬 -> y정렬 해준답니다
 
 """
-git commit -m "code: Solve boj 00000 문제 이름 (seokzin)"
+git commit -m "code: Solve boj 11650 좌표 정렬하기 (seokzin)"
 """

447054562c533b6e37961ff18fad31ac6419dee0
hing9u <yeonheechae425@gmail.com>
2021-08-01 16:53:23 +0900

    code: Solve boj 02798 블랙잭 (yeonhee)

--- BOJ/bruteforcing/02798-블랙잭/02798-블랙잭-yeonhee.py
+++ BOJ/bruteforcing/02798-블랙잭/02798-블랙잭-yeonhee.py
@@ -1,6 +1,20 @@
+N, M = map(int, input().split())
+numbers = list(map(int, input().split()))
 
+result = 0
+
+for i in range(N):
+    for j in range(i+1, N):
+        for k in range(j+1, N):
+            sum_of_nums = numbers[i] + numbers[j] + numbers[k]
+            if sum_of_nums > M:
+                continue
+            else:
+                result = max(result, sum_of_nums)
+
+print(result)
 
 
 """
-git commit -m "code: Solve boj 00000 문제 이름 (yeonhee)"
+git commit -m "code: Solve boj 02798 블랙잭 (yeonhee)"
 """

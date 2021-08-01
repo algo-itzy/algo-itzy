3909f9fc425b786b463cebe9408d367b02bfeb8f
Chae YeonHee <87457066+hing9u@users.noreply.github.com>
2021-08-02 00:15:04 +0900

    code: Solve boj 10250 ACM 호텔 (yeonhee)

--- BOJ/implementation/10250-ACM_호텔/10250-ACM_호텔-yeonhee.py
+++ BOJ/implementation/10250-ACM_호텔/10250-ACM_호텔-yeonhee.py
@@ -1,5 +1,13 @@
+import sys
+input = sys.stdin.readline
 
-
+for _ in range(int(input())):
+    H, W, N = map(int, input().split())
+    if N % H == 0:
+        floor, unit = H, N//H
+    else:
+        floor, unit = N % H, N//H + 1
+    print (floor * 100 + unit)
 
 """
 git commit -m "code: Solve boj 00000 문제 이름 (yeonhee)"

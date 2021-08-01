e8fba635fa2f6202ecfbbebfc933579a98fc62be
hing9u <yeonheechae425@gmail.com>
2021-08-01 16:17:14 +0900

    code: Solve boj 02609 최대공약수와 최소공배수 (yeonhee)

--- BOJ/number_theory/02609-최대공약수와_최소공배수/02609-최대공약수와_최소공배수-yeonhee.py
+++ BOJ/number_theory/02609-최대공약수와_최소공배수/02609-최대공약수와_최소공배수-yeonhee.py
@@ -1,6 +1,13 @@
+num1, num2 = map(int, input().split())
 
+factor1 = set([i for i in range(1, num1+1) if num1%i == 0])
+factor2 = set([i for i in range(1, num2+1) if num2%i == 0])
+
+gcd = max(factor1 & factor2)  # 최대공약수
+print(gcd)
+print(int(num1 * num2 / gcd))  # 최소공배수
 
 
 """
-git commit -m "code: Solve boj 00000 문제 이름 (yeonhee)"
+git commit -m "code: Solve boj 02609 최대공약수와 최소공배수 (yeonhee)"
 """

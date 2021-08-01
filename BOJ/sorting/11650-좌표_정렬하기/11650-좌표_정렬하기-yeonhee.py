cd72070de25667a27eaf51b68b949ff8709b82e8
Chae YeonHee <87457066+hing9u@users.noreply.github.com>
2021-08-02 00:42:14 +0900

    code: Solve boj 11650 좌표 정렬하기 (yeonhee)

--- BOJ/sorting/11650-좌표_정렬하기/11650-좌표_정렬하기-yeonhee.py
+++ BOJ/sorting/11650-좌표_정렬하기/11650-좌표_정렬하기-yeonhee.py
@@ -1,6 +1,13 @@
+import sys
+input = sys.stdin.readline
 
+coordinates = []
 
+for _ in range(int(input())):
+    coordinates.append(list(map(int, input().split())))
 
-"""
-git commit -m "code: Solve boj 00000 문제 이름 (yeonhee)"
-"""
+coordinates.sort(key = lambda x: (x[0], x[1]))
+
+for x, y in coordinates:
+    print(x, y)
+    

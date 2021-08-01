81f244c015a3569f11847675e189706b00ec595d
Chae YeonHee <87457066+hing9u@users.noreply.github.com>
2021-08-02 00:25:13 +0900

    code: Solve boj 10816 숫자 카드 2 (yeonhee)

--- BOJ/binary_search/10816-숫자_카드_2/10816-숫자_카드_2-yeonhee.py
+++ BOJ/binary_search/10816-숫자_카드_2/10816-숫자_카드_2-yeonhee.py
@@ -1,4 +1,17 @@
+import sys
+from collections import Counter
 
+input = sys.stdin.readline
+
+N = int(input())
+numbers = list(map(int, input().split()))
+M = int(input())
+key_nums = list(map(int, input().split()))
+
+num_cnt = Counter(numbers)
+
+for key_num in key_nums:
+    print(num_cnt[key_num], end = ' ')
 
 
 """

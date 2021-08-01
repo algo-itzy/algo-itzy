2542ace0a7262ba5c67e6d7caceea7dea1fdce1b
seokzin <seokzin95@gmail.com>
2021-08-02 01:10:29 +0900

    code: Solve boj 10250 ACM 호텔 (seokzin)

--- BOJ/implementation/10250-ACM_호텔/10250-ACM_호텔-seokzin.py
+++ BOJ/implementation/10250-ACM_호텔/10250-ACM_호텔-seokzin.py
@@ -1,6 +1,13 @@
+t = int(input())
 
+for _ in range(t):
+  h, w, n = map(int, input().split())
+  room = (((n-1)%h)+1)*100 + (((n-1)//h)+1)
+  
+  print(room)
 
+# 예외 처리를 없애고 싶어서 n-1을 활용했습니다
 
 """
-git commit -m "code: Solve boj 00000 문제 이름 (seokzin)"
+git commit -m "code: Solve boj 10250 ACM 호텔 (seokzin)"
 """

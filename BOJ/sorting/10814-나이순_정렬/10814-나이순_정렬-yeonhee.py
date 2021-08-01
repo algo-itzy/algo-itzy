589de61db8376d2bdbd667d03934f637f47fb9b2
Chae YeonHee <87457066+hing9u@users.noreply.github.com>
2021-08-02 00:15:30 +0900

    code: Solve boj 10814 나이순 정렬 (yeonhee)

--- BOJ/sorting/10814-나이순_정렬/10814-나이순_정렬-yeonhee.py
+++ BOJ/sorting/10814-나이순_정렬/10814-나이순_정렬-yeonhee.py
@@ -1,5 +1,19 @@
+import sys
+input = sys.stdin.readline
 
+members = {}
 
+for _ in range(int(input())):
+    age, name = input().split()
+    age = int(age)
+    if age in members:
+        members[age].append(name)
+    else:
+        members[age] = [name]
+
+for k, v in sorted(members.items(), key = lambda x: x[0]):
+    for member in v:
+        print(k, member)
 
 """
 git commit -m "code: Solve boj 00000 문제 이름 (yeonhee)"

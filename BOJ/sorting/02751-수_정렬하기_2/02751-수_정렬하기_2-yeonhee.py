3d90276eb47e9da96361a0605a52ebd7daa456a7
hing9u <yeonheechae425@gmail.com>
2021-08-01 17:33:55 +0900

    code: Solve boj 02751 수 정렬하기 2 (yeonhee)

--- BOJ/sorting/02751-수_정렬하기_2/02751-수_정렬하기_2-yeonhee.py
+++ BOJ/sorting/02751-수_정렬하기_2/02751-수_정렬하기_2-yeonhee.py
@@ -1,6 +1,10 @@
+import sys
+input = sys.stdin.readline
 
+numbers = [int(input()) for _ in range(int(input()))]
+print('\n'.join(map(str, sorted(numbers))))
 
 
 """
-git commit -m "code: Solve boj 00000 문제 이름 (yeonhee)"
+git commit -m "code: Solve boj 02751 수 정렬하기 2 (yeonhee)"
 """

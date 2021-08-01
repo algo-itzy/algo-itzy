4e404de4e0027464cf571d219b1d260c5a94e9ce
Julia-we-s2 <thizzyzz@naver.com>
2021-08-01 09:37:43 +0900

    code: Solve boj 01259 펠린드롬수 (yeonju)

--- BOJ/implementation/01259-펠린드롬수/01259-펠린드롬수-yeonju.py
+++ BOJ/implementation/01259-펠린드롬수/01259-펠린드롬수-yeonju.py
@@ -1,3 +1,20 @@
+while True:
+
+    N = input() 
+    
+    if N == '0': # 0 을 입력 받으면 종료
+        break
+
+    if N == N[::-1]: # 입력 받은 수가 그 수를 역순으로 했을 때와 같은지 비교
+        print('yes')
+    else:
+        print('no')
+    
+
+
+# string = 'Hello, World!'
+# reversed_str = string[::-1]
+# print(reversed_str)
 
 
 

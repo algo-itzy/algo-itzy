95fd455808780737356bbd01cd6353a6bed35312
seokzin <seokzin95@gmail.com>
2021-08-02 01:04:53 +0900

    code: Solve boj 02609 최대공약수와 최소공배수 (seokzin)

--- BOJ/number_theory/02609-최대공약수와_최소공배수/02609-최대공약수와_최소공배수-seokzin.py
+++ BOJ/number_theory/02609-최대공약수와_최소공배수/02609-최대공약수와_최소공배수-seokzin.py
@@ -1,5 +1,18 @@
+a, b = map(int, input().split())
 
+def gcd(x, y):
+  if y == 0:
+    return x
+  return gcd(y, x%y)
 
+gcd = gcd(a, b)
+lcm = a*b // gcd
+
+print(gcd)
+print(lcm)
+
+# 일부러 재귀를 사용
+# gcd() 연산을 2번 하기 싫어 lcm 함수 선언 X
 
 """
 git commit -m "code: Solve boj 00000 문제 이름 (seokzin)"

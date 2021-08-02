26fc768ccd80458070e498fabd259cb1556f0227
YoonBaek <royal729@naver.com>
2021-08-02 18:37:31 +0900

    code: Solve boj 4153 직각삼각형 (yoonbaek)

--- BOJ/geometry/04153-직각삼각형/04153-직각삼각형-yoonbaek.py
+++ BOJ/geometry/04153-직각삼각형/04153-직각삼각형-yoonbaek.py
@@ -1,6 +1,26 @@
+# solved by YoonBaek
+from sys import stdin, stdout
 
+input = stdin.readline
+print = stdout.write
 
+def is_pyth(a, b, c):
+    if a > b:
+        a, b = b, a
+    if b > c:
+        b, c = c, b
 
-"""
-git commit -m "code: Solve boj 00000 문제 이름 (yoonbaek)"
-"""
+    if a**2 + b**2 == c**2:
+        return "right\n"
+
+    return "wrong\n"
+        
+
+if __name__ == "__main__":
+    
+    while True:
+        a, b, c = map(int, input().split())
+    
+        if a == b == c == 0:
+            break
+        print(is_pyth(a, b, c))

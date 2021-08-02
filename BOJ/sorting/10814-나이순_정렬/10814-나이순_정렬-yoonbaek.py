a74dadb16c0db67c5d6002d10d5212b062339a4d
YoonBaek <royal729@naver.com>
2021-08-02 19:57:39 +0900

    code: Solve boj 10814 나이순 정렬 (yoonbaek)

--- BOJ/sorting/10814-나이순_정렬/10814-나이순_정렬-yoonbaek.py
+++ BOJ/sorting/10814-나이순_정렬/10814-나이순_정렬-yoonbaek.py
@@ -1,6 +1,24 @@
+# solved by Yoon Baek
+from sys import stdin, stdout
 
+input, print = stdin.readline, stdout.write
 
+# save
+def save() -> None:
+    age, name = input().split()
+    # save name at AGEth list
+    ages[int(age)].append(name)
 
-"""
-git commit -m "code: Solve boj 00000 문제 이름 (yoonbaek)"
-"""
+if __name__ == "__main__":
+    # generate list to save data
+    ages = []
+    # make 200 (age range) level2 lists
+    for _ in range(201): ages+=[[]]
+
+    n = int(input())
+    for _ in range(n): save()
+
+    # untie
+    for age, names in enumerate(ages):
+        for name in names:
+            print(f"{age} {name}\n")

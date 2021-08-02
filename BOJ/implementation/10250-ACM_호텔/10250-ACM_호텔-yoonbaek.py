ca808ff141246ff64fa04a58243b835ccc48b202
YoonBaek <royal729@naver.com>
2021-08-02 19:15:29 +0900

    code: Solve boj 10250 ACM 호텔 (yoonbaek)

--- BOJ/implementation/10250-ACM_호텔/10250-ACM_호텔-yoonbaek.py
+++ BOJ/implementation/10250-ACM_호텔/10250-ACM_호텔-yoonbaek.py
@@ -1,6 +1,21 @@
+# Solved by YoonBaek
+from sys import stdin, stdout
 
+input = stdin.readline
+print = stdout.write
 
+def solve(h: int, w: int, n: int) -> str:
+    room_num = n//h+ 1
+    floor = n%h
+    if not floor:
+        room_num -= 1
+        floor = h
 
-"""
-git commit -m "code: Solve boj 00000 문제 이름 (yoonbaek)"
-"""
+    return f"{floor}{room_num:02}\n" 
+
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(t):
+        h, w, n = map(int, input().split())
+    
+        print(solve(h,w,n))

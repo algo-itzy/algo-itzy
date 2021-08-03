734b47ec7f006a405fd5a4691860e4b7a7cf5e89
서울_4반_정지웅 <j2woong1@gmail.com>
2021-08-03 23:22:03 +0900

    code: solve boj 10814 나이순_정렬 (jiwoong)

--- BOJ/sorting/10814-나이순_정렬/10814-나이순_정렬-jiwoong.py
+++ BOJ/sorting/10814-나이순_정렬/10814-나이순_정렬-jiwoong.py
@@ -1,6 +1,13 @@
+# 나이가 증가하는 순으로, 나이가 같으면 먼저 가입한 사람이 앞에 오는 순서로 정렬
+# 각 회원의 나이와 이름이 공백으로 구분
 
+import sys
 
+n = int(sys.stdin.readline())
+mems = []
 
-"""
-git commit -m "code: Solve boj 00000 문제 이름 (jiwoong)"
-"""
+for _ in range(n):
+    mems.append(list(sys.stdin.readline().split()))
+mems.sort(key=lambda x: int(x[0]))
+for i in range(n):
+    print(mems[i][0], mems[i][1])

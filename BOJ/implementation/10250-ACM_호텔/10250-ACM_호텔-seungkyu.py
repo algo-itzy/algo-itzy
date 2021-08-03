2f84246bbdf91d686e8296fbe410e2667d0bcade
ed-kyu <seungkyukim.dev@gmail.com>
2021-08-03 23:48:29 +0900

    code: Solve boj 10250 ACM 호텔 (seungkyu)

--- BOJ/implementation/10250-ACM_호텔/10250-ACM_호텔-seungkyu.py
+++ BOJ/implementation/10250-ACM_호텔/10250-ACM_호텔-seungkyu.py
@@ -1,6 +1,27 @@
+'''
+층과 거리 계산하는 것은 간단,
+층이 꼭대기일 경우의 수를 따로 처리해주어야 함(이것 때문에 한 번 틀림)
+'''
+import sys
 
+def solve():
+    input = sys.stdin.readline
+    t = int(input())
 
+    for _ in range(t):
+        h, w, n = map(int, input().split())
+        floor = n % h
+        dist = (n//h) + 1
 
-"""
-git commit -m "code: Solve boj 00000 문제 이름 (seungkyu)"
-"""
+        # 층이 꼭대기인 경우를 따로 생각
+        if floor == 0:
+            floor = h
+            dist = (n//h)
+        # 호수가 10호 미만일 경우와 이외의 경우로 나눠서 생각
+        if dist < 10:
+            print(f'{floor}0{dist}')
+        else:
+            print(f'{floor}{dist}')
+        
+if __name__ == "__main__":
+    solve()

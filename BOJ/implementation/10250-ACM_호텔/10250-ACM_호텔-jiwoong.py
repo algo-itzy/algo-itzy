a99aa1d115813c90fe4894773beca4da218f6a10
서울_4반_정지웅 <j2woong1@gmail.com>
2021-08-03 23:21:03 +0900

    code: solve boj 10250 ACM_호텔 (jiwoong)

--- BOJ/implementation/10250-ACM_호텔/10250-ACM_호텔-jiwoong.py
+++ BOJ/implementation/10250-ACM_호텔/10250-ACM_호텔-jiwoong.py
@@ -1,6 +1,13 @@
+# 빈 방 배정
+# 걷는 거리가 같을 때에는 아래층의 방을 더 선호
+import sys
 
-
-
-"""
-git commit -m "code: Solve boj 00000 문제 이름 (jiwoong)"
-"""
+t = int(input())
+for _ in range(t):
+    h, w, n = map(int, sys.stdin.readline().split()) # 층 수, 각 층의 방 수, 몇 번째 손님
+    floor = n % h
+    room = n // h + 1
+    if floor == 0:
+        room -= 1
+        floor = h
+    print(floor * 100 + room)

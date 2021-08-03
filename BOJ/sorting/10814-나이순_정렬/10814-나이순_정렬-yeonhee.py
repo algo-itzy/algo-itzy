bdbc23741fe66cef0c0733e18f99a3cdcbb88ede
Chae YeonHee <87457066+hing9u@users.noreply.github.com>
2021-08-03 21:24:17 +0900

    fix: Delete comments

--- BOJ/sorting/10814-나이순_정렬/10814-나이순_정렬-yeonhee.py
+++ BOJ/sorting/10814-나이순_정렬/10814-나이순_정렬-yeonhee.py
@@ -6,6 +6,7 @@ members = {}
 for _ in range(int(input())):
     age, name = input().split()
     age = int(age)
+    
     if age in members:
         members[age].append(name)
     else:
@@ -14,7 +15,3 @@ for _ in range(int(input())):
 for k, v in sorted(members.items(), key = lambda x: x[0]):
     for member in v:
         print(k, member)
-
-"""
-git commit -m "code: Solve boj 00000 문제 이름 (yeonhee)"
-"""

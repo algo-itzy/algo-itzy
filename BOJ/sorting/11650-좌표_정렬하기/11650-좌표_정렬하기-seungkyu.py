0f87c1c28cb72ab59ad2aba2dcb69bdb52a68cb4
ed-kyu <seungkyukim.dev@gmail.com>
2021-08-03 23:01:06 +0900

    code: Solve boj 11650 좌표 정렬하기 (seungkyu)

--- BOJ/sorting/11650-좌표_정렬하기/11650-좌표_정렬하기-seungkyu.py
+++ BOJ/sorting/11650-좌표_정렬하기/11650-좌표_정렬하기-seungkyu.py
@@ -1,6 +1,21 @@
+import sys
 
 
+def solve():
+    input = sys.stdin.readline
+    n = int(input())
+    dots = []
+    
+    for _ in range(n):
+        x_y = list(map(int, input().split()))
+        # [x, y] 형태 그대로 새로운 리스트에 append
+        dots.append(x_y)
 
-"""
-git commit -m "code: Solve boj 00000 문제 이름 (seungkyu)"
-"""
+    # 1. x[0] -> x , 2. x[1] -> y 순서로 정렬함 (-붙이면 반대로 정렬)
+    sorted_dots = sorted(dots, key=lambda x: (x[0], x[1]))
+    for dots in sorted_dots:
+        print(*dots)
+
+
+if __name__ == "__main__":
+    solve()

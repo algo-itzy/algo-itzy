1f276b256393e046d40352c92792fffde10dd6fb
ed-kyu <seungkyukim.dev@gmail.com>
2021-08-03 23:45:23 +0900

    code: Solve boj 02751 수 정렬하기 2 (seungkyu)

--- BOJ/sorting/02751-수_정렬하기_2/02751-수_정렬하기_2-seungkyu.py
+++ BOJ/sorting/02751-수_정렬하기_2/02751-수_정렬하기_2-seungkyu.py
@@ -1,6 +1,16 @@
+import sys
 
 
+def solve():
+    n = int(input())
+    numbers = []
 
-"""
-git commit -m "code: Solve boj 00000 문제 이름 (seungkyu)"
-"""
+    # 숫자를 모두 입력 받고 한 번만 정렬 => 시간 오래 걸림
+    for _ in range(n):
+        number = int(sys.stdin.readline().strip())
+        numbers.append(number)
+    print(*sorted(numbers))
+
+
+if __name__ == "__main__":
+    solve()

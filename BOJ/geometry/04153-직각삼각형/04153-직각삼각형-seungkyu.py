d6887068f71ad2033f161177cea5a0aa29548580
ed-kyu <seungkyukim.dev@gmail.com>
2021-08-03 23:47:37 +0900

    code: Solve boj 04153 직각삼각형 (seungkyu)

--- BOJ/geometry/04153-직각삼각형/04153-직각삼각형-seungkyu.py
+++ BOJ/geometry/04153-직각삼각형/04153-직각삼각형-seungkyu.py
@@ -1,6 +1,18 @@
+def solve():
+
+    while 1:
+        lenths = list(map(int, input().split()))
+        # 길이가 0인 경우는 없으므로 lenths[0] 만 체크해도 될듯
+        if lenths[0] == 0 and lenths[1] == 0 and lenths[2] == 0:
+            break
+        # 길이가 작은 2개 변 골라내기 위해 정렬
+        lenths.sort()
+
+        if lenths[0] ** 2 + lenths[1] ** 2 == lenths[2] ** 2:
+            print("right")
+        else:
+            print("wrong")
 
 
-
-"""
-git commit -m "code: Solve boj 00000 문제 이름 (seungkyu)"
-"""
+if __name__ == "__main__":
+    solve()

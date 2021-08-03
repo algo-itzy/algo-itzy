efa8458c4e6198879e7bf5d3fd09b44add7466d3
Chae YeonHee <87457066+hing9u@users.noreply.github.com>
2021-08-03 21:14:54 +0900

    fix: Edit comments

--- BOJ/number_theory/02609-최대공약수와_최소공배수/02609-최대공약수와_최소공배수-yeonhee.py
+++ BOJ/number_theory/02609-최대공약수와_최소공배수/02609-최대공약수와_최소공배수-yeonhee.py
@@ -3,7 +3,10 @@ num1, num2 = map(int, input().split())
 factor1 = set([i for i in range(1, num1+1) if num1%i == 0])
 factor2 = set([i for i in range(1, num2+1) if num2%i == 0])
 
-gcd = max(factor1 & factor2)  # 최대공약수
+# 최대공약수 : set의 교집합을 이용한 풀이
+gcd = max(factor1 & factor2)
 print(gcd)
-print(int(num1 * num2 / gcd))  # 최소공배수
+
+# 최소공배수 : 위에서 구한 최대공약수를 이용한 풀이
+print(int(num1 * num2 / gcd))
 

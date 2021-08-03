d0da80ff58530357a96ada6edbdd65b2de15e330
Chae YeonHee <87457066+hing9u@users.noreply.github.com>
2021-08-03 21:18:31 +0900

    fix: Reduce length of code

--- BOJ/bruteforcing/02798-블랙잭/02798-블랙잭-yeonhee.py
+++ BOJ/bruteforcing/02798-블랙잭/02798-블랙잭-yeonhee.py
@@ -1,3 +1,6 @@
+from typing import Mapping
+
+
 N, M = map(int, input().split())
 numbers = list(map(int, input().split()))
 
@@ -7,9 +10,7 @@ for i in range(N):
     for j in range(i+1, N):
         for k in range(j+1, N):
             sum_of_nums = numbers[i] + numbers[j] + numbers[k]
-            if sum_of_nums > M:
-                continue
-            else:
+            if sum_of_nums <= M:
                 result = max(result, sum_of_nums)
 
 print(result)

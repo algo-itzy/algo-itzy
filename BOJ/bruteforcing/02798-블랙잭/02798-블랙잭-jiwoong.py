edff2ab543b3e2d8b7460ba4905d8f0edd51b19f
서울_4반_정지웅 <j2woong1@gmail.com>
2021-08-03 23:18:49 +0900

    code: solve boj 02798 블랙잭 (jiwoong)

--- BOJ/bruteforcing/02798-블랙잭/02798-블랙잭-jiwoong.py
+++ BOJ/bruteforcing/02798-블랙잭/02798-블랙잭-jiwoong.py
@@ -1,6 +1,14 @@
+import sys
 
+n, m = map(int, sys.stdin.readline().split())
+cards = list(map(int, sys.stdin.readline().split()))
+ans = 0
 
-
-"""
-git commit -m "code: Solve boj 00000 문제 이름 (jiwoong)"
-"""
+for a in range(n - 2):
+    for b in range(a + 1, n - 1):
+        for c in range(b + 1, n):
+            if cards[a] + cards[b] + cards[c] > m:
+                continue
+            else:
+                ans = max(ans, cards[a] + cards[b] + cards[c])
+print(ans)

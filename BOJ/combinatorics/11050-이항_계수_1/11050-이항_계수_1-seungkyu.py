e48b0ddf23bea26c6a56f14f8f57db34de74fc37
ed-kyu <seungkyukim.dev@gmail.com>
2021-08-03 23:51:17 +0900

    code: Solve boj 11050 이항 계수 1 (seungkyu)

--- BOJ/combinatorics/11050-이항_계수_1/11050-이항_계수_1-seungkyu.py
+++ BOJ/combinatorics/11050-이항_계수_1/11050-이항_계수_1-seungkyu.py
@@ -1,6 +1,21 @@
-
-
-
 """
-git commit -m "code: Solve boj 00000 문제 이름 (seungkyu)"
+팩토리얼
 """
+# import math, math.factorial 이용 => 시간 비슷
+
+
+def solve():
+    n, k = map(int, input().split())
+    print(factorial(n) // (factorial(k) * factorial(n-k)))
+
+
+def factorial(n):
+
+    if n <= 1:
+        return 1
+
+    return factorial(n-1) * n
+
+
+if __name__ == "__main__":
+    solve()

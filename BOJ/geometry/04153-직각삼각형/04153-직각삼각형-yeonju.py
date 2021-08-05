710e5ee0a2b356b62aa9c412369d47661d48a772
Julia-we-s2 <thizzyzz@naver.com>
2021-08-05 18:49:50 +0900

    code: Solve boj 04153 직각삼각형 (yeonju)

--- BOJ/geometry/04153-직각삼각형/04153-직각삼각형-yeonju.py
+++ BOJ/geometry/04153-직각삼각형/04153-직각삼각형-yeonju.py
@@ -1,3 +1,20 @@
+"""
+각 테스트케이시는 모두 30,000보다 작은 양의 정수
+입력 마지막 줄에는 000
+"""
+
+tri = list(map(int, input().split()))
+
+while tri!=[0,0,0]: # 0 0 0 이 나오면 종료
+
+    tri.sort() # 오름차순 배열
+
+    if tri[2]**2 == tri[0]**2 + tri[1]**2: # 직각 삼각형 구하는 공식 사용
+        print('right')
+        
+    else: print('wrong')
+    tri = list(map(int, input().split()))
+
 
 
 

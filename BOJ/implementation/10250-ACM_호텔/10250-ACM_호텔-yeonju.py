70aec25e93ed16b6b36d85f9ff47c00888a9ef00
Julia-we-s2 <thizzyzz@naver.com>
2021-08-05 18:54:45 +0900

    code: Solve boj 10250 ACM 호텔 (yeonju)

--- BOJ/implementation/10250-ACM_호텔/10250-ACM_호텔-yeonju.py
+++ BOJ/implementation/10250-ACM_호텔/10250-ACM_호텔-yeonju.py
@@ -1,4 +1,22 @@
+"""
+1<=H, W<=99
+"""
 
+# T는 테스트 케이스의 개수
+# H는 층수, W 는 호수, N은 마지막 번째 손님
+
+T = int(input())
+
+for i in range(T):
+    H, W, N = map(int,input().split()) 
+    
+    a = N % H # N번째 손님이 머물게 될 층의 수
+    b = N// H +1 # N번쨰 손님이 머물게 될 호수
+    if a == 0:
+        a = H
+        b = N//H
+    
+    print(a*100+ b)
 
 
 """

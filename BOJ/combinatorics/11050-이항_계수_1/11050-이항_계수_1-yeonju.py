39bf055612cba77bd1806eea35528804de32fe0c
Julia-we-s2 <thizzyzz@naver.com>
2021-08-05 18:57:50 +0900

    code: Solve boj 11050 이항 계수 1 (yeonju)

--- BOJ/combinatorics/11050-이항_계수_1/11050-이항_계수_1-yeonju.py
+++ BOJ/combinatorics/11050-이항_계수_1/11050-이항_계수_1-yeonju.py
@@ -1,5 +1,8 @@
+from math import factorial # 팩토리얼을 사용 
 
+N, K = list(map(int, input().split())) 
 
+print(factorial(N)//(factorial(K) * factorial(N-K))) 
 
 """
 git commit -m "code: Solve boj 00000 문제 이름 (yeonju)"

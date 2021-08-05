4d5b962ade49faacd3ead886666b87ff96c73445
YoonBaek <royal729@naver.com>
2021-08-05 18:32:01 +0900

    code: Solv boj 11650 좌표 정렬하기 (yoonbaek)

--- BOJ/sorting/11650-좌표_정렬하기/11650-좌표_정렬하기-yoonbaek.py
+++ BOJ/sorting/11650-좌표_정렬하기/11650-좌표_정렬하기-yoonbaek.py
@@ -1,6 +1,24 @@
+# solved by YoonBaek
+from sys import stdin, stdout
 
 
+print = stdout.write
 
-"""
-git commit -m "code: Solve boj 00000 문제 이름 (yoonbaek)"
-"""
+
+def input()->list:
+    lines = stdin.read().rstrip().split("\n")[1:]
+    co_ords = []
+
+    for line in lines:
+        x, y = line.split()
+        co_ords.append([int(x), int(y)])
+
+    return co_ords
+
+
+if __name__ == "__main__":
+    co_ords = input()
+    co_ords.sort(key=lambda x:(x[0], x[1]))
+
+    for x, y in co_ords:
+        print(f"{x} {y}\n")

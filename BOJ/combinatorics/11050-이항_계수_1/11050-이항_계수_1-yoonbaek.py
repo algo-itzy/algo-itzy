f87dd7f426da071edf960ebbe6955164bdb7f425
YoonBaek <royal729@naver.com>
2021-08-05 12:26:36 +0900

    code: Solve boj 11050 이항 계수 (yoonbaek)

--- BOJ/combinatorics/11050-이항_계수_1/11050-이항_계수_1-yoonbaek.py
+++ BOJ/combinatorics/11050-이항_계수_1/11050-이항_계수_1-yoonbaek.py
@@ -1,6 +1,26 @@
+# solved by YoonBaek
+# before facts: 84
+# after facts: 
+
+facts = []
+for _ in range(10+1):
+    facts += [0]
 
 
+def factorial(n: int)->int:
+    facts[0] = facts[1] = 1
+    for val in range(2,n+1):
+        if not facts[val]:
+            facts[val] = val * facts[val-1]
+ 
+    return facts[n]
 
-"""
-git commit -m "code: Solve boj 00000 문제 이름 (yoonbaek)"
-"""
+
+def combination(n: int, k: int)->int:
+    return factorial(n) // (factorial(n-k)*factorial(k))
+
+
+if __name__ == "__main__":
+    n, k = list(map(int, input().split()))
+
+    print(combination(n,k))

fc65ac4b344db3ba70d5df1acdd322ded6926dba
Julia-we-s2 <thizzyzz@naver.com>
2021-08-05 18:55:59 +0900

    code: Solve boj 10814 나이순 정렬 (yeonju)

--- BOJ/sorting/10814-나이순_정렬/10814-나이순_정렬-yeonju.py
+++ BOJ/sorting/10814-나이순_정렬/10814-나이순_정렬-yeonju.py
@@ -1,3 +1,22 @@
+"""
+회원들의 나이가 증가하는 순으로
+나이가 같으면 먼저 가입한 사람이 앞에 
+회원 수는 1<=N <-100,000
+"""
+import sys
+
+N = int(sys.stdin.readline()) # 온라인 저지 회원의 수 
+li=[]
+
+for _ in range(N):
+    (age,name) = sys.stdin.readline().split()
+    li.append(list(int(age),name))
+
+sorted_list =sorted(li, key = lambda x: x[0]) # 어린 순서대로 배열
+
+for i in sorted_list:
+    print(i[0],i[1])
+
 
 
 

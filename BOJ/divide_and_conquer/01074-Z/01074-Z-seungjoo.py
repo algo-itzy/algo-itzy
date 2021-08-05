1ad8961fa27259fe6250e12dc4a9b2486ed9d7c9
chaselover <gkstmdwn234@gmail.com>
2021-08-06 08:52:02 +0900

    code: Solve boj 1074 Z (seungjoo)

--- BOJ/divide_and_conquer/01074-Z/01074-Z-seungjoo.py
+++ BOJ/divide_and_conquer/01074-Z/01074-Z-seungjoo.py
@@ -1,1 +1,22 @@
-# delete this and solve here
+import sys
+input = sys.stdin.readline
+
+# 핵심은 배열을 4등분하는 분할을 통해 
+# 배열의 크기가 1인 사각형들의 모임으로 만드는 것. 
+def Z(n,r,c):
+    # 배열의 크기가 1이 되면 0을 정복하여 반환한다.
+    if n==0: 
+        return 0
+    # 배열의 크기를 반을 기준으로 4영역으로 분할한다.
+    harf = 2**(n-1)
+    if r < harf and c<harf: 
+        return Z(n-1,r,c)
+    elif r < harf and c>=harf: 
+        return harf**2 + Z(n-1,r,c-harf)
+    elif r >=harf and c<harf: 
+        return 2*harf**2 + Z(n-1,r-harf,c)
+    else:
+        return 3*harf**2 + Z(n-1,r-harf,c-harf)
+
+N,r,c = map(int,input().split())
+print(Z(N,r,c))

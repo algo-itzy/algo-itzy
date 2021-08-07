3710b887cad58a197e89fdc9866b1e00792a81e7
서울_4반_정지웅 <j2woong1@gmail.com>
2021-08-07 19:12:22 +0900

    code: solve boj 01463 1로_만들기 (jiwoong)

--- BOJ/dp/01463-1로_만들기/01463-1로_만들기-jiwoong.py
+++ BOJ/dp/01463-1로_만들기/01463-1로_만들기-jiwoong.py
@@ -1,1 +1,17 @@
-# delete this and solve here
+# X가 3으로 나누어 떨어지면, 3으로 나눈다.
+# X가 2로 나누어 떨어지면, 2로 나눈다.
+# 1을 뺀다.
+
+import sys
+
+input = sys.stdin.readline()
+n = int(input)
+lst = [0] * (n + 1)
+
+for i in range(2, n + 1):
+    lst[i] = lst[i - 1] + 1
+    if i % 3 == 0:
+        lst[i] = min(lst[i], lst[i // 3] + 1)
+    if i % 2 == 0:
+        lst[i] = min(lst[i], lst[i // 2] + 1)
+print(lst[n])

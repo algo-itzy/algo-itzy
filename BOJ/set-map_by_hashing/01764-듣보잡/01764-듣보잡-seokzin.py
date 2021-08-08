43f48d9d90d924b1aebad73ba80deb84a89b29be
seokzin <seokzin95@gmail.com>
2021-08-09 01:24:51 +0900

    code: Solve boj 01764 듣보잡 (seokzin)

--- BOJ/set-map_by_hashing/01764-듣보잡/01764-듣보잡-seokzin.py
+++ BOJ/set-map_by_hashing/01764-듣보잡/01764-듣보잡-seokzin.py
@@ -1,1 +1,19 @@
-# delete this and solve here
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+d = []  # 듣
+b = []  # 보
+
+for _ in range(n):
+    d.append(input().rstrip())
+
+for _ in range(m):
+    b.append(input().rstrip())
+
+db = sorted(list(set(d) & set(b)))  # 듣보
+
+print(len(db))
+
+print(*db, sep='\n')

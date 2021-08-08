336c92e38985606ce5ce70f99fe8a846d30836cb
YoonBaek <royal729@naver.com>
2021-08-08 11:03:25 +0900

    code: Solve boj 01463 1로 만들기 (yoonbaek)

--- BOJ/dp/01463-1로_만들기/01463-1로_만들기-yoonbaek.py
+++ BOJ/dp/01463-1로_만들기/01463-1로_만들기-yoonbaek.py
@@ -1,1 +1,28 @@
-# delete this and solve here
+# solved by YoonBaek
+
+def init_dp()->list:
+    dp = [0]*int(n + 1)
+
+    return dp
+
+def get_min_ops(n: int):
+    #
+    for i in range(2, n+1):
+        # case -1
+        min_ops[i] = min_ops[i-1]+1
+        # case / 2
+        if not i%2:
+            min_ops[i] = min(min_ops[i], min_ops[i//2]+1)
+        # case / 3
+        if not i%3:
+            min_ops[i] = min(min_ops[i], min_ops[i//3]+1)   
+
+    return min_ops[n]
+
+
+    
+if __name__ == "__main__":
+    n = int(input())
+    min_ops = init_dp()
+    
+    print(get_min_ops(n))

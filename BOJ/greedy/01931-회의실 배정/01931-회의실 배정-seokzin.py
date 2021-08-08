5490821f72cd0276cf2539d32bae8e4bc554cc6d
seokzin <seokzin95@gmail.com>
2021-08-09 01:28:51 +0900

    code: Solve boj 01931 회의실 배정 (seokzin)

--- BOJ/greedy/01931-회의실 배정/01931-회의실 배정-seokzin.py
+++ BOJ/greedy/01931-회의실 배정/01931-회의실 배정-seokzin.py
@@ -1,1 +1,21 @@
-# delete this and solve here
+n = int(input())
+times = []  # 회의 시간 리스트
+
+for _ in range(n):
+    start, end = map(int, input().split())
+    times.append([start, end])
+
+times.sort(key=lambda x: (x[1], x[0]))
+
+last = 0  # 마지막 회의 종료 시간
+cnt = 0
+
+for s, e in times:
+    if s >= last:
+        cnt += 1
+        last = e
+
+print(cnt)
+
+# Greedy
+# 정렬만으로 최적을 보장한다? 100% 와닿지는 않았다.

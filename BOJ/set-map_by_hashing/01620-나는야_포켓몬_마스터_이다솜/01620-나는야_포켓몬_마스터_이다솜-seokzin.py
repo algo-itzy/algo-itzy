b1ff561500b8ec1b5ca41a5062864e4e2028bcb1
seokzin <seokzin95@gmail.com>
2021-08-09 01:23:13 +0900

    code: Solve boj 01620 나는야 포켓몬 마스터 이다솜 (seokzin)

--- BOJ/set-map_by_hashing/01620-나는야_포켓몬_마스터_이다솜/01620-나는야_포켓몬_마스터_이다솜-seokzin.py
+++ BOJ/set-map_by_hashing/01620-나는야_포켓몬_마스터_이다솜/01620-나는야_포켓몬_마스터_이다솜-seokzin.py
@@ -1,1 +1,20 @@
-# delete this and solve here
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+dic_idx = {}  # idx: name 쌍
+dic_name = {}  # name: idx 쌍
+
+for idx in range(1, n+1):
+    name = input().rstrip()  # \n 제거
+    dic_idx[idx] = name 
+    dic_name[name] = idx
+
+for _ in range(m):
+    q = input().rstrip() 
+
+    print(dic_name[q]) if q.isalpha() else print(dic_idx[int(q)])
+
+# Value로 Key 출력 방법 - dict를 뒤집어 Value-Key dic을 만든다.
+# 사실 더 좋은 풀이가 있을 것 같지만 이 정도에서 마무리 합니다.

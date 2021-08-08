95ce22af0ceab812ac064a17e2b3501224b06276
chaselover <gkstmdwn234@gmail.com>
2021-08-08 17:54:01 +0900

    code: Solve boj 01620 나는야 포켓몬 마스터 이다솜 (seungjoo)

--- BOJ/set-map_by_hashing/01620-나는야_포켓몬_마스터_이다솜/01620-나는야_포켓몬_마스터_이다솜-seungjoo.py
+++ BOJ/set-map_by_hashing/01620-나는야_포켓몬_마스터_이다솜/01620-나는야_포켓몬_마스터_이다솜-seungjoo.py
@@ -1,1 +1,20 @@
-# delete this and solve here
+import sys
+input = sys.stdin.readline
+from collections import defaultdict
+
+# 검색은 hash table
+N,M = map(int,input().split())
+poc_num = defaultdict(int)
+poc_name = defaultdict(str)
+
+for i in range(1,N+1):
+    a = input().rstrip()
+    poc_num[a] = i
+    poc_name[i] = a
+
+for _ in range(M):
+    b = input().rstrip()
+    if b.isdigit():
+        print(poc_name[int(b)])
+    else:
+        print(poc_num[b])

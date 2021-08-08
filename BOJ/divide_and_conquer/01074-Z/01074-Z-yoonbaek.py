20593b2a3f873889f6c00b3e938486dad9a76a81
YoonBaek <royal729@naver.com>
2021-08-08 11:42:49 +0900

    code: Solve boj 1074 Z (yoonbaek)

--- BOJ/divide_and_conquer/01074-Z/01074-Z-yoonbaek.py
+++ BOJ/divide_and_conquer/01074-Z/01074-Z-yoonbaek.py
@@ -1,1 +1,28 @@
-# delete this and solve here
+# solved by YoonBaek
+
+def z(n: int, r: int, c:int):
+    num = 0; quad = 0
+
+    while n:
+        half = 2**(n-1)
+
+        # choose quad place
+        if c < half and r < half : quad = 0
+        if c >= half and r < half : quad = 1
+        if c < half and r >= half : quad = 2
+        if c >= half and r >= half : quad = 3
+
+        # jump quad
+        num += half**2 * quad
+        c %= half; r %= half
+
+        # reduce scope
+        n-=1
+
+    return num
+
+
+if __name__ == "__main__":
+    n, r, c = map(int, input().split())
+
+    print(z(n,r,c))

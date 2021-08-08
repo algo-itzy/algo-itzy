56fd3e0e9b9c680d692d8eab091d41b9b54ca50c
chaselover <gkstmdwn234@gmail.com>
2021-08-08 18:46:41 +0900

    code: Solve boj 01764 듣보잡 (seungjoo)

--- BOJ/set-map_by_hashing/01764-듣보잡/01764-듣보잡-seungjoo.py
+++ BOJ/set-map_by_hashing/01764-듣보잡/01764-듣보잡-seungjoo.py
@@ -1,1 +1,18 @@
-# delete this and solve here
+import sys
+input = sys.stdin.readline
+
+N,M = map(int,input().split())
+not_hear_list = {input().rstrip():1 for _ in range(N)}
+not_human=[]
+for _ in range(M):
+    not_see = input().rstrip()
+    try:
+        if not_hear_list[not_see]:
+            not_human.append(not_see)
+    except:
+        continue
+# 사전순 출력을 위한 정렬
+not_human.sort()
+print(len(not_human))
+for human in not_human:
+    print(human)

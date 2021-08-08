48720a858235f57f6eb900b786a46f8fc42e59c6
seokzin <seokzin95@gmail.com>
2021-08-09 01:27:55 +0900

    code: Solve boj 01927 최소 힙 (seokzin)

--- BOJ/priority_queue/01927-최소힙/01927-최소 힙-seokzin.py
+++ BOJ/priority_queue/01927-최소힙/01927-최소 힙-seokzin.py
@@ -1,1 +1,17 @@
-# delete this and solve here
+import heapq
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+hq = []
+
+for _ in range(n):
+    x = int(input())
+
+    if x == 0:
+        print(heapq.heappop(hq) if hq else 0)
+    else:
+        heapq.heappush(hq, x)
+
+# 내용보단 우선순위 큐 개념이 중요했던 문제

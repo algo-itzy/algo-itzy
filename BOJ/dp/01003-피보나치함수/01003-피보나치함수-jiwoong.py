b53289a76e84ed684291e4e69c325d2279ff116f
서울_4반_정지웅 <j2woong1@gmail.com>
2021-08-10 00:23:05 +0900

    code: solve boj 01003 피보나치함수 (jiwoong)

--- BOJ/dp/01003-피보나치함수/01003-피보나치함수-jiwoong.py
+++ BOJ/dp/01003-피보나치함수/01003-피보나치함수-jiwoong.py
@@ -1,1 +1,17 @@
-# delete this and solve here
+# 0이 출력되는 횟수와 1이 출력되는 횟수를 공백으로 구분해서 출력
+import sys
+
+input = sys.stdin.readline
+
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    c1 = 0
+    c0 = 1
+    tmp = 0
+    for _ in range(n):
+        tmp = c1
+        c1 = c1 + c0
+        c0 = tmp
+    print(c0, c1)

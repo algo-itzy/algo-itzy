ef9cf93d860244d2d30ec081769d6d1e36d12b5e
Chae YeonHee <87457066+hing9u@users.noreply.github.com>
2021-08-09 23:07:22 +0900

    code: Solve boj 01003 피보나치 함수 (yeonhee)

--- BOJ/dp/01003-피보나치함수/01003-피보나치함수-yeonhee.py
+++ BOJ/dp/01003-피보나치함수/01003-피보나치함수-yeonhee.py
@@ -1,1 +1,18 @@
-# delete this and solve here
+import sys
+input = sys.stdin.readline
+
+for _ in range(int(input())):
+    zero = [1, 0]
+    one = [0, 1]
+    n = int(input())
+
+    for i in range(2, n+1):
+        zero.append(zero[i-1] + zero[i-2])
+        one.append(one[i-1] + one[i-2])
+
+    print(zero[n], one[n], sep=' ')
+
+"""
+결과 또한 피보나치 수열을 따르고 있어서, 피보나치 재귀 함수를 이용했더니 시간 초과가 났습니다.
+
+"""

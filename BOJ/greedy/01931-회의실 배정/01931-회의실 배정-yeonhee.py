9c652c9801adb7206de709fed6308d6f424ed37c
Chae YeonHee <87457066+hing9u@users.noreply.github.com>
2021-08-10 00:03:47 +0900

    code: Solve boj 01931 회의실 배정 (yeonhee)

--- BOJ/greedy/01931-회의실 배정/01931-회의실 배정-yeonhee.py
+++ BOJ/greedy/01931-회의실 배정/01931-회의실 배정-yeonhee.py
@@ -1,1 +1,20 @@
-# delete this and solve here
+import sys
+input = sys.stdin.readline
+
+time_list = [list(map(int, input().split())) for _ in range(int(input()))]
+time_list.sort(key=lambda x: (x[1], x[0]))
+
+end_time = 0
+cnt = 0
+
+for start, end in time_list:
+    if start >= end_time:
+        end_time = end
+        cnt += 1
+
+print(cnt)
+
+"""
+인프런 강의에 동일한 문제가 있고,
+코딩테스트에서도 한번 경험해 봤던 문제였습니다 :)
+"""

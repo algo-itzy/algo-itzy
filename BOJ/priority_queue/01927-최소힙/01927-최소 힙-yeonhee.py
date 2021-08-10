7cc17e704ad8d9cf1a748d8c936b5b816ca5ea02
Chae YeonHee <87457066+hing9u@users.noreply.github.com>
2021-08-10 21:23:44 +0900

    code: Solve boj 01927 최소 힙 (yeonhee)

--- BOJ/priority_queue/01927-최소힙/01927-최소 힙-yeonhee.py
+++ BOJ/priority_queue/01927-최소힙/01927-최소 힙-yeonhee.py
@@ -1,1 +1,19 @@
-# delete this and solve here
+import sys
+import heapq
+
+input = sys.stdin.readline
+heap = []
+
+for _ in range(int(input())):
+    x = int(input())
+
+    if x == 0:
+        print(heapq.heappop(heap) if heap else 0)
+    else:
+        heapq.heappush(heap, x)
+
+"""
+힙은 처음 접했네요.
+아래 링크가 도움이 되었습니다.
+https://www.daleseo.com/python-heapq/
+"""

ee92e604133df5feca06b097bbb21a77be98a344
Chae YeonHee <87457066+hing9u@users.noreply.github.com>
2021-08-10 23:58:25 +0900

    code: Solve boj 02606 바이러스 (yeonhee)

--- BOJ/dfs/02606-바이러스/02606-바이러스-yeonhee.py
+++ BOJ/dfs/02606-바이러스/02606-바이러스-yeonhee.py
@@ -1,1 +1,32 @@
-# delete this and solve here
+from collections import deque
+
+
+def bfs(n):
+    q = deque([n])
+    infected[n] = 1
+
+    while q:
+        x = q.popleft()
+
+        for w in graph[x]:
+            if not infected[w]:
+                q.append(w)
+                infected[w] = 1
+
+    return infected
+
+
+n = int(input())
+graph = [[] for _ in range(n+1)]
+infected = [0] * (n+1)
+
+for _ in range(int(input())):
+    c1, c2 = map(int, input().split())
+    graph[c1].append(c2)
+    graph[c2].append(c1)
+
+print(sum(bfs(1))-1)
+
+"""
+기본적인 bfs 문제
+"""

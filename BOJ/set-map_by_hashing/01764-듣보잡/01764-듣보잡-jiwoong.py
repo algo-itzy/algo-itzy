fb06a143e4f4597c4ec3641b8a70cac0f3c22de9
서울_4반_정지웅 <j2woong1@gmail.com>
2021-08-10 23:39:25 +0900

    code: solve boj 01764 듣보잡 (jiwoong)

--- BOJ/set-map_by_hashing/01764-듣보잡/01764-듣보잡-jiwoong.py
+++ BOJ/set-map_by_hashing/01764-듣보잡/01764-듣보잡-jiwoong.py
@@ -1,1 +1,28 @@
-# delete this and solve here
+"""
+# 듣보잡
+첫째 줄에 듣도 못한 사람의 수 N, 보도 못한 사람의 수 M
+둘째 줄부터 N개의 줄에 걸쳐 듣도 못한 사람의 이름과, N+2째 줄부터 보도 못한 사람의 이름이 순서대로
+
+듣보잡의 수와 그 명단을 사전순으로 출력
+"""
+
+import sys
+
+input = sys.stdin.readline
+n, m = map(int, input().split())
+
+listen = set() # 듣
+see = set() # 보
+
+for _ in range(n):
+    listen.add(input().strip())
+
+for _ in range(m):
+    see.add(input().strip())
+
+ans = sorted(list(listen & see))
+
+print(len(ans))
+
+for i in ans:
+    print(i)

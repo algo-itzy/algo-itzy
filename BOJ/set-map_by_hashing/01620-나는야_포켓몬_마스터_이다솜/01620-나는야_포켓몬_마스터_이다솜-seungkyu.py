cfb4e37a4f49193607d4ba91627ce876993ea377
ed-kyu <seungkyukim.dev@gmail.com>
2021-08-10 22:22:19 +0900

    code: Solve 01620 나는야 포켓몬 마스터 이다솜 (seungkyu)

--- BOJ/set-map_by_hashing/01620-나는야_포켓몬_마스터_이다솜/01620-나는야_포켓몬_마스터_이다솜-seungkyu.py
+++ BOJ/set-map_by_hashing/01620-나는야_포켓몬_마스터_이다솜/01620-나는야_포켓몬_마스터_이다솜-seungkyu.py
@@ -1,1 +1,23 @@
-# delete this and solve here
+import sys
+
+input = sys.stdin.readline
+N, M = map(int, input().split())
+
+# key가 이름일 때와 번호일 때 2가지 경우 모두 사용되므로 2개 dict 사용
+pokemon_key_num = {}
+pokemon_key_name = {}
+
+for i in range(1,N+1):
+    poke_name = input().strip()
+    # input 받아서 2개의 dict에 모두 넣어주기
+    pokemon_key_num[i] = poke_name
+    pokemon_key_name[poke_name] = i
+
+for _ in range(M):
+    test = input().strip()
+    # 원래는 str이므로 int 변환 시 오류나는지에 따라 구분
+    try:
+        test = int(test)
+        print(pokemon_key_num[test])
+    except:
+        print(pokemon_key_name[test])

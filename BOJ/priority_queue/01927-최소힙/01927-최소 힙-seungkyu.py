d361224a5d6b342be2c98bfaa06e04abee17c6ff
ed-kyu <seungkyukim.dev@gmail.com>
2021-08-11 23:05:09 +0900

    code: Solve boj 01927 최소 힙 (seungkyu)

--- BOJ/priority_queue/01927-최소힙/01927-최소 힙-seungkyu.py
+++ BOJ/priority_queue/01927-최소힙/01927-최소 힙-seungkyu.py
@@ -1,1 +1,21 @@
-# delete this and solve here
+import sys
+# 파이썬 리스트는 링크드리스트라 인덱스 접근이 느림(시간 초과뜸)
+import heapq  
+
+input = sys.stdin.readline
+N = int(input())
+
+min_heaps = []
+
+for _ in range(N):
+    num = int(input())
+
+    if num:
+        heapq.heappush(min_heaps, num)  # heapq.heappush(리스트, 넣을 숫자)
+    else:
+        
+        if min_heaps:
+            print(heapq.heappop(min_heaps))  # heapq.heappop(리스트)
+        else:
+            print(0)
+        

b40c5ff21670bbf74d33c27b0532732247a2ae80
YoonBaek <royal729@naver.com>
2021-08-12 01:01:13 +0900

    code: Solve boj 1764 듣보잡 (yoonbaek)

--- BOJ/set-map_by_hashing/01764-듣보잡/01764-듣보잡-yoonbaek.py
+++ BOJ/set-map_by_hashing/01764-듣보잡/01764-듣보잡-yoonbaek.py
@@ -1,1 +1,27 @@
-# delete this and solve here
+# solved by YoonBaek 
+
+# hash를 이용한 풀이
+# 단어 정렬에만 주의하면 됐던 문제
+
+import sys
+
+rd = sys.stdin.readline
+wr = sys.stdout.write
+
+if __name__ == "__main__":
+    N, M = map(int, rd().split())
+    dbj = {}
+
+    cnt, result = 0, []
+    for _ in range(N+M):
+        name = rd().rstrip()
+        if dbj.get(name):
+            cnt += 1
+            result.append(name)
+        else:
+            dbj[name] = 1
+
+    result.sort()
+
+    wr(f"{cnt}\n")
+    print(*result, sep = "\n")

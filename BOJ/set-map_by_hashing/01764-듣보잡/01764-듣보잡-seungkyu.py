53136f7a6719df73cc2ef4cfa367d8e3322ebbb7
ed-kyu <seungkyukim.dev@gmail.com>
2021-08-12 02:38:16 +0900

    code: Solve boj 01764 듣보잡 (seungkyu)

--- BOJ/set-map_by_hashing/01764-듣보잡/01764-듣보잡-seungkyu.py
+++ BOJ/set-map_by_hashing/01764-듣보잡/01764-듣보잡-seungkyu.py
@@ -1,1 +1,23 @@
-# delete this and solve here
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+
+person_d = {}  # 듣도 못한 사람 저장
+person_both = []  # 두 경우 모두 해당할 때만 저장
+
+for i in range(N):
+    person_d[input().strip()] = 1
+
+for _ in range(M):
+    person = input().strip()
+
+    if person in person_d:  # 두 경우 모두 해당될 때
+        person_both.append(person)  # 저장
+        
+person_both.sort()  # 사전 순으로 정렬
+
+# 답 출력
+print(len(person_both))
+for person in person_both:
+    print(person)

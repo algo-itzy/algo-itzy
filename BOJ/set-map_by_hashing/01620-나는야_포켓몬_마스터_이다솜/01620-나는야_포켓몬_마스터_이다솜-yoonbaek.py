aab444a957529e5a16b88457383cb9ba3736f797
YoonBaek <royal729@naver.com>
2021-08-11 10:31:27 +0900

    code: Solve boj 1620 나는야 포켓몬 마스터 이다솜 (yoonbaek)

--- BOJ/set-map_by_hashing/01620-나는야_포켓몬_마스터_이다솜/01620-나는야_포켓몬_마스터_이다솜-yoonbaek.py
+++ BOJ/set-map_by_hashing/01620-나는야_포켓몬_마스터_이다솜/01620-나는야_포켓몬_마스터_이다솜-yoonbaek.py
@@ -1,1 +1,20 @@
-# delete this and solve here
+# solved by YoonBaek
+# 어차피 숫자 이름인 포켓몬은 없다고 생각해서,
+# 그냥 key, val을 반전시켜서 Pokedex에 저장했습니다.
+from sys import stdin, stdout
+
+rd = stdin.readline
+wr = stdout.write
+
+if __name__ == "__main__":
+    N, M = map(int, rd().split())
+
+    Pokedex = {}
+    for i in range(1, N+1):
+        name = rd().rstrip()
+        Pokedex[name] = i
+        Pokedex[str(i)] = name
+
+    for _ in range(M):
+        query = rd().rstrip()
+        wr(f"{Pokedex[query]}\n")

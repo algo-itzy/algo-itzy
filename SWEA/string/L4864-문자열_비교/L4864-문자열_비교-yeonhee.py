763412368f41ee26edd44dc74086dd2447c2c045
Chae YeonHee <87457066+hing9u@users.noreply.github.com>
2021-08-12 23:43:34 +0900

    code: Solve swea L4864 문자열 비교 (yeonhee)

--- SWEA/string/L4864-문자열_비교/L4864-문자열_비교-yeonhee.py
+++ SWEA/string/L4864-문자열_비교/L4864-문자열_비교-yeonhee.py
@@ -1,1 +1,9 @@
-# delete this and solve here
+import sys
+sys.stdin = open('input.txt')
+
+for t in range(1, int(input())+1):
+    str1 = input()
+    str2 = input()
+
+    print(f'#{t}', end=' ')
+    print(1 if str1 in str2 else 0)

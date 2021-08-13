d139f6122183631745db55e667b860a4c793408a
seokzin <seokzin95@gmail.com>
2021-08-13 15:42:03 +0900

    code: Solve swea L4864 문자열 비교 (seokzin)

--- SWEA/string/L4864-문자열_비교/L4864-문자열_비교-seokzin.py
+++ SWEA/string/L4864-문자열_비교/L4864-문자열_비교-seokzin.py
@@ -1,1 +1,7 @@
-# delete this and solve here
+T = int(input())
+
+for tc in range(1, T+1):
+    str1 = input()  # short str
+    str2 = input()  # long str
+    
+    print(f"#{tc}", 1 if str2.find(str1) > 0 else 0)

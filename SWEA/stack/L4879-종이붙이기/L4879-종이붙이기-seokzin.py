b3513fe037eb31224e359be170a3e26ebf6eea85
seokzin <seokzin95@gmail.com>
2021-08-13 15:43:09 +0900

    code: Solve swea L4879 종이붙이기 (seokzin)

--- SWEA/stack/L4879-종이붙이기/L4879-종이붙이기-seokzin.py
+++ SWEA/stack/L4879-종이붙이기/L4879-종이붙이기-seokzin.py
@@ -1,1 +1,13 @@
-# delete this and solve here
+T = int(input())
+
+for tc in range(1, T+1):
+    N = int(input())
+
+    dp = [1, 3]
+
+    for i in range(2, N//10):
+        dp.append(dp[i-1] + 2*dp[i-2])
+
+    print(f"#{tc} {dp[-1]}")
+
+# 규칙을 찾아 점화식을 구현하는 문제

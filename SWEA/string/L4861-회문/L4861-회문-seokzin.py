e392d2a902b2df181768fdc3accbcc811e19589f
seokzin <seokzin95@gmail.com>
2021-08-13 15:41:36 +0900

    code: Solve swea L4861 회문 (seokzin)

--- SWEA/string/L4861-회문/L4861-회문-seokzin.py
+++ SWEA/string/L4861-회문/L4861-회문-seokzin.py
@@ -1,1 +1,29 @@
-# delete this and solve here
+T = int(input())
+
+for tc in range(1, T+1):
+    N, M = map(int, input().split())
+    table = []
+
+    for _ in range(N):
+        table.append(input())
+
+    # 가로 탐색
+    for y in range(N):
+        for x in range(N-M+1):
+            row = table[y][x:M+x]  
+            
+            if row == row[::-1]:  
+                print(f"#{tc}", row)
+
+
+    # 세로 탐색
+    for y in range(N-M+1):
+        for x in range(N):
+            col = []
+            for i in range(M):
+                col.append(table[y+i][x])
+
+            if col == col[::-1]:  
+                print(f"#{tc} ", *col, sep='')
+
+# 무식하게 검증 했습니다.

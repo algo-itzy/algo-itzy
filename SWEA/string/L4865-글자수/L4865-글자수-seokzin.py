e8cc92b12c99581cfc222f25909226908b823d1a
seokzin <seokzin95@gmail.com>
2021-08-13 15:42:30 +0900

    code: Solve swea L4865 글자수 (seokzin)

--- SWEA/string/L4865-글자수/L4865-글자수-seokzin.py
+++ SWEA/string/L4865-글자수/L4865-글자수-seokzin.py
@@ -1,1 +1,14 @@
-# delete this and solve here
+T = int(input())
+
+for tc in range(1, T+1):
+    str1 = input()
+    str2 = input()
+    
+    dic = {}
+
+    for c in set(str1):
+        dic[c] = str2.count(c)
+
+    print(f'#{tc}', max(dic.values()))
+
+# 딕셔너리를 활용하래서 해봅니다.

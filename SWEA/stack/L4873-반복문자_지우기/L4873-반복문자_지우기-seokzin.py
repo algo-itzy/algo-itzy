680f309298ebdae7253148e99c9972430f365e0d
seokzin <seokzin95@gmail.com>
2021-08-13 15:44:07 +0900

    code: Solve swea L4873 반복문자 지우기 (seokzin)

--- SWEA/stack/L4873-반복문자_지우기/L4873-반복문자_지우기-seokzin.py
+++ SWEA/stack/L4873-반복문자_지우기/L4873-반복문자_지우기-seokzin.py
@@ -1,1 +1,13 @@
-# delete this and solve here
+T = int(input())
+
+for tc in range(1, T+1):
+    s = input()
+    stack = []
+
+    for c in s:
+        if stack and stack[-1] == c:
+            stack.pop()
+        else:
+            stack.append(c)
+
+    print(f'#{tc}', len(stack))

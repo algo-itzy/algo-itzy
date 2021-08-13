86b0b8a80d572b3e69b224c821fea004fef56b26
seokzin <seokzin95@gmail.com>
2021-08-13 15:43:41 +0900

    code: Solve swea L4871 그래프 경로 (seokzin)

--- SWEA/stack/L4871-그래프_경로/L4871-그래프_경로-seokzin.py
+++ SWEA/stack/L4871-그래프_경로/L4871-그래프_경로-seokzin.py
@@ -1,1 +1,32 @@
-# delete this and solve here
+def dfs(x):
+    global res
+
+    visit[x] = 1
+
+    if x == G:
+        res = 1
+        return
+
+    for nx in graph[x]:
+        if not visit[nx]:
+            dfs(nx)
+
+T = int(input())
+for tc in range(1, T+1):
+    V, E = map(int, input().split())
+    graph = [[] for _ in range(V+1)]
+    visit = [0] * (V+1)
+    res = 0
+
+    for i in range(E):
+        s, e = map(int, input().split())
+        graph[s].append(e)
+
+    S, G = map(int, input().split())
+
+    dfs(S)
+
+    print(f'#{tc} {res}')
+
+# DFS의 return 처리는 어렵네요. 재귀를 벗어나면서 return이 계속 반복돼서..
+# 그래서 global 변수를 어쩔 수 없이 썼습니다.

c1d5ff463117cd73c840495b5a6d309e7ea7bbcf
seokzin <seokzin95@gmail.com>
2021-08-13 15:40:50 +0900

    code: Solve swea L4866 괄호검사 (seokzin)

--- SWEA/stack/L4866-괄호검사/L4866-괄호검사-seokzin.py
+++ SWEA/stack/L4866-괄호검사/L4866-괄호검사-seokzin.py
@@ -1,1 +1,27 @@
-# delete this and solve here
+def is_vps(s):
+    stack = [] # 괄호 담을 스택
+
+    for c in s:
+        if c == '(':
+            stack.append('(')
+        elif c == '{':
+            stack.append('{')
+        elif c == ')':
+            if stack and stack[-1] == '(':
+                stack.pop()
+            else:
+                return 0
+        elif c == '}':
+            if stack and stack[-1] == '{':
+                stack.pop()
+            else:
+                return 0
+    
+    return 1 if not stack else 0
+
+T = int(input())
+
+for tc in range(1, T+1):
+    print(f"#{tc}", is_vps(input()))
+
+# 백준 9012 괄호 풀이가 생각났습니다.

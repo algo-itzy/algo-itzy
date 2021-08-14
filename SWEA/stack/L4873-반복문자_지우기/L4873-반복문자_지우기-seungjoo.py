954dbe71e6368f859ec614ec60f6ce2d0afc5c6e
chaselover <gkstmdwn234@gmail.com>
2021-08-15 03:25:19 +0900

    code: Solve swea L4873 반복문자_지우기 (seungjoo)

--- SWEA/stack/L4873-반복문자_지우기/L4873-반복문자_지우기-seungjoo.py
+++ SWEA/stack/L4873-반복문자_지우기/L4873-반복문자_지우기-seungjoo.py
@@ -1,1 +1,21 @@
-# delete this and solve here
+
+def repeat_str(s):
+    init = len(s)
+    stack = []
+    cnt = 0
+    while s:
+        pre = s.pop()
+        if stack:
+            if pre == stack[-1]:
+                stack.pop()
+                cnt += 1
+            else:
+                stack.append(pre)
+        else:
+            stack.append(pre)
+    return init - 2*cnt
+
+
+for test in range(1,int(input())+1):
+    s = list(input())
+    print(f'#{test} {repeat_str(s)}')

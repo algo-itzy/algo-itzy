a912b2dcc9759e2660b32382894c16828d72de34
chaselover <gkstmdwn234@gmail.com>
2021-08-15 01:56:03 +0900

    code: Solve swea L4879 종이붙이기  (seungjoo)

--- SWEA/string/L4865-글자수/L4865-글자수-seungjoo.py
+++ SWEA/string/L4865-글자수/L4865-글자수-seungjoo.py
@@ -3,11 +3,11 @@ input = sys.stdin.readline
 from collections import defaultdict
 
 def count_chrs(s1, s2):
-    char_set = defaultdict(int)
+    char_set = defaultdict(int) # deault값 입력을 자동화하기 위해 defaultdict사용했습니다
     for chr in s2:
         char_set[chr] += 1
     max_cnt = 0
-    for char in s1:
+    for char in s1: # s1문자열에 들어있는 문자의 수를 세줬습니다
         max_cnt = max(max_cnt,char_set[char])
     return max_cnt
 

1b85deba8ff739980c4e2fc3fd308c49dfdab826
Chae YeonHee <87457066+hing9u@users.noreply.github.com>
2021-08-14 16:30:15 +0900

    code: Solve swea L4879 종이붙이기 (yeonhee)

--- SWEA/stack/L4879-종이붙이기/L4879-종이붙이기-yeonhee.py
+++ SWEA/stack/L4879-종이붙이기/L4879-종이붙이기-yeonhee.py
@@ -1,1 +1,14 @@
-# delete this and solve here
+import sys
+sys.stdin = open('input.txt')
+
+
+def solution(n):
+    if n == 1:
+        return 1
+    # 결과가 [1, 3, 5, 11, 21, 43, 85, ...]
+    return 2*solution(n-1) - 1 if n%2 else 2*solution(n-1) + 1
+
+
+for t in range(1, int(input())+1):
+    n = int(input())
+    print(f'#{t} {solution(n//10)}')

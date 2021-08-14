df47cbc7b5eb99e07655afb72e61c6b816a61af5
ed-kyu <seungkyukim.dev@gmail.com>
2021-08-14 23:23:43 +0900

    code: Solve swea L4865 글자수 (seungkyu)

--- SWEA/string/L4865-글자수/L4865-글자수-seungkyu.py
+++ SWEA/string/L4865-글자수/L4865-글자수-seungkyu.py
@@ -1,1 +1,23 @@
-# delete this and solve here
+import sys
+sys.stdin = open('sample_input.txt')
+
+T = int(input())
+
+for test_case in range(1, T+1):
+
+    N = input()
+    M = input()
+
+    word_dict = {}  # M 한바퀴 돌면서 딕셔너리 생성
+    for char in M:
+        if char not in word_dict:
+            word_dict[char] = 1
+        else:
+            word_dict[char] += 1
+
+    max_num = 0
+    for char in N:  # N 글자를 한바퀴 돌면서 max 찾기
+        if max_num < word_dict[char]:
+            max_num = word_dict[char]
+
+    print(f'#{test_case} {max_num}')

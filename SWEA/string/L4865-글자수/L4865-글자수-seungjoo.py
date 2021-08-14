048439a2fb224e62a96e2c1d61b58e0ad8ae9e87
chaselover <gkstmdwn234@gmail.com>
2021-08-15 00:55:38 +0900

    code: Solve swea L4865 글자수 (seungjoo)

--- SWEA/string/L4865-글자수/L4865-글자수-seungjoo.py
+++ SWEA/string/L4865-글자수/L4865-글자수-seungjoo.py
@@ -1,1 +1,18 @@
-# delete this and solve here
+import sys
+input = sys.stdin.readline
+from collections import defaultdict
+
+def count_chrs(s1, s2):
+    char_set = defaultdict(int)
+    for chr in s2:
+        char_set[chr] += 1
+    max_cnt = 0
+    for char in s1:
+        max_cnt = max(max_cnt,char_set[char])
+    return max_cnt
+
+    
+for test in range(1,int(input())+1):
+    str1 = input().rstrip()
+    str2 = input().rstrip()
+    print(f'#{test} {count_chrs(str1, str2)}')

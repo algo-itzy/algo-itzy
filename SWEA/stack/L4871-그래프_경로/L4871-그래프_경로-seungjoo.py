9fc4a56edc629e01c672f1865a32d7e69ddea941
chaselover <gkstmdwn234@gmail.com>
2021-08-15 03:11:30 +0900

    code: Solve swea L4871 그래프_경로 (seungjoo)

--- SWEA/stack/L4871-그래프_경로/L4871-그래프_경로-seungjoo.py
+++ SWEA/stack/L4871-그래프_경로/L4871-그래프_경로-seungjoo.py
@@ -1,1 +1,27 @@
-# delete this and solve here
+
+def find_path(s,g):
+    global answer
+    if s==g:
+        answer = 1
+        return
+    for next_node in graph[s]:
+        if not visited[next_node]:
+            visited[next_node] = True
+            find_path(next_node,g)
+
+
+
+for test in range(1,int(input())+1):
+    V, E = map(int,input().split())
+    graph = {i:[] for i in range(1,V+1)}
+    answer = 0
+    visited = {i:False for i in range(1,V+1)}
+    for _ in range(E):
+        a, b = map(int,input().split())
+        graph[a] += [b]
+    s,g = map(int,input().split())
+    visited[s] = True
+    find_path(s,g)
+            
+    print(f'#{test} {answer}')
+

24c704e0847998481ba0144de244daa11a955a32
chaselover <gkstmdwn234@gmail.com>
2021-08-15 14:58:50 +0900

    code: Solve swea 5356 의석이의_세로로_말해요 (seungjoo)

--- SWEA/implementation/5356-의석이의_세로로_말해요/5356-의석이의_세로로_말해요-seungjoo.py
+++ SWEA/implementation/5356-의석이의_세로로_말해요/5356-의석이의_세로로_말해요-seungjoo.py
@@ -1,1 +1,15 @@
-# delete this and solve here
+
+def read_vertical(s):
+    answer = ''
+    cnt = 0
+    while cnt <= 15:
+        for string in s:
+            if string:
+                answer += string.pop(0)
+        cnt+=1
+    return answer
+
+
+for test in range(1, int(input())+1):
+    strings = list(list(input()) for _ in range(5))
+    print(f'#{test} {read_vertical(strings)}')

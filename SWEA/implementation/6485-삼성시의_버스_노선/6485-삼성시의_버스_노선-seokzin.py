f90093a5c9498dd67e4f87962dfb78b9cefb98d0
seokzin <seokzin95@gmail.com>
2021-08-15 20:23:24 +0900

    code: Solve swea 6485 삼성시의 버스 노선 (seokzin)

--- SWEA/implementation/6485-삼성시의_버스_노선/6485-삼성시의_버스_노선-seokzin.py
+++ SWEA/implementation/6485-삼성시의_버스_노선/6485-삼성시의_버스_노선-seokzin.py
@@ -1,1 +1,19 @@
-# delete this and solve here
+T = int(input())
+
+for tc in range(1, T+1):
+    N = int(input())
+    arr = [0] * 5001
+    res = []
+
+    for _ in range(N):
+        a, b = map(int, input().split())
+
+        for i in range(a, b+1):
+            arr[i] += 1
+
+    for _ in range(int(input())):
+        res.append(arr[int(input())])
+
+    print(f"#{tc}", *res)
+
+# 색칠하기 문제처럼 arr에 카운트를 누적했습니다.

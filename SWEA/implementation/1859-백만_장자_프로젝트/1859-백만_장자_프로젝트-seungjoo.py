a9d853bbc4b8c170b1cd0cc742c0fc291beb36c0
chaselover <gkstmdwn234@gmail.com>
2021-08-15 16:09:01 +0900

    code: Solve swea 1859 백만_장자_프로젝트 (seungjoo)

--- SWEA/implementation/1859-백만_장자_프로젝트/1859-백만_장자_프로젝트-seungjoo.py
+++ SWEA/implementation/1859-백만_장자_프로젝트/1859-백만_장자_프로젝트-seungjoo.py
@@ -1,1 +1,14 @@
-# delete this and solve here
+
+
+for test in range(1,int(input())+1):
+    N = int(input())
+    prices = list(map(int, input().split()))
+    answer = 0
+    max_price = 0 
+    for j in range(N-1,-1,-1):
+        if max_price > prices[j]:
+            answer += max_price - prices[j]
+        else:
+            max_price = prices[j]
+
+    print(f'#{test} {answer}')

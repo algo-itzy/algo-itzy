a4ac0299ea5caabe2d390f985d1c5583b6f3d695
seokzin <seokzin95@gmail.com>
2021-08-15 20:15:10 +0900

    code: Solve swea 1859 백만 장자 프로젝트 (seokzin)

--- SWEA/implementation/1859-백만_장자_프로젝트/1859-백만_장자_프로젝트-seokzin.py
+++ SWEA/implementation/1859-백만_장자_프로젝트/1859-백만_장자_프로젝트-seokzin.py
@@ -1,1 +1,18 @@
-# delete this and solve here
+T = int(input())
+
+for tc in range(1, T+1):
+    N = int(input())
+    costs = list(map(int, input().split()))
+    top = costs[-1]
+    res = 0
+
+    for i in range(len(costs)-1, -1, -1):
+        if top > costs[i]:
+            res += top-costs[i]
+        else:
+            top = costs[i]
+
+    print(f"#{tc} {res}")
+
+# 처음엔 매번 최대값을 갱신해 앞에서 풀었으나 시간초과입니다...
+# 그냥 시중에 있는 역탐색으로 풀었습니다. 아쉽

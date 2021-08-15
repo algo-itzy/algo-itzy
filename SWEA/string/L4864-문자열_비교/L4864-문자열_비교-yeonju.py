d1b58be55f27b0bcaeff9ff3c7b85793d2587654
위연주 <thizzyzz@naver.com>
2021-08-15 10:51:46 +0900

    code: solve swea L4864 문자열 비교 (yeonju)

--- SWEA/string/L4864-문자열_비교/L4864-문자열_비교-yeonju.py
+++ SWEA/string/L4864-문자열_비교/L4864-문자열_비교-yeonju.py
@@ -1,1 +1,17 @@
-# delete this and solve here
+T = int(input())        #  테스트 케이스의 개수 T
+
+for tc in range(T):
+    N = input()         # str1 ab
+    M = input()         # str2 abcdefg
+    ans = 0 
+    for i in range(len(M) - len(N) +1):
+        for j in range(len(N)):
+            if M[i+j] != N[j]:
+                break   # 앞에서부터 한글자라도 틀리면 문자 비교 멈추기
+        else:           # for .. else : for 문 다 돌고나서 else 로 
+            ans = 1     # 있는지 없는지만 확인하면 되기에, 하나 찾고나서 바로 break 
+            break
+
+    print(f'#{tc+1} {ans}')
+
+

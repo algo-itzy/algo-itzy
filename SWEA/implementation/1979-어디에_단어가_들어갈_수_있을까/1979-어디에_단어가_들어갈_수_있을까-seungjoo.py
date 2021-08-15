ecaeb7e8577ffae0f6664585ba4e7b2fcd89e478
chaselover <gkstmdwn234@gmail.com>
2021-08-15 14:38:56 +0900

    code: Solve swea 1979 어디에_단어가_들어갈_수_있을까 (seungjoo)

--- SWEA/implementation/1979-어디에_단어가_들어갈_수_있을까/1979-어디에_단어가_들어갈_수_있을까-seungjoo.py
+++ SWEA/implementation/1979-어디에_단어가_들어갈_수_있을까/1979-어디에_단어가_들어갈_수_있을까-seungjoo.py
@@ -1,1 +1,34 @@
-# delete this and solve here
+def count_row(n, target, puzzle):
+    sum_seat = 0
+    for row in range(n):
+        cnt = 0
+        cells = []
+        for column in range(n):
+            if puzzle[row][column]:
+                cnt += 1
+            else:
+                if cnt:
+                    cells.append(cnt)
+                cnt = 0
+        cells.append(cnt)
+        sum_seat += cells.count(target)
+    return sum_seat
+
+
+def find_seat(size, length):
+    n = size
+    l = length
+    answer = 0
+
+    puzzle = [list(map(int, input().split())) for _ in range(n)]
+    answer += count_row(n, l, puzzle)
+
+    new_puzzle = [k for k in zip(*puzzle)]
+    answer += count_row(n, l, new_puzzle)
+
+    return answer
+
+
+for test in range(1, int(input())+1):
+    N, K = map(int, input().split())
+    print(f'#{test} {find_seat(N, K)}')

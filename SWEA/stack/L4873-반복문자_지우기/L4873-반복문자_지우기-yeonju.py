8995c49cea8e23251b71afd1ff684d66f2dee78b
위연주 <thizzyzz@naver.com>
2021-08-15 10:21:54 +0900

    code: Solve SWEA L4873 반복문자 지우기(yeonju)

--- SWEA/stack/L4873-반복문자_지우기/L4873-반복문자_지우기-yeonju.py
+++ SWEA/stack/L4873-반복문자_지우기/L4873-반복문자_지우기-yeonju.py
@@ -1,1 +1,19 @@
-# delete this and solve here
+t = int(input())
+
+for tc in range(t):
+    st = input()
+    
+    li =[] 
+
+    for i in st:
+        if li and i == li[-1]:  # 담겨있는 문자와 담으려는 문자가 같으면 제거
+            li.pop()
+        else:
+            li.append(i)
+            
+
+
+  
+    print(f'#{tc+1} {len(li)}'}
+
+   

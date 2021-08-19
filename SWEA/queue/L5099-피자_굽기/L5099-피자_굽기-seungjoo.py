735aa81221bdd7347031de19c27d16c51a5854f6
chaselover <gkstmdwn234@gmail.com>
2021-08-20 01:58:24 +0900

    code: solve SWEA 5099 피자 굽기 (seungjoo)

--- SWEA/queue/L5099-피자_굽기/L5099-피자_굽기-seungjoo.py
+++ SWEA/queue/L5099-피자_굽기/L5099-피자_굽기-seungjoo.py
@@ -1,1 +1,22 @@
-# delete this and solve here
+from collections import deque
+
+for test in range(1,int(input())+1):
+    N, M = map(int, input().split())
+    pizzas = list(map(int,input().split()))
+    pizzas_idx = [[pizza,idx+1] for idx, pizza in enumerate(pizzas)]
+    oven = deque()
+    # 오븐의 크기 N
+    for pizza in pizzas_idx:
+        oven.append(pizza)
+        while len(oven) == N:
+            oven.rotate(-1)
+            oven[-1][0] //=2
+            if not oven[-1][0]:
+                oven.pop()
+    while len(oven)>1:
+        oven.rotate(-1)
+        oven[-1][0] //=2
+        if not oven[-1][0]:
+            oven.pop()
+    answer = oven[-1][1]
+    print(f'#{test} {answer}')

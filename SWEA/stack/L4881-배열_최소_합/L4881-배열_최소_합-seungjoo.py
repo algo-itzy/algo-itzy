b944379e35597db16dbeb4c1909d7392fa714be4
chaselover <gkstmdwn234@gmail.com>
2021-08-20 01:15:07 +0900

    code: solve SWEA 4881 배열_최소_합 (seungjoo)

--- SWEA/stack/L4881-배열_최소_합/L4881-배열_최소_합-seungjoo.py
+++ SWEA/stack/L4881-배열_최소_합/L4881-배열_최소_합-seungjoo.py
@@ -1,1 +1,22 @@
-# delete this and solve here
+
+def DFS(row,sum_nums):
+    global min_sum
+    if row == N:
+        min_sum = min(min_sum,sum_nums)
+        return
+    if sum_nums > min_sum:
+        return
+    for i in range(N):
+        if not col_check[i]:
+            col_check[i] = True
+            DFS(row+1,sum_nums+matrix[row][i])
+            col_check[i] = False
+
+
+for test in range(1,int(input())+1):
+    N = int(input())
+    matrix = [list(map(int,input().split())) for _ in range(N)]
+    min_sum = float('inf')
+    col_check = [False] * N
+    DFS(0,0)
+    print(f'#{test} {min_sum}')

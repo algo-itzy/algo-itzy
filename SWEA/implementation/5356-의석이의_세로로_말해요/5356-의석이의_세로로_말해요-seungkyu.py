47e5361b5e92b004307ed0474f80ba2c50930671
ed-kyu <seungkyukim.dev@gmail.com>
2021-08-19 12:12:08 +0900

    code: Solve swea 5356 의석이의 세로로 말해요 (seungkyu)

--- SWEA/implementation/5356-의석이의_세로로_말해요/5356-의석이의_세로로_말해요-seungkyu.py
+++ SWEA/implementation/5356-의석이의_세로로_말해요/5356-의석이의_세로로_말해요-seungkyu.py
@@ -1,1 +1,21 @@
-# delete this and solve here
+import sys
+sys.stdin = open('sample_input 2.txt')
+
+T = int(input())
+
+for test_case in range(1, T+1):
+    words = []
+    for _ in range(5):
+        words += input().split()
+
+    words_t = [[] for _ in range(15)]  # 제일 긴 길이가 15라고 해서 배열을 만들었습니다.
+    
+    for word in words:
+        for idx, chr in enumerate(word):  # ex) 4글자이면 words_t[4]에 저장되도록
+            words_t[idx] += chr
+    
+    
+    print(f'#{test_case}', end=' ')
+    for words in words_t:
+        print(*words, sep='' ,end='')
+    print()

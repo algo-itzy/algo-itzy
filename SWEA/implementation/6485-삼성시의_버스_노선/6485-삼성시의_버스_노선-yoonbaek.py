47d0d1d7d75d06c69abfd5b3e060c9ca7144f060
YoonBaek <royal729@naver.com>
2021-08-19 17:44:32 +0900

    code: Solve swea 6485 삼성시의 버스 노선 (yoonbaek)

--- SWEA/implementation/6485-삼성시의_버스_노선/6485-삼성시의_버스_노선-yoonbaek.py
+++ SWEA/implementation/6485-삼성시의_버스_노선/6485-삼성시의_버스_노선-yoonbaek.py
@@ -1,1 +1,25 @@
-# delete this and solve here
+# solved by YoonBaek
+
+if __name__ == "__main__":
+    T = int(input())
+
+    for tc in range(1, T+1):
+        N = int(input())
+
+        # 정류장 리스트 생성
+        routes = [0] * (5000+1)
+        # 정류장별 버스 수++
+        for _ in range(N):
+            start, end = map(int, input().split())
+            for i in range(start, end+1):
+                routes[i] += 1
+        
+        P = int(input())
+        ans = []
+        
+        print(f"#{tc}", end=" ")
+        for _ in range(P):
+            j = int(input())
+            ans.append(routes[j])
+        # 출력할 게 많으므로 한 번에 출력
+        print(*ans)

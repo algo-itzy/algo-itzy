0e23a9a1a04555e476b47fd68dee4cc51b1eb642
chaselover <gkstmdwn234@gmail.com>
2021-08-20 01:00:32 +0900

    code: solve SWEA 4880 토너먼트_카드게임 (seungjoo)

--- SWEA/stack/L4880-토너먼트_카드게임/L4880-토너먼트_카드게임-seungjoo.py
+++ SWEA/stack/L4880-토너먼트_카드게임/L4880-토너먼트_카드게임-seungjoo.py
@@ -1,1 +1,33 @@
-# delete this and solve here
+
+def rock_siccor_paper(fights):
+    length = len(fights)
+    if length==1:
+        return fights[0]
+    elif length==2:
+        if fights[0][1]==1:
+            if fights[1][1]==1 or fights[1][1]==3:
+                return fights[0]
+            elif fights[1][1]==2:
+                return fights[1]
+        elif fights[0][1]==2:
+            if fights[1][1]==1 or fights[1][1]==2:
+                return fights[0]
+            elif fights[1][1]==3:
+                return fights[1]
+        else:
+            if fights[1][1]==2 or fights[1][1]==3:
+                return fights[0]
+            elif fights[1][1]==1:
+                return fights[1]
+    else:
+        return rock_siccor_paper([rock_siccor_paper(fights[:(length+1)//2]),rock_siccor_paper(fights[(length+1)//2:])])
+
+
+
+for test in range(1,int(input())+1):
+    N = int(input())
+    cards = list(map(int, input().split()))
+    cards_idx = [[idx+1,cards] for idx, cards in enumerate(cards)]
+    # 1가위, 2 바위 3 보
+    winner, number = rock_siccor_paper(cards_idx)
+    print(f'#{test} {winner}')

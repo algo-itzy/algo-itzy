69df839a0b4b18b23a889a7d5e911faacac4e4f8
YoonBaek <royal729@naver.com>
2021-08-19 16:21:47 +0900

    Solve swea 1859 백만 장자 프로젝트 (yoonbaek)

--- SWEA/implementation/1859-백만_장자_프로젝트/1859-백만_장자_프로젝트-yoonbaek.py
+++ SWEA/implementation/1859-백만_장자_프로젝트/1859-백만_장자_프로젝트-yoonbaek.py
@@ -1,1 +1,24 @@
-# delete this and solve here
+# solved by YoonBaek
+if __name__ == "__main__":
+    T = int(input())
+
+    for tc in range(1, T+1):
+        N = int(input())
+        profits = 0
+
+        prices = list(map(int, input().split()))
+        len_prices = len(prices)
+
+        while prices:
+            max_idx = max_price = 0
+            
+            for idx, price in enumerate(prices):
+                if price > max_price:
+                    max_idx, max_price = idx, price
+
+            for idx in range(max_idx):
+                profits += max_price - prices[idx]
+            
+            prices = prices[max_idx+1:]
+
+        print(f"#{tc} {profits}")

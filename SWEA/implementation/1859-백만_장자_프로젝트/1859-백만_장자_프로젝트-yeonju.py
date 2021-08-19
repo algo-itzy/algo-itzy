2f5bf6b97500ecf8f37a36327c4551d75e61da28
위연주 <thizzyzz@naver.com>
2021-08-19 18:12:22 +0900

    code: solve swea 6485 백만 장자 프로젝트 (yeonju)

--- SWEA/implementation/1859-백만_장자_프로젝트/1859-백만_장자_프로젝트-yeonju.py
+++ SWEA/implementation/1859-백만_장자_프로젝트/1859-백만_장자_프로젝트-yeonju.py
@@ -1,1 +1,39 @@
-# delete this and solve here
+t = int(input())
+
+for tc in range(t):
+    n = int(input())
+    price = list(map(int, input().split()))
+
+    cnt = 0
+    maximum = price[-1]             # 맨 뒤의 값을 최댓값으로  설정
+    for i in range(n-1, -1, -1):    # 뒤에서부터 앞으로 확인해오기
+        if price[i] > maximum:      # 최댓값 갱신
+            maximum = price[i]
+        else:
+            cnt += maximum - price[i] # 차익 계산 
+
+    print(f'#{tc+1} {cnt}')
+
+
+
+# 아래 풀이는 통과가 안 되네요,, 메모리를 많이 차지해서 그런지 ㅇ_0
+# t = int(input())
+#
+# for tc in range(t):
+#
+#     n = int(input())
+#
+#     price = list(map(int, input().split()))
+#     order_price = sorted(price, reverse = True)
+#     # print(order_price)
+#
+#     profit = 0
+#
+#     for i in range(0,n):
+#         for j in range(0,n):
+#             if price[i] < order_price[j] and price.index(order_price[j]) > i:
+#                 profit += order_price[j] - price[i]
+#                 break
+#
+#     print(f'#{tc+1} {profit}')
+#

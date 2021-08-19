82a52876e9669f31784bfed54f0777fc388f86ad
YoonBaek <royal729@naver.com>
2021-08-19 17:43:49 +0900

    code: Solve swea 5356 의석이의_세로로_말해요(yoonbaek)

--- SWEA/implementation/5356-의석이의_세로로_말해요/5356-의석이의_세로로_말해요-yoonbaek.py
+++ SWEA/implementation/5356-의석이의_세로로_말해요/5356-의석이의_세로로_말해요-yoonbaek.py
@@ -1,1 +1,15 @@
-# delete this and solve here
+from itertools import zip_longest
+
+if __name__ == "__main__":
+    T = int(input())
+
+    for tc in range(1, T+1):
+        arr = [input() for _ in range(5)]
+
+        print(f"#{tc}", end = " ")
+
+        for i in zip_longest(*arr, fillvalue=""):
+            print(*i, sep="", end="")
+
+        print()
+            

2a89da52c39e6778b3ada391d85236a8316b1418
위연주 <thizzyzz@naver.com>
2021-08-20 17:23:12 +0900

    code: solve SWEA 1961 숫자 배열 회전 (yeonju)

--- SWEA/implementation/1961-숫자_배열_회전/1961-숫자_배열_회전-yeonju.py
+++ SWEA/implementation/1961-숫자_배열_회전/1961-숫자_배열_회전-yeonju.py
@@ -1,1 +1,25 @@
-# delete this and solve here
+def rotate(arr):
+    
+    new_arr = [[0]*n for _ in range(n)]
+    for i in range(n):
+        for j in range(n):
+            new_arr[j][n-i-1] = arr[i][j] # 오른쪽 방향으로 90도씩 회전하여 새 배열에 담기 
+    return new_arr
+
+t = int(input())
+
+for tc in range(t):
+    n = int(input())
+
+    arr_input = [list(map(int, input().split())) for _ in range(n)] 
+    
+    ans90 =rotate(arr_input)
+    ans180 = rotate(ans90)
+    ans270 = rotate(ans180)
+
+    print(f'#{tc+1}')
+    for i in range(n):
+        print("".join(map(str, ans90[i])), end=' ')
+        print("".join(map(str, ans180[i])), end = ' ')
+        print("".join(map(str, ans270[i])))
+

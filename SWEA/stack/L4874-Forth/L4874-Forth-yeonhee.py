2bde28730d245613624fd6f514fda2483e368590
hing9u <yeonheechae425@gmail.com>
2021-08-21 23:51:05 +0900

    code: Solve swea 4874 Forth (yeonhee)

--- SWEA/stack/L4874-Forth/L4874-Forth-yeonhee.py
+++ SWEA/stack/L4874-Forth/L4874-Forth-yeonhee.py
@@ -1,1 +1,32 @@
-# delete this and solve here
+import sys
+sys.stdin = open('input.txt')
+
+for t in range(1, int(input())+1):
+    opcode = list(input().split())
+    stack = []
+
+    try:
+        for op in opcode:
+            if op.isdigit():
+                stack.append(int(op))
+            elif op == '.':
+                result = stack.pop()
+                if stack:
+                    result = 'error'
+            else:
+                num2, num1 = stack.pop(), stack.pop()
+                if op == '+': res = num1 + num2
+                elif op == '*': res = num1 * num2
+                elif op == '-': res = num1 - num2
+                elif op == '/': res = num1 // num2
+                stack.append(res)
+
+    except:
+        result = 'error'
+
+    print(f'#{t} {result}')
+
+"""
+if 한줄코딩을 별로 좋아하지는 않지만,
+가독성을 위해 이렇게 했습니다. 
+"""

978d9b6c72c14643676a0c814412a3d0026463d7
hing9u <yeonheechae425@gmail.com>
2021-08-22 03:44:51 +0900

    code: Solve swea 5102 노드의 거리 (yeonhee)

--- SWEA/queue/L5102-노드의_거리/L5102-노드의_거리-yeonhee.py
+++ SWEA/queue/L5102-노드의_거리/L5102-노드의_거리-yeonhee.py
@@ -1,1 +1,36 @@
-# delete this and solve here
+import sys
+from collections import deque
+sys.stdin = open('input.txt')
+
+
+def bfs(s, g):
+    global result
+    queue = deque([s])
+    visited[s] = 1
+
+    while queue:
+        tmp = queue.popleft()
+        for w in graph[tmp]:
+            if not visited[w]:
+                queue.append(w)
+                visited[w] = visited[tmp] + 1
+                if w == g:
+                    result = visited[w] - 1  # 출발할 떄 1이 더해져서
+                    return
+
+
+for t in range(1, int(input())+1):
+    v, e = map(int, input().split())
+    graph = [[] * (v+1) for _ in range(v+1)]
+    visited = [0] * (v+1)
+    result = 0
+
+    for _ in range(e):
+        start, end = map(int, input().split())
+        graph[start].append(end)
+        graph[end].append(start)
+
+    s, g = map(int, input().split())
+    bfs(s, g)
+
+    print(f'#{t} {result}')

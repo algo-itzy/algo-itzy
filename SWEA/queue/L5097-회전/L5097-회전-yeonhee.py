6db810e70624c485bc6632fa6c827efacb10a11b
hing9u <yeonheechae425@gmail.com>
2021-08-22 02:07:48 +0900

    code: Solve swea 5097 회전 (yeonhee)

--- SWEA/queue/L5097-회전/L5097-회전-yeonhee.py
+++ SWEA/queue/L5097-회전/L5097-회전-yeonhee.py
@@ -1,1 +1,9 @@
-# delete this and solve here
+import sys
+sys.stdin = open('input.txt')
+
+for t in range(1, int(input())+1):
+    n, m = map(int, input().split())
+    numbers = list(map(int, input().split()))
+    for _ in range(m):
+        numbers.append(numbers.pop(0))
+    print(f'#{t} {numbers[0]}')

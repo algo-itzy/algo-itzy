6adc1c70e894a157e5fab0a490d0e95f812333f3
seokzin <seokzin95@gmail.com>
2021-08-22 09:29:03 +0900

    code: Solve swea 5097 회전 (seokzin)

--- SWEA/queue/L5097-회전/L5097-회전-seokzin.py
+++ SWEA/queue/L5097-회전/L5097-회전-seokzin.py
@@ -1,1 +1,5 @@
-# delete this and solve here
+for tc in range(1, int(input())+1):
+    n, m = map(int, input().split())
+    print(f'#{tc}', input().split()[m%n])
+
+# 문제 의도가 뭘까요.. 일부러 pop()을 썼어야 했나?

76d7282125da0cfae3ed9bff1049b10a4eaa2615
hing9u <yeonheechae425@gmail.com>
2021-08-24 00:14:52 +0900

    code: Solve 4881 배열 최소 합 (yeonhee)

--- SWEA/stack/L4881-배열_최소_합/L4881-배열_최소_합-yeonhee.py
+++ SWEA/stack/L4881-배열_최소_합/L4881-배열_최소_합-yeonhee.py
@@ -1,1 +1,31 @@
-# delete this and solve here
+import sys
+sys.stdin = open('input.txt')
+
+
+def dfs(r):  # row
+    global tmp, result
+
+    if tmp > result:
+        return
+
+    if r == n:
+        if tmp < result:
+            result = tmp
+        return
+
+    for i in range(n):
+        if not visited[i]:
+            visited[i] = 1
+            tmp += arr[r][i]
+            dfs(r+1)
+            visited[i] = 0
+            tmp -= arr[r][i]
+
+
+for t in range(1, int(input())+1):
+    n = int(input())
+    arr = [list(map(int, input().split())) for _ in range(n)]
+    visited = [0] * n
+    tmp, result = 0, 1000
+    dfs(0)
+    print(f'#{t} {result}')

14e8517a7bd764ba2c71eb75a7e6b6a20fcfae92
서울_4반_정지웅 <j2woong1@gmail.com>
2021-08-25 23:07:19 +0900

    code: solve swea 5178 노드의_합 (jiwoong)

--- SWEA/tree/L5178-노드의_합/L5178-노드의_합-jiwoong.py
+++ SWEA/tree/L5178-노드의_합/L5178-노드의_합-jiwoong.py
@@ -1,1 +1,15 @@
-# delete this and solve here
+T = int(input())
+for tc in range(1, T + 1):
+    N, M, L = map(int, input().split())
+    tree = [0] * (N + 1)
+    for _ in range(M):
+        leaf, num = map(int, input().split())
+        tree[leaf] = num
+
+    for i in range(N - M, 0, -1):
+        if i * 2 <= N:
+            tree[i] = tree[i * 2]
+            if i * 2 + 1 <= N:
+                tree[i] += tree[i * 2 + 1]
+
+    print(f'#{tc} {tree[L]}')

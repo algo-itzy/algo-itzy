add941782d582310be28b4e2759ec155f225be2d
hing9u <yeonheechae425@gmail.com>
2021-08-25 15:38:24 +0900

    code: Solve swea 5018 숫자 추가 (yeonhee)

--- SWEA/linked_list/L5018-숫자_추가/L5018-숫자_추가-yeonhee.py
+++ SWEA/linked_list/L5018-숫자_추가/L5018-숫자_추가-yeonhee.py
@@ -1,1 +1,12 @@
-# delete this and solve here
+import sys
+sys.stdin = open('input.txt')
+
+for t in range(1, int(input())+1):
+    N, M, L = map(int, input().split())
+    numbers = list(map(int, input().split()))
+
+    for _ in range(M):
+        idx, num = map(int, input().split())
+        numbers.insert(idx, num)
+
+    print(f'#{t} {numbers[L]}')

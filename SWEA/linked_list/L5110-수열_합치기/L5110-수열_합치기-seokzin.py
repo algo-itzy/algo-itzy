9b623292ab5357b2806e3cf04b99497c2c6afc46
seokzin <seokzin95@gmail.com>
2021-08-25 17:07:42 +0900

    code: Solve swea 5110 수열 합치기 (seokzin)

--- SWEA/linked_list/L5110-수열_합치기/L5110-수열_합치기-seokzin.py
+++ SWEA/linked_list/L5110-수열_합치기/L5110-수열_합치기-seokzin.py
@@ -1,1 +1,16 @@
-# delete this and solve here
+for tc in range(1, int(input())+1):
+    N, M = map(int, input().split())
+    res = []
+    
+    for _ in range(M):
+        arr = list(map(int, input().split()))
+
+        for i in range(len(res)):
+            if res[i] > arr[0]:
+                res[i:i] = arr  # 얘는 Pass
+                # res = res[:i] + arr + res[i:]  # 얘는 시간초과
+                break
+        else:
+            res += arr  # for문 통과시 맨 뒤에 extend
+
+    print(f'#{tc}', *res[-1:-11:-1])  # 뒤집어서 10개 출력 어떻게 더 잘하지

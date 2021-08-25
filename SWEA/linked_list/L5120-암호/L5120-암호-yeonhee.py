b00f0919926fea51cafbc5065d165763cfc6814e
hing9u <yeonheechae425@gmail.com>
2021-08-25 16:46:01 +0900

    code: Solve swea 5120 암호 (yeonhee)

--- SWEA/linked_list/L5120-암호/L5120-암호-yeonhee.py
+++ SWEA/linked_list/L5120-암호/L5120-암호-yeonhee.py
@@ -1,1 +1,17 @@
-# delete this and solve here
+import sys
+sys.stdin = open('input.txt')
+
+for t in range(1, int(input())+1):
+    n, m, k = map(int, input().split())
+    numbers = list(map(int, input().split()))
+    idx = m
+
+    for _ in range(k):
+        if not idx:
+            numbers.append(numbers[idx - 1] + numbers[idx])
+            idx -= 1
+        else:
+            numbers.insert(idx, numbers[idx - 1] + numbers[idx])
+        idx = (idx + m) % len(numbers)
+
+    print(f'#{t}', *numbers[::-1][:10])

b39486b1949ea2e244ea4e89684c649169fe40a2
서울_4반_정지웅 <j2woong1@gmail.com>
2021-08-25 23:05:57 +0900

    code: solve swea 5176 이진탐색 (jiwoong)

--- SWEA/tree/L5176-이진탐색/L5176-이진탐색-jiwoong.py
+++ SWEA/tree/L5176-이진탐색/L5176-이진탐색-jiwoong.py
@@ -1,1 +1,24 @@
-# delete this and solve here
+def inorder(root):
+    global num
+    if 1 <= root <= N:
+        # 왼쪽
+        left = root * 2
+        if 0 < left < N + 1:
+            inorder(left)
+        tree[root] = num
+        num += 1
+        # 오른쪽
+        right = root * 2 + 1
+        if 0 < right < N + 1:
+            inorder(right)
+
+
+T = int(input())
+for tc in range(1, T + 1):
+    N = int(input())
+
+    tree = [0] * (N + 1)
+
+    num = 1
+    inorder(1)
+    print(f"#{tc}", tree[1], tree[N // 2])

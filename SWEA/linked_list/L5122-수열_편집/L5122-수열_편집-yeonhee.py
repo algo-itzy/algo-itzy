4ad9df9cc42f23ed2c7121740db45c0a857bd023
hing9u <yeonheechae425@gmail.com>
2021-08-25 16:55:32 +0900

    code: Solve swea 5122 수열 편집 (yeonhee)

--- SWEA/linked_list/L5122-수열_편집/L5122-수열_편집-yeonhee.py
+++ SWEA/linked_list/L5122-수열_편집/L5122-수열_편집-yeonhee.py
@@ -1,1 +1,19 @@
-# delete this and solve here
+import sys
+sys.stdin = open('input.txt')
+
+for t in range(1, int(input())+1):
+    N, M, L = map(int, input().split())
+    numbers = list(map(int, input().split()))
+
+    for _ in range(M):
+        cmd = input().split()
+
+        if cmd[0] == 'I':
+            numbers.insert(int(cmd[1]), int(cmd[2]))
+        elif cmd[0] == 'D':
+            numbers.pop(int(cmd[1]))
+        elif cmd[0] == 'C':
+            numbers[int(cmd[1])] = int(cmd[2])
+
+    result = numbers[L] if len(numbers) > L else -1
+    print(f'#{t} {result}')

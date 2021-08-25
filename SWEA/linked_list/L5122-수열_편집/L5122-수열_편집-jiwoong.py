d5328fb171c574f0813bf2ff9644a4df060fdc80
서울_4반_정지웅 <j2woong1@gmail.com>
2021-08-25 23:04:31 +0900

    code: solve swea 5122 수열_편집 (jiwoong)

--- SWEA/linked_list/L5122-수열_편집/L5122-수열_편집-jiwoong.py
+++ SWEA/linked_list/L5122-수열_편집/L5122-수열_편집-jiwoong.py
@@ -1,1 +1,23 @@
-# delete this and solve here
+T = int(input())
+for tc in range(1, T + 1):
+    N, M, L = map(int, input().split())
+    seq = input().split()
+
+    for _ in range(M):
+        op = input().split()
+        # 추가, 뒤로 이동
+        if op[0] == 'I':
+            seq.insert(int(op[1]), op[2])
+        # 삭제, 앞으로 이동
+        elif op[0] == 'D':
+            seq.pop(int(op[1]))
+        # 변경
+        else:
+            seq[int(op[1])] = op[2]
+    # 존재 안하면
+    ans = -1
+
+    if len(seq) > L:
+        ans = seq[L]
+
+    print('#{} {}'.format(tc, ans))

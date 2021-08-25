ca4e374d2d58312ef59ae450b4078fff9451a771
seokzin <seokzin95@gmail.com>
2021-08-25 17:07:10 +0900

    code: Solve swea 5018 숫자 추가 (seokzin)

--- SWEA/linked_list/L5018-숫자_추가/L5018-숫자_추가-seokzin.py
+++ SWEA/linked_list/L5018-숫자_추가/L5018-숫자_추가-seokzin.py
@@ -1,1 +1,11 @@
-# delete this and solve here
+for tc in range(1, int(input())+1):
+    N, M, L = map(int, input().split())
+    nums = list(map(int, input().split()))
+
+    for _ in range(M):
+        nums.insert(*map(int, input().split()))
+        
+    print(f'#{tc}', nums[L])
+
+# map도 구조 분해 할당이 되는 것이 신기했습니다.
+# 더 나은 풀이가 있겠지만 대충 insert로 풀었습니다. 

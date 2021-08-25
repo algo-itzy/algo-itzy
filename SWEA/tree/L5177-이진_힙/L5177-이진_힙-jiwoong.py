5729f6666e44c29eef7e4e3557443d261dce63ce
서울_4반_정지웅 <j2woong1@gmail.com>
2021-08-25 23:06:40 +0900

    code: solve swea 5177 이진_힙 (jiwoong)

--- SWEA/tree/L5177-이진_힙/L5177-이진_힙-jiwoong.py
+++ SWEA/tree/L5177-이진_힙/L5177-이진_힙-jiwoong.py
@@ -1,1 +1,24 @@
-# delete this and solve here
+T = int(input())
+for tc in range(1, T + 1):
+    N = int(input())
+    nums = list(map(int, input().split()))
+
+    q = [0 for i in range(N + 1)]
+
+    for i in range(1, N + 1):
+        # q 삽입
+        q[i] = nums[i - 1]
+        heap = i
+        while True:
+            if q[heap] <= q[heap // 2] and heap != 1:
+                q[heap], q[heap // 2] = q[heap // 2], q[heap]
+            else:
+                break
+            heap //= 2
+
+    ans = 0
+    while N > 1:
+        ans += q[N // 2]
+        N //= 2
+
+    print(f"#{tc} {ans}")

913a78e584589c3b7b298b3e5194d87b4a437de8
서울_4반_정지웅 <j2woong1@gmail.com>
2021-08-25 23:02:14 +0900

    code: solve swea 5018 숫자_추가 (jiwoong)

--- SWEA/linked_list/L5018-숫자_추가/L5018-숫자_추가-jiwoong.py
+++ SWEA/linked_list/L5018-숫자_추가/L5018-숫자_추가-jiwoong.py
@@ -1,1 +1,9 @@
-# delete this and solve here
+T = int(input())
+for tc in range(1, T + 1):
+    N, M, L = list(map(int, input().split()))
+    nums = list(map(int, input().split()))
+    for _ in range(M):
+        i, j = list(map(int, input().split()))
+        nums.insert(i, j)
+
+    print("#{0} {1}".format(tc, nums[L]))

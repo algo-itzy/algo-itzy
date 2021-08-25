8dba0bf122a63ee3ad2f057cccada86e7816c712
서울_4반_정지웅 <j2woong1@gmail.com>
2021-08-25 23:05:16 +0900

    code: solve 5174 subtree (jiwoong)

--- SWEA/tree/L5174-subtree/L5174-subtree-jiwoong.py
+++ SWEA/tree/L5174-subtree/L5174-subtree-jiwoong.py
@@ -1,1 +1,28 @@
-# delete this and solve here
+def subtree(node):
+    global ans
+    if node != 0:
+        ans += 1
+        subtree(left[node])
+        subtree(right[node])
+
+
+T = int(input())
+
+for tc in range(1, T + 1):
+    E, N = map(int, input().split())  # N은 내가 원하는 루트
+    arr = list(map(int, input().split()))
+
+    left = [0] * (E + 2)  # 첫 번째 자식
+    right = [0] * (E + 2)  # 두 번째 자식
+
+    for i in range(E):
+        paren, child = arr[i * 2], arr[2 * i + 1]
+        if left[paren] == 0:
+            left[paren] = child
+        else:
+            right[paren] = child
+
+    ans = 0
+    subtree(N)
+
+    print(f'#{tc} {ans}')

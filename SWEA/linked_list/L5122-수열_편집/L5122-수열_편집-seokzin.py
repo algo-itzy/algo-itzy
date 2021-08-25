a2abc3884822b82e0c377e8b61c5b3f9dfefa032
seokzin <seokzin95@gmail.com>
2021-08-25 17:08:45 +0900

    code: Solve swea 5122 수열 편집 (seokzin)

--- SWEA/linked_list/L5122-수열_편집/L5122-수열_편집-seokzin.py
+++ SWEA/linked_list/L5122-수열_편집/L5122-수열_편집-seokzin.py
@@ -1,1 +1,15 @@
-# delete this and solve here
+for tc in range(1, int(input())+1):
+    N, M, L = map(int, input().split())	
+    arr = list(map(int, input().split()))
+
+    for _ in range(M) :
+        cmd = list(input().split())
+
+        if cmd[0] == 'I':
+            arr.insert(int(cmd[1]), int(cmd[2]))
+        elif cmd[0] == 'C':
+            arr[int(cmd[1])] = int(cmd[2])
+        elif cmd[0] == 'D':
+            arr.pop(int(cmd[1]))
+    
+    print(f"#{tc}", arr[L] if L < len(arr) else -1)

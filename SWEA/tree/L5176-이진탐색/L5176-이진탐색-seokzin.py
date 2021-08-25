ac1d5b67b870f9913348242f79d49e575d5f0868
seokzin <seokzin95@gmail.com>
2021-08-25 17:09:49 +0900

    code: Solve swea 5176 이진탐색 (seokzin)

--- SWEA/tree/L5176-이진탐색/L5176-이진탐색-seokzin.py
+++ SWEA/tree/L5176-이진탐색/L5176-이진탐색-seokzin.py
@@ -1,1 +1,23 @@
-# delete this and solve here
+def inorder(n):
+    global cnt
+
+    if n <= N:
+        inorder(n*2)      # left
+
+        arr[n] = cnt      # root
+        cnt += 1             
+
+        inorder(n*2 + 1)  # right
+
+
+for tc in range(1, int(input())+1):
+    N = int(input())
+    arr = [0 for _ in range(N+1)]
+    cnt = 1
+
+    inorder(1)  # 1부터 넣기 시작
+
+    print(f'#{tc}', arr[1], arr[N//2])
+
+# 생소한 문제라 어려웠습니다. 코드 참조했음
+# cnt를 글로벌하게 관리하는게 핵심인듯

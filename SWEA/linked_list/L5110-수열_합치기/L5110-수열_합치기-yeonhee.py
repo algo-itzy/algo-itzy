a711624fc668ac0167f203216d766ddc36ab83ef
hing9u <yeonheechae425@gmail.com>
2021-08-25 16:46:26 +0900

    code: Solve swea 5110 수열 합치기 (yeonhee)

--- SWEA/linked_list/L5110-수열_합치기/L5110-수열_합치기-yeonhee.py
+++ SWEA/linked_list/L5110-수열_합치기/L5110-수열_합치기-yeonhee.py
@@ -1,1 +1,17 @@
-# delete this and solve here
+import sys
+sys.stdin = open('input.txt')
+
+for t in range(1, int(input())+1):
+    n, m = map(int, input().split())
+    result = [1001]
+
+    for _ in range(m):
+        numbers = list(map(int, input().split()))
+
+        for i in range(len(result)):
+            if result[i] > numbers[0]:
+                # result = result[:i] + numbers + result[i:] : 시간초과
+                result[i:i] = numbers
+                break
+
+    print(f'#{t}', *result[-11:-1][::-1])

30a653becae2e10ba3a86839fb1fb54858c50bc2
hing9u <yeonheechae425@gmail.com>
2021-08-26 15:54:01 +0900

    code: Solve swea 5174 subtree (yeonhee)

--- SWEA/tree/L5174-subtree/L5174-subtree-yeonhee.py
+++ SWEA/tree/L5174-subtree/L5174-subtree-yeonhee.py
@@ -1,1 +1,25 @@
-# delete this and solve here
+import sys
+from collections import deque
+
+sys.stdin = open('input.txt')
+
+for t in range(1, int(input())+1):
+    E, N = map(int, input().split())
+    nums = list(map(int, input().split()))
+    graph = {i: [] for i in range(1, E+2)}
+    visited = [0] * (E+2)
+
+    for i in range(0, 2*E, 2):
+        graph[nums[i]] += [nums[i+1]]
+
+    # bfs
+    q = deque([N])
+    visited[N] = 1
+
+    while q:
+        now = q.popleft()
+        for next in graph[now]:
+            q.append(next)
+            visited[next] = 1
+
+    print(f'#{t} {sum(visited)}')

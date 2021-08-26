45a7f77eb0ef4f3e0209b262a4db6c3880a7a632
YoonBaek <royal729@naver.com>
2021-08-26 19:39:17 +0900

    git commit -m "code: Solve swea L4881 배열 최소 합 (yoonbaek)"

--- SWEA/stack/L4881-배열_최소_합/L4881-배열_최소_합-yoonbaek.py
+++ SWEA/stack/L4881-배열_최소_합/L4881-배열_최소_합-yoonbaek.py
@@ -1,1 +1,35 @@
-# delete this and solve here
+# solved by YoonBaek
+
+
+def permutation_pruning(i, cur_min):
+    global global_min
+
+    # pruning
+    if cur_min > global_min:
+        return
+
+    if i == N:
+        global_min = min(global_min, cur_min)  
+    
+    for j in range(N): 
+        if not visited[j]:
+            visited[j] = 1
+            permutation_pruning(i+1, cur_min+mat[i][j])
+            visited[j] = 0
+    
+
+if __name__ == "__main__":
+    tc = int(input())
+
+    for T in range(1, tc+1):
+        N = int(input())
+        
+        mat = [list(map(int, input().split())) for _ in range(N)]
+        
+        # 10보다 작은 자연수 N개라고 했음
+        global_min = N * 9
+        visited = [0] * N
+
+        permutation_pruning(0, 0)
+        
+        print(f"#{T} {global_min}")

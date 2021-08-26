f0bda8475ca922edaad8255520e42abc0e0b318d
ed-kyu <seungkyukim.dev@gmail.com>
2021-08-26 18:01:13 +0900

    code: Solve swea 5018 숫자 추가 (seungkyu)

--- SWEA/linked_list/L5018-숫자_추가/L5018-숫자_추가-seungkyu.py
+++ SWEA/linked_list/L5018-숫자_추가/L5018-숫자_추가-seungkyu.py
@@ -1,1 +1,15 @@
-# delete this and solve here
+import sys
+sys.stdin = open('input.txt')
+
+T = int(input())
+
+for test_case in range(1, T+1):
+
+    N, M, L = map(int, input().split())
+    numbers = list(map(int, input().split()))
+
+    for _ in range(M):
+        idx, num = map(int, input().split())
+        numbers.insert(idx, num)
+
+    print(f'#{test_case} {numbers[L]}')

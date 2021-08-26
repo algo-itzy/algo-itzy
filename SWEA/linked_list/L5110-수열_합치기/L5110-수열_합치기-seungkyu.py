8ce73c02bfd6a4c8976a3480482981e4d135dc0f
ed-kyu <seungkyukim.dev@gmail.com>
2021-08-26 18:02:38 +0900

    code: Solve 5110 수열 합치기 (seungkyu)

--- SWEA/linked_list/L5110-수열_합치기/L5110-수열_합치기-seungkyu.py
+++ SWEA/linked_list/L5110-수열_합치기/L5110-수열_합치기-seungkyu.py
@@ -1,1 +1,20 @@
-# delete this and solve here
+import sys
+sys.stdin = open('input.txt')
+
+T = int(input())
+
+for test_case in range(1, T+1):
+
+    N, M = map(int, input().split())
+
+    numbers = []
+    for _ in range(M):
+        nums = list(map(int, input().split()))
+        for idx, num in enumerate(numbers):
+            if nums[0] < num:
+                numbers[idx:idx] = nums  #  idx:idx 배열 중간에 값 넣기
+                break
+        else:
+            numbers += nums  # 끝에 요소들 붙이기
+
+    print(f'#{test_case}', *numbers[-1:-11:-1])

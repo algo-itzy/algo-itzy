bb2af32cbac213c41fcf3c57bd8ac0d68be5ad4c
hing9u <yeonheechae425@gmail.com>
2021-08-26 17:42:44 +0900

    code: Solve swea 5178 노드의 합 (yeonhee)

--- SWEA/tree/L5178-노드의_합/L5178-노드의_합-yeonhee.py
+++ SWEA/tree/L5178-노드의_합/L5178-노드의_합-yeonhee.py
@@ -1,1 +1,15 @@
-# delete this and solve here
+import sys
+sys.stdin = open('input.txt')
+
+for t in range(1, int(input())+1):
+    N, M, L = map(int, input().split())
+    tree = [0] * (N+2)
+
+    for _ in range(M):
+        idx, num = map(int, input().split())
+        tree[idx] = num
+
+    for i in range(N-M, 0, -1):
+        tree[i] = tree[i*2] + tree[i*2 + 1]
+
+    print(f'#{t} {tree[L]}')

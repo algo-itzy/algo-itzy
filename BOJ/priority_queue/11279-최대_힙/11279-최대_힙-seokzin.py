c5d4937c7b357bf1f5c8eefee4fdb5cc90806168
seokzin <seokzin95@gmail.com>
2021-08-27 20:23:49 +0900

    Solve boj 11279 최대 힙 (seokzin)

--- BOJ/priority_queue/11279-최대_힙/11279-최대_힙-seokzin.py
+++ BOJ/priority_queue/11279-최대_힙/11279-최대_힙-seokzin.py
@@ -1,1 +1,15 @@
-# delete this and solve here
+import heapq
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+hq = []
+
+for _ in range(n):
+    x = int(input())
+
+    if x == 0:
+        print(-1 * heapq.heappop(hq) if hq else 0)
+    else:
+        heapq.heappush(hq, -x)

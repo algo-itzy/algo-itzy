be322a8ddf279654653f2311d73f3eb78ad27342
seokzin <seokzin95@gmail.com>
2021-08-27 20:24:03 +0900

    Solve boj 11399 ATM (seokzin)

--- BOJ/greedy/11399-ATM/11399-ATM-seokzin.py
+++ BOJ/greedy/11399-ATM/11399-ATM-seokzin.py
@@ -1,1 +1,10 @@
-# delete this and solve here
+n = int(input())
+p = sorted(list(map(int, input().split())))
+res = 0
+
+for i in range(1, n+1):
+  res += sum(p[0 : i])
+
+print(res)
+
+# SJF 스케줄링과 비슷

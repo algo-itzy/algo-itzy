a3515d627a6c7519b76c1e72dbb2969283f0a8d7
seokzin <seokzin95@gmail.com>
2021-08-27 20:52:33 +0900

    feat: Update mkdir program

--- BOJ/mkdir.py
+++ BOJ/mkdir.py
@@ -1,39 +1,48 @@
 import os
 
 # ----------------------------------------------------------------------------------
-# 풀 문제 카테고리, 문제번호, 문제명 입력
+print('------------------------------------------------------------')
+print('노션 테이블의 [문제번호][문제 이름][알고리즘 분류]를 복붙해주세요')
+print('------------------------------------------------------------')
 
-problem = '07576-토마토'
-path = './bfs/' + problem
-readme = '# 7576 토마토\nhttps://www.acmicpc.net/problem/7576'
+datas = []
 
-"""
-요소 간 구분은 - (hyphen), 요소 내 공백은 _ (underbar) 지켜서 입력해주세요.
-boj 문제 중 문제번호가 4자리인 경우 앞에 0을 붙여주세요.
+for _ in range(7):  # 7문제로 고정했습니다. 추후 리팩토링
+    datas.append(input().split())
 
-예시 ) bfs/07576-토마토
-"""
+for data in datas:
+    num = data.pop(0).zfill(5)
+    category = data.pop()
+    title = ' '.join(data)
+    title_bar = title.replace(' ', '_')
+
+    problem = f'{num}-{title_bar}'
+    path = f'./{category}/{problem}' 
+
+    readme = f"# {str(int(num))} {title}\nhttps://www.acmicpc.net/problem/{num}"
 
 # ----------------------------------------------------------------------------------
 
-# 디렉토리 생성
-try:
-    if not os.path.exists(path):
-        os.makedirs(path)
-except OSError:
-    print('이미 존재하는 디렉토리입니다.')
+    # 디렉토리 생성
+    try:
+        if not os.path.exists(path):
+            os.makedirs(path)
+    except OSError:
+        print('이미 존재하는 디렉토리입니다.')
 
-# 개별 파일 생성
-members = ['jiwoong', 'seokzin', 'seungjoo', 'seungkyu', 'yeonhee', 'yeonju', 'yoonbaek']
+    # 개별 파일 생성
+    members = ['jiwoong', 'seokzin', 'seungjoo', 'seungkyu', 'yeonhee', 'yeonju', 'yoonbaek']
 
-for member in members:
-    filepath = os.path.join(path, f'{problem}-{member}.py')
-    fid = open(filepath, 'w')
-    fid.write('# delete this and solve here')
-    fid.close()
+    for member in members:
+        filepath = os.path.join(path, f'{problem}-{member}.py')
+        fid = open(filepath, 'w', encoding='utf8')
+        fid.write(f'# git commit -m "Solve boj {num} {title} ({member})"')
+        fid.close()
 
-# README 생성
-md_path = os.path.join(path, 'README.md')
-md_fid = open(md_path, 'w', encoding='utf8')
-md_fid.write(readme)
-md_fid.close()
+    # README 생성
+    md_path = os.path.join(path, 'README.md')
+    md_fid = open(md_path, 'w', encoding='utf8')
+    md_fid.write(readme)
+    md_fid.close()
+
+print('디렉토리 생성 끝!')

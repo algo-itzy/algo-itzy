5531e8375e75037f7271029078c945c2c1d22bd8
ed-kyu <seungkyukim.dev@gmail.com>
2021-08-28 12:24:05 +0900

    Solve boj 11399 ATM (seungkyu)

--- BOJ/greedy/11399-ATM/11399-ATM-seungkyu.py
+++ BOJ/greedy/11399-ATM/11399-ATM-seungkyu.py
@@ -1,1 +1,12 @@
-# git commit -m "Solve boj 11399 ATM (seungkyu)"
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+Pi = list(map(int, input().split()))
+
+Pi.sort()  # 정렬해서 풀면 쉬운 문제
+total = 0
+
+for idx, num in enumerate(Pi):
+    total += (len(Pi)-idx)*num
+print(total)

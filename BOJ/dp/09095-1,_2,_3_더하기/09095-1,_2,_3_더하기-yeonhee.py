f9ee37f25a6803a7ef445c58d7e46c0dcccbe7f7
hing9u <yeonheechae425@gmail.com>
2021-08-29 01:50:12 +0900

    code: Solve boj 09095 1, 2, 3 더하기 (yeonhee)

--- BOJ/dp/09095-1,_2,_3_더하기/09095-1,_2,_3_더하기-yeonhee.py
+++ BOJ/dp/09095-1,_2,_3_더하기/09095-1,_2,_3_더하기-yeonhee.py
@@ -1,1 +1,7 @@
-# git commit -m "Solve boj 09095 1, 2, 3 더하기 (yeonhee)"
+dp = [1, 2, 4]
+
+for i in range(3, 11):
+    dp.append(dp[i-3] + dp[i-2] + dp[i-1])
+
+for _ in range(int(input())):
+    print(dp[int(input())-1])

c2abfacf7bc35bd6dac84c0e40e197dd223c591b
Seungkyu Kim <76573337+ed-kyu@users.noreply.github.com>
2021-08-28 16:07:53 +0900

    code: Solve boj 02630 색종이 만들기 (seungkyu)

--- BOJ/divide_and_conquer/02630-색종이_만들기/02630-색종이_만들기-seungkyu.py
+++ BOJ/divide_and_conquer/02630-색종이_만들기/02630-색종이_만들기-seungkyu.py
@@ -1,1 +1,30 @@
-# git commit -m "Solve boj 02630 색종이 만들기 (seungkyu)"
+# 분할 정복 문제, 인덱스를 기준으로 나누기
+import sys
+
+
+def divide_conquer(x, y, N):
+    global zero_num, one_num  # 0 정사각형, 1 정사각형 수
+    start = matrix[x][y]
+
+    for i in range(x, x+N):
+        for j in range(y, y+N):
+          if matrix[i][j] != start:
+              divide_conquer(x, y, N//2)  # 2사분면
+              divide_conquer(x, y + N//2, N//2)  # 3사분면
+              divide_conquer(x + N//2, y, N//2)  # 1사분면
+              divide_conquer(x + N//2, y + N//2, N//2)  # 4사분면
+              return
+
+    if start == 0:
+        zero_num += 1
+    else:
+        one_num += 1
+
+N = int(sys.stdin.readline())
+matrix = [list(map(int, sys.stdin.readline().split())) for _ in range(N)] 
+
+zero_num = one_num = 0
+
+divide_conquer(0, 0, N)
+
+print(zero_num, one_num, sep='\n')

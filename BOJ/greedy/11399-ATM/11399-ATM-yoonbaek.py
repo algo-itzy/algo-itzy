7918daa3e0724468b945932041effaa9a88d6c13
YoonBaek <royal729@naver.com>
2021-08-28 20:24:55 +0900

    Solve boj 11399 ATM (yoonbaek)

--- BOJ/greedy/11399-ATM/11399-ATM-yoonbaek.py
+++ BOJ/greedy/11399-ATM/11399-ATM-yoonbaek.py
@@ -1,1 +1,13 @@
-# git commit -m "Solve boj 11399 ATM (yoonbaek)"
+# 어르신들 버스 내리실 때 모습 생각하면서 풀었습니다.
+
+if __name__ == "__main__":
+    N = int(input())
+    Ps = sorted(list(map(int, input().split())))
+
+    # time per person
+    tpp, total = 0, 0
+    for p in Ps:
+        tpp += p
+        total += tpp
+
+    print(total)

231f806531d45920bcb9ee36f9ae2aaa163c8026
서울_4반_정지웅 <j2woong1@gmail.com>
2021-08-28 14:45:52 +0900

    code: solve 2630 색종이_만들기 (jiwoong)

--- BOJ/divide_and_conquer/02630-색종이_만들기/02630-색종이_만들기-jiwoong.py
+++ BOJ/divide_and_conquer/02630-색종이_만들기/02630-색종이_만들기-jiwoong.py
@@ -1,1 +1,37 @@
-# git commit -m "Solve boj 02630 색종이 만들기 (jiwoong)"
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+paper = [list(map(int, input().split())) for _ in range(n)]
+
+white = 0  # 0
+blue = 0  # 1
+
+
+def div_conq(row, col, n):
+    global white, blue
+    total = paper[row][col]
+    tmp = True
+
+    for i in range(row, row + n):
+        if not tmp:
+            break
+        for j in range(col, col + n):
+            if total != paper[i][j]:
+                tmp = False
+                div_conq(row, col, n // 2)
+                div_conq(row, col + n // 2, n // 2)
+                div_conq(row + n // 2, col, n // 2)
+                div_conq(row + n // 2, col + n // 2, n // 2)
+                break
+
+    if tmp:
+        if total == 1:
+            blue += 1
+        else:
+            white += 1
+
+
+div_conq(0, 0, n)
+print(white)
+print(blue)

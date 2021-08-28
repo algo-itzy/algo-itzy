54b2d4ad8d1b8c985b54a06145f347c05b3c2198
ed-kyu <seungkyukim.dev@gmail.com>
2021-08-28 12:21:33 +0900

    Solve boj 09095 1, 2, 3 더하기 (seungkyu)

--- BOJ/dp/09095-1,_2,_3_더하기/09095-1,_2,_3_더하기-seungkyu.py
+++ BOJ/dp/09095-1,_2,_3_더하기/09095-1,_2,_3_더하기-seungkyu.py
@@ -1,1 +1,18 @@
-# git commit -m "Solve boj 09095 1, 2, 3 더하기 (seungkyu)"
+import sys
+input = sys.stdin.readline
+
+def dp(n):
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 2
+    elif n == 3:
+        return 4
+    else:
+        return dp(n-1) + dp(n-2) + dp(n-3)
+
+
+T = int(input())
+for _ in range(T):
+    n = int(input())
+    print(dp(n))

476870ad42b3ca5d88b7c80b4116df6874a304f0
서울_4반_정지웅 <j2woong1@gmail.com>
2021-08-28 15:02:26 +0900

    code: solve 11279 최대 힙 (jiwoong)

--- BOJ/priority_queue/11279-최대_힙/11279-최대_힙-jiwoong.py
+++ BOJ/priority_queue/11279-최대_힙/11279-최대_힙-jiwoong.py
@@ -1,1 +1,16 @@
-# git commit -m "Solve boj 11279 최대 힙 (jiwoong)"
+import sys
+import heapq
+
+input = sys.stdin.readline
+
+N = int(input())
+heap = []
+for _ in range(N):
+    x = int(input())
+    if x == 0:
+        if heap:
+            print(-heapq.heappop(heap))
+        else:
+            print('0')
+    else:
+        heapq.heappush(heap, -x)

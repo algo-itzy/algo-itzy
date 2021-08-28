4a7f66335662eccf6db3da68061b7c9b2c681430
hing9u <yeonheechae425@gmail.com>
2021-08-29 01:50:40 +0900

    code: Solve boj 11279 최대 힙 (yeonhee)

--- BOJ/priority_queue/11279-최대_힙/11279-최대_힙-yeonhee.py
+++ BOJ/priority_queue/11279-최대_힙/11279-최대_힙-yeonhee.py
@@ -1,1 +1,18 @@
-# git commit -m "Solve boj 11279 최대 힙 (yeonhee)"
+import sys
+import heapq
+
+input = sys.stdin.readline
+
+N = int(input())
+heap = []
+
+for _ in range(N):
+    x = int(input())
+    if x == 0:
+        print(-1 * heapq.heappop(heap) if heap else 0)
+    else:
+        heapq.heappush(heap, -x)
+
+"""
+시간초과 오랜만...
+"""

d633163c9a1f49bc1c6c87490350ec42a58cdd89
서울_4반_정지웅 <j2woong1@gmail.com>
2021-08-28 15:04:03 +0900

    code: solve 11723 집합 (jiwoong)

--- BOJ/implementation/11723-집합/11723-집합-jiwoong.py
+++ BOJ/implementation/11723-집합/11723-집합-jiwoong.py
@@ -1,1 +1,33 @@
-# git commit -m "Solve boj 11723 집합 (jiwoong)"
+import sys
+
+input = sys.stdin.readline
+
+S = set()
+M = int(input())
+for i in range(M):
+    op = input().split()
+
+    if op[0] == 'add':
+        S.add(int(op[1]))
+
+    elif op[0] == 'remove':
+        if int(op[1]) in S:
+            S.remove(int(op[1]))
+
+    elif op[0] == 'check':
+        if int(op[1]) in S:
+            print(1)
+        else:
+            print(0)
+
+    elif op[0] == 'toggle':
+        if int(op[1]) in S:
+            S.remove(int(op[1]))
+        else:
+            S.add(int(op[1]))
+
+    elif op[0] == 'all':
+        S = {i for i in range(1, 21)}
+
+    elif op[0] == 'empty':
+        S = set()

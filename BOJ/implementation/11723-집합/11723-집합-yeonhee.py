ccd13e3680ab2ac4eb9164cd2c733e8e9de81deb
hing9u <yeonheechae425@gmail.com>
2021-08-29 01:52:21 +0900

    code: Solve boj 11723 집합 (yeonhee)

--- BOJ/implementation/11723-집합/11723-집합-yeonhee.py
+++ BOJ/implementation/11723-집합/11723-집합-yeonhee.py
@@ -1,1 +1,26 @@
-# git commit -m "Solve boj 11723 집합 (yeonhee)"
+import sys
+input = sys.stdin.readline
+
+M = int(input())
+S = set()
+S_all = set(map(str, range(1, 21)))
+
+for _ in range(M):
+    cmd = input().split()
+
+    if cmd[0] == 'add':
+        S.add(cmd[1])
+    elif cmd[0] == 'remove':
+        S.discard(cmd[1])
+    elif cmd[0] == 'check':
+        print(1 if cmd[1] in S else 0)
+    elif cmd[0] == 'toggle':
+        S.discard(cmd[1]) if cmd[1] in S else S.add(cmd[1])
+    elif cmd[0] == 'all':
+        S = S_all
+    elif cmd[0] == 'empty':
+        S = set()
+
+"""
+S_all 안하면 시간초과 나네요...
+"""

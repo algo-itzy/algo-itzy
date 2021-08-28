d5c25fe114d37869c57ab657ab79db6254773e00
서울_4반_정지웅 <j2woong1@gmail.com>
2021-08-28 15:03:13 +0900

    code: solve 11399 ATM (jiwoong)

--- BOJ/greedy/11399-ATM/11399-ATM-jiwoong.py
+++ BOJ/greedy/11399-ATM/11399-ATM-jiwoong.py
@@ -1,1 +1,9 @@
-# git commit -m "Solve boj 11399 ATM (jiwoong)"
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+wait = sorted(list(map(int, input().split())))
+ans = 0
+for i in range(N):
+    ans += wait[i] * (N - i)
+print(ans)

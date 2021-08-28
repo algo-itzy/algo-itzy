bb6ffbead38a232397037e87e894702adf3ca9f8
서울_4반_정지웅 <j2woong1@gmail.com>
2021-08-28 15:01:34 +0900

    code: solve 9095 1, 2, 3 더하기 (jiwoong)

--- BOJ/dp/09095-1,_2,_3_더하기/09095-1,_2,_3_더하기-jiwoong.py
+++ BOJ/dp/09095-1,_2,_3_더하기/09095-1,_2,_3_더하기-jiwoong.py
@@ -1,1 +1,20 @@
-# git commit -m "Solve boj 09095 1, 2, 3 더하기 (jiwoong)"
+def method(num):
+    global ans
+    if num > N:
+        return
+    elif num == N:
+        ans += 1
+    else:
+        method(num + 1)
+        method(num + 2)
+        method(num + 3)
+
+
+T = int(input())
+for tc in range(1, T + 1):
+    N = int(input())
+
+    ans = 0
+    method(0)
+
+    print(ans)

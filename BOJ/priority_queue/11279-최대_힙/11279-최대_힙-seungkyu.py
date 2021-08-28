9ca55451a7be4ff7c438165519de19874c567502
ed-kyu <seungkyukim.dev@gmail.com>
2021-08-28 12:22:54 +0900

    Solve boj 11279 최대 힙 (seungkyu)

--- BOJ/priority_queue/11279-최대_힙/11279-최대_힙-seungkyu.py
+++ BOJ/priority_queue/11279-최대_힙/11279-최대_힙-seungkyu.py
@@ -1,1 +1,16 @@
-# git commit -m "Solve boj 11279 최대 힙 (seungkyu)"
+import heapq
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+max_heap = []
+for _ in range(n):
+    x = int(input())
+    if x == 0:
+        if max_heap:
+            print((-1)*heapq.heappop(max_heap))  #  -1 곱해서 max heap만듬
+        else:
+            print(0)
+    else:
+        heapq.heappush(max_heap, -x)

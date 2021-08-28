8bf71441ed84cd7ebd31f0768977e236d35c3e89
hing9u <yeonheechae425@gmail.com>
2021-08-29 01:51:10 +0900

    code: Solve boj 11399 ATM (yeonhee)

--- BOJ/greedy/11399-ATM/11399-ATM-yeonhee.py
+++ BOJ/greedy/11399-ATM/11399-ATM-yeonhee.py
@@ -1,1 +1,14 @@
-# git commit -m "Solve boj 11399 ATM (yeonhee)"
+N = int(input())
+p = list(map(int, input().split()))
+waiting = 0
+result = 0
+
+for i in sorted(p):
+    waiting += i
+    result += waiting
+
+print(result)
+
+"""
+오랜만에 행복한 문제네요...
+"""

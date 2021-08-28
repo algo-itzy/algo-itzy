8e750adbb731a7f277ff3d7fb87d38c44984185a
hing9u <yeonheechae425@gmail.com>
2021-08-29 01:47:22 +0900

    code: Solve boj 02630 색종이 만들기 (yeonhee)

--- BOJ/divide_and_conquer/02630-색종이_만들기/02630-색종이_만들기-yeonhee.py
+++ BOJ/divide_and_conquer/02630-색종이_만들기/02630-색종이_만들기-yeonhee.py
@@ -1,1 +1,29 @@
-# git commit -m "Solve boj 02630 색종이 만들기 (yeonhee)"
+def cut_paper(row, col, n):
+    global white, blue
+
+    paper_sum = 0
+    for r in range(row, row+n):
+        for c in range(col, col+n):
+            paper_sum += paper[r][c]
+
+    if paper_sum == n*n:
+        blue += 1
+
+    elif paper_sum == 0:
+        white += 1
+
+    else:
+        cut_paper(row, col, n//2)
+        cut_paper(row + n//2, col, n//2)
+        cut_paper(row, col + n//2, n//2)
+        cut_paper(row + n//2, col + n//2, n//2)
+
+
+N = int(input())
+paper = [list(map(int, input().split())) for _ in range(N)]
+
+white, blue = 0, 0
+
+cut_paper(0, 0, N)
+print(white)
+print(blue)

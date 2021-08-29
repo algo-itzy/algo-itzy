41549b27407b7958fe9ef397d65a9f706643892e
위연주 <thizzyzz@naver.com>
2021-08-29 10:58:33 +0900

    Solve boj 02630 색종이 만들기 (yeonju)

--- BOJ/divide_and_conquer/02630-색종이_만들기/02630-색종이_만들기-yeonju.py
+++ BOJ/divide_and_conquer/02630-색종이_만들기/02630-색종이_만들기-yeonju.py
@@ -26,39 +26,8 @@ print(num.count(0)) # 흰색
 print(num.count(1)) # 파란색
 
 
-
-
-
-# n = int(input()) # 한 변의 길이 n
-
-# arr = [list(map(int, input().split())) for _ in range(n)]
-# white = 0
-# blue = 0
-# num = []
-
-# def div(x, y, n):
-#     global white, blue
-#     color = arr[x][y]
-#     for i in range(x, x+n):
-#         for j in range(y, y+n):
-#             if color != arr[x][y]:
-#                 div(x, y, n//2)
-#                 div(x, y+n//2, n//2)
-#                 div(x+n//2, y, n//2)
-#                 div(x+n//2, y+n//2, n//2)
-#                 return 
-
-#     if color == 0:
-#         white+= 1
-#     else:
-#         blue += 1
-
-# div(0,0,n)
-
-# print(white) # 흰색 
-# print(blue) # 파란색
-
 # [row, column][row, column+ n//2]
 # [row+ n//2][row + n//2 , coluumn + n//2]
+
 # git commit -m "Solve boj 02630 색종이 만들기 (yeonju)"
 

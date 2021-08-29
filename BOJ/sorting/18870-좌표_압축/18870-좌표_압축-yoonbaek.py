c5290a35e4ec08b69002056a3bcc951561badc9b
YoonBaek <royal729@naver.com>
2021-08-30 00:06:53 +0900

    code: Solve boj 18870 좌표 압축 (yoonbaek)

--- BOJ/sorting/18870-좌표_압축/18870-좌표_압축-yoonbaek.py
+++ BOJ/sorting/18870-좌표_압축/18870-좌표_압축-yoonbaek.py
@@ -1,1 +1,18 @@
+# solved by YoonBaek
+if __name__ == "__main__":
+    N = int(input())
+    numbers = list(map(int, input().split()))
+    # 중복을 제거하고 정렬합니다.
+    duple_check = sorted(list(set(numbers)))
+
+    # duple_check의 인덱스와 값으로 hash를 구성합니다.
+    HASH = {}
+    for idx, val in enumerate(duple_check):
+        HASH[val] = idx
+
+    # 해쉬를 참조해 결과 출력
+    f = lambda x: str(HASH[x])
+
+    print(" ".join(map(f, numbers)))
+
 # git commit -m "code: Solve boj 18870 좌표 압축 (yoonbaek)"

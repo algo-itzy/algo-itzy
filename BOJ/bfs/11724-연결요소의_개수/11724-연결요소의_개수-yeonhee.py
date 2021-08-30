d429e7da7608f6abeb8f1651c295647359c9341f
hing9u <yeonheechae425@gmail.com>
2021-08-30 19:50:58 +0900

    code: Solve boj 11724 연결요소의 개수 (yeonhee)

--- BOJ/bfs/11724-연결요소의_개수/11724-연결요소의_개수-yeonhee.py
+++ BOJ/bfs/11724-연결요소의_개수/11724-연결요소의_개수-yeonhee.py
@@ -1,1 +1,31 @@
-# git commit -m "code: Solve boj 11724 연결요소의 개수 (yeonhee)"
+import sys
+from collections import defaultdict
+
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+
+def dfs(v):
+    visited[v] = 1
+    for w in graph[v]:
+        if not visited[w]:
+            dfs(w)
+
+
+N, M = map(int, input().split())
+graph = defaultdict(list)
+
+for _ in range(M):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+visited = [0] * (N+1)
+cnt = 0
+
+for i in range(1, N+1):
+    if not visited[i]:
+        dfs(i)
+        cnt += 1
+
+print(cnt)

fb1166699e22c163ee7962ce0c0acfd6ef49e555
seokzin <seokzin95@gmail.com>
2021-08-30 23:57:54 +0900

    code: Solve boj 18870 좌표 압축 (seokzin)

--- BOJ/sorting/18870-좌표_압축/18870-좌표_압축-seokzin.py
+++ BOJ/sorting/18870-좌표_압축/18870-좌표_압축-seokzin.py
@@ -1,1 +1,13 @@
-# git commit -m "code: Solve boj 18870 좌표 압축 (seokzin)"
+n = int(input())
+
+li = list(map(int, input().split()))
+
+li2 = sorted(set(li))
+
+dic = {li2[i] : i for i in range(len(li2))}
+# dic = { val: idx for idx, val in enumerate(li2)}
+
+for i in li:
+    print(dic[i], end= " ")
+
+# enumerate로 인덱스 붙이기 가능 - 튜플로 생성됨. 다만 위보다 좀 느림

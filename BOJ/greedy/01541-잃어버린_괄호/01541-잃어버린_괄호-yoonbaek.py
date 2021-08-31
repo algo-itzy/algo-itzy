5cc9a1bc7fcfdac0769bffa4a7050689d3cd4655
YoonBaek <royal729@naver.com>
2021-08-31 17:58:48 +0900

    code: Solve boj 01541 잃어버린 괄호 (yoonbaek)

--- BOJ/greedy/01541-잃어버린_괄호/01541-잃어버린_괄호-yoonbaek.py
+++ BOJ/greedy/01541-잃어버린_괄호/01541-잃어버린_괄호-yoonbaek.py
@@ -1,1 +1,20 @@
+# 잃어버린 괄호
+# 플러스 끼리 묶어서 더한 값을 죄다 빼기만 하면 결과가 나오는 문제였습니다.
+
+if __name__ == "__main__":
+    note = input()
+    exprs = note.split("-")
+    length = len(exprs)
+
+    minimum = 0
+    for i in range(length):
+        exprs[i] = sum(map(int, exprs[i].split("+")))
+        if i == 0:
+            minimum = exprs[i]
+        else:
+            minimum -= exprs[i]
+
+    print(minimum)    
+
+
 # git commit -m "code: Solve boj 01541 잃어버린 괄호 (yoonbaek)"

e2b91a9785580a4af3c6c1e7e6302c327c80f199
chaselover <gkstmdwn234@gmail.com>
2021-08-31 15:44:00 +0900

    code: Solve boj 01260 DFS와 BFS (seungjoo)

--- BOJ/dfs/01260-DFS와_BFS/01260-DFS와_BFS-seungjoo.py
+++ BOJ/dfs/01260-DFS와_BFS/01260-DFS와_BFS-seungjoo.py
@@ -1,1 +1,47 @@
 # git commit -m "code: Solve boj 01260 DFS와 BFS (seungjoo)"
+import sys
+input = sys.stdin.readline
+from collections import defaultdict
+
+
+def DFS(v):
+    path.append(v)
+    check[v] = True
+    for i in graph[v]:
+        if check[i]==False:
+            DFS(i)
+
+
+def BFS(v):
+    queue = [v]
+    path.append(v)
+    check[v] = True
+    while queue:
+        v = queue.pop(0)
+        for i in graph[v]:
+            if check[i] ==False:
+                queue.append(i)
+                path.append(i)
+                check[i] = True
+
+
+N,M,V = list(map(int,input().split()))
+graph = defaultdict(list)
+
+for _ in range(M):
+    a,b = map(int,input().split())
+    graph[a] += [b]
+    graph[b] += [a]
+
+for i in range(1,N+1):
+    graph[i].sort()
+
+path = []
+check = [True] +[False]*(N)
+DFS(V)
+print(*path)
+
+check = [True] +[False]*(N)
+path.clear()
+BFS(V)
+print(*path)

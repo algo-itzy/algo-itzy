0a4296e7a9143c02aa658fb2413f081b31868683
chaselover <gkstmdwn234@gmail.com>
2021-08-31 15:19:53 +0900

    code: Solve boj 11726 2xn 타일링 (seungjoo)

--- BOJ/dp/11726-2xn_타일링/11726-2xn_타일링-seungjoo.py
+++ BOJ/dp/11726-2xn_타일링/11726-2xn_타일링-seungjoo.py
@@ -1,1 +1,15 @@
-# git commit -m "code: Solve boj 11726 2xn 타일링 (seungjoo)"
+import sys
+input = sys.stdin.readline
+n = int(input())
+
+dp = [0]*(n+1)
+
+dp[0] = 0
+dp[1] = 1
+if n>=2:
+    dp[2] = 2
+if n>=3:
+    for i in range(3,n+1):
+        dp[i] = dp[i-1] + dp[i-2]
+
+print(dp[n]%10007)

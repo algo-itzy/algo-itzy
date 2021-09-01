d6d64805aec0f74dacbaaf0b64ff32054e781076
서울_4반_정지웅 <j2woong1@gmail.com>
2021-09-01 23:19:05 +0900

    code: solve boj 18870 좌표_압축 (jiwoong)

--- BOJ/sorting/18870-좌표_압축/18870-좌표_압축-jiwoong.py
+++ BOJ/sorting/18870-좌표_압축/18870-좌표_압축-jiwoong.py
@@ -1,1 +1,13 @@
-# git commit -m "code: Solve boj 18870 좌표 압축 (jiwoong)"
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+arr = list(map(int, input().split()))
+arr2 = sorted(list(set(arr)))
+
+dic = {}
+for i in range(len(arr2)):
+    dic[arr2[i]] = i
+for i in arr:
+    print(dic[i], end=' ')

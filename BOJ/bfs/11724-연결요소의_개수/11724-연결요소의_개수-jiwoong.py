1a630fc819fb12005707523233ec42a8ac04aa78
서울_4반_정지웅 <j2woong1@gmail.com>
2021-09-01 23:17:27 +0900

    code: solve boj 11724 연결요소의_개수 (jiwoong)

--- BOJ/bfs/11724-연결요소의_개수/11724-연결요소의_개수-jiwoong.py
+++ BOJ/bfs/11724-연결요소의_개수/11724-연결요소의_개수-jiwoong.py
@@ -1,1 +1,32 @@
-# git commit -m "code: Solve boj 11724 연결요소의 개수 (jiwoong)"
+from collections import deque
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+visited = [False] * (N + 1)
+graph = [[] for _ in range(N + 1)]
+
+for _ in range(M):
+    n1, n2 = map(int, sys.stdin.readline().split())
+    graph[n1].append(n2)
+    graph[n2].append(n1)
+
+
+def bfs(x):
+    visited[x] = 1
+    q = deque([x])
+
+    while q:
+        v = q.popleft()
+        for nodes in graph[v]:
+            if not visited[nodes]:
+                visited[nodes] = True
+                q.append(nodes)
+
+
+ans = 0
+for i in range(1, N + 1):
+    if not visited[i]:
+        ans += 1
+        bfs(i)
+
+print(ans)

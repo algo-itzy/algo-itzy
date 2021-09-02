26b6dec3dda9b18dfdf13929bf3b4c348505f247
ed-kyu <seungkyukim.dev@gmail.com>
2021-09-02 15:25:53 +0900

    code: Solve boj 11726 2xn 타일링 (seungkyu)

--- BOJ/dp/11726-2xn_타일링/11726-2xn_타일링-seungkyu.py
+++ BOJ/dp/11726-2xn_타일링/11726-2xn_타일링-seungkyu.py
@@ -1,1 +1,9 @@
-# git commit -m "code: Solve boj 11726 2xn 타일링 (seungkyu)"
+import sys
+input = sys.stdin.readline
+# 시간 초과 안나도록 조심
+dp = [0, 1, 2]
+for i in range(3, 1001):
+    dp.append(dp[i-1] + dp[i-2])
+
+n = int(input())
+print(dp[n]%10007)

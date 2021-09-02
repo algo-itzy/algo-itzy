56c048bbf4edc562d34d721870dc460fc6fba3dd
hing9u <yeonheechae425@gmail.com>
2021-09-02 12:28:22 +0900

    code: Solve boj 01389 케빈 베이컨의 6단계 법칙 (yeonhee)

--- BOJ/bfs/01389-케빈_베이컨의_6단계_법칙/01389-케빈_베이컨의_6단계_법칙-yeonhee.py
+++ BOJ/bfs/01389-케빈_베이컨의_6단계_법칙/01389-케빈_베이컨의_6단계_법칙-yeonhee.py
@@ -1,1 +1,34 @@
-# git commit -m "code: Solve boj 01389 케빈 베이컨의 6단계 법칙 (yeonhee)"
+from collections import deque, defaultdict
+
+
+def bfs(v):
+    bacon = [0] * (N+1)
+    visited = [0] * (N+1)
+    visited[v] = 1
+    q = deque([v])
+
+    while q:
+        v = q.popleft()
+        for w in graph[v]:
+            if not visited[w]:
+                bacon[w] = bacon[v] + 1
+                q.append(w)
+                visited[w] = 1
+
+    return sum(bacon)
+
+
+N, M = map(int, input().split())
+graph = defaultdict(list)
+
+for _ in range(M):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+result = []
+
+for i in range(1, N+1):
+    result.append(bfs(i))
+
+print(result.index(min(result))+1)

6856891780cb32a7e84bb9023236b3827aeb730c
ed-kyu <seungkyukim.dev@gmail.com>
2021-09-02 15:28:43 +0900

    code: Solve boj 18870 좌표 압축 (seungkyu)

--- BOJ/sorting/18870-좌표_압축/18870-좌표_압축-seungkyu.py
+++ BOJ/sorting/18870-좌표_압축/18870-좌표_압축-seungkyu.py
@@ -1,1 +1,20 @@
-# git commit -m "code: Solve boj 18870 좌표 압축 (seungkyu)"
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+numbers = list(set(map(int, input().split())))
+numbers.sort()
+
+ans = []
+num_dict = {}
+
+# 1바퀴 돌면서 작은 순서대로 인덱스값 지정해주기
+for idx, num in enumerate(numbers):
+    num_dict[num] = idx
+
+# dictionary에서 값 찾아서 출력
+for num in numbers:
+    ans.append(num_dict[num])
+
+print(*ans)

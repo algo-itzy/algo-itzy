84bcc57b60715f96bf74ac68df2ad42b3f3cdef0
chaselover <gkstmdwn234@gmail.com>
2021-09-03 20:24:36 +0900

    code: Solve boj 02579 계단 오르기 (seungjoo)

--- BOJ/dp/02579-계단_오르기/02579-계단_오르기-seungjoo.py
+++ BOJ/dp/02579-계단_오르기/02579-계단_오르기-seungjoo.py
@@ -1,1 +1,13 @@
-# git commit -m "code: Solve boj 02579 계단 오르기 (seungjoo)"
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+score =[0] + [int(input()) for _ in range(n)]
+dp=[0,score[1]]
+if n >= 2:
+    dp.append(score[1]+score[2])
+if n >= 3:
+    for i in range(3,n+1): 
+        dp.append(max(dp[i-2]+score[i],dp[i-3]+score[i]+score[i-1]))
+
+print(dp[n])

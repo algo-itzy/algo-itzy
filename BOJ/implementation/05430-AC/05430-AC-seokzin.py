daf34f48e84356dc6a5536698d58ed3f70ba9794
seokzin <seokzin95@gmail.com>
2021-09-04 00:08:22 +0900

    code: Solve boj 05430 AC (seokzin)

--- BOJ/implementation/05430-AC/05430-AC-seokzin.py
+++ BOJ/implementation/05430-AC/05430-AC-seokzin.py
@@ -1,1 +1,26 @@
-# git commit -m "code: Solve boj 05430 AC (seokzin)"
+for _ in range(int(input())):
+    p = input()
+    n = int(input())
+    arr = input()
+
+    if n == 0:
+        arr = list()
+    else:
+        arr = list(map(int, arr.replace('[', '').replace(']', '').split(',')))
+
+    flag = 0  # 뒤집기 체크 - 0 / -1
+
+    for cmd in p:
+        if cmd == 'R':
+            flag = -1-flag
+        elif cmd == 'D':
+            if not arr:
+                print('error')
+                break
+            else:
+                arr.pop(flag) # pop(0), pop(-1)            
+    else:
+        arr = arr if not flag else arr[::-1]
+        print('[', ','.join(map(str, arr)), ']', sep='')
+
+# 진짜 문제 너무너무 지저분해.. 내가 어렵게 푼건가..?

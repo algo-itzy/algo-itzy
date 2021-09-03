dec6981670d156b304e75db6301bab6d1d26ffb4
seokzin <seokzin95@gmail.com>
2021-09-04 00:10:42 +0900

    code: Solve boj 02178 미로 탐색 (seokzin)

--- BOJ/bfs/02178-미로_탐색/02178-미로_탐색-seokzin.py
+++ BOJ/bfs/02178-미로_탐색/02178-미로_탐색-seokzin.py
@@ -1,1 +1,27 @@
-# git commit -m "code: Solve boj 02178 미로 탐색 (seokzin)"
+from collections import deque
+
+def bfs():
+    D = ((1, 0), (0, 1), (-1, 0), (0, -1))
+    q = deque()
+
+    q.append((0, 0))
+    visit[0][0] = 1
+
+    while q:
+        x, y = q.popleft()
+
+        for d in D:
+            nx, ny = x+d[0], y+d[1]
+            
+            if 0 <= nx < n and 0 <= ny < m and arr[nx][ny] and not visit[nx][ny]:
+                visit[nx][ny] = visit[x][y] + 1
+                q.append([nx, ny])
+
+
+n, m = map(int, input().split())
+arr = [list(map(int, input())) for _ in range(n)]
+visit = [[0]*m for _ in range(n)]
+
+bfs()
+
+print(visit[-1][-1])

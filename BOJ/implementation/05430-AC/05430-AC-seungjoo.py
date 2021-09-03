10a507a5bf6bea1c16e82b6a4d54ceda06cf8035
chaselover <gkstmdwn234@gmail.com>
2021-09-03 21:24:28 +0900

    code: Solve boj 05430 AC (seungjoo)

--- BOJ/implementation/05430-AC/05430-AC-seungjoo.py
+++ BOJ/implementation/05430-AC/05430-AC-seungjoo.py
@@ -1,1 +1,30 @@
-# git commit -m "code: Solve boj 05430 AC (seungjoo)"
+import sys
+input = sys.stdin.readline
+
+for test in range(int(input())):
+    p = input().rstrip()
+    n = int(input())
+    xi = input().rstrip()[1:-1].split(',')
+    if not n and 'D' in p:
+        print('error')
+        continue
+    reverse_x = 0
+    left = 0
+    right = len(xi)-1
+    for command in p:
+        if command == 'R':
+            reverse_x ^= 1
+        else:
+            if left <= right:
+                if reverse_x:
+                    right -= 1
+                else:
+                    left += 1
+            else:
+                print('error')
+                break
+    else:
+        if reverse_x:
+            print(f"[{','.join(xi[left:right+1][::-1])}]")
+        else:
+            print(f"[{','.join(xi[left:right+1])}]")

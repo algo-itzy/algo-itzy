f173308f6868822065410b0020fded327f8ee8e5
서울_4반_정지웅 <j2woong1@gmail.com>
2021-09-03 23:26:31 +0900

    code: solve 1676 팩토리얼_0의_개수 (jiwoong)

--- BOJ/mathematics/01676-팩토리얼_0의_개수/01676-팩토리얼_0의_개수-jiwoong.py
+++ BOJ/mathematics/01676-팩토리얼_0의_개수/01676-팩토리얼_0의_개수-jiwoong.py
@@ -1,1 +1,25 @@
-# git commit -m "code: Solve boj 01676 팩토리얼 0의 개수 (jiwoong)"
+import sys
+
+input = sys.stdin.readline
+N = int(input())  # N!
+
+
+def fact(N):
+    if N == 1:
+        return 1
+    else:  # factorial 구현
+        num = N * fact(N - 1)
+        return num
+
+
+if N == 0:
+    print(0)
+else:
+    num = str(fact(N))  # 문자열 처리로 0의 개수 count
+    cnt = 0
+    for i in range(len(num) - 1, -1, -1):  # 뒤에서부터 거꾸로
+        if num[i] == "0":
+            cnt += 1
+        else:
+            break
+    print(cnt)

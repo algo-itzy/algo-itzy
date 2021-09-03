831afe9006a75b28c3f57a410307e113fbaf42b0
chaselover <gkstmdwn234@gmail.com>
2021-09-03 20:14:41 +0900

    code: Solve boj 02178 미로 탐색 (seungjoo)

--- BOJ/bfs/02178-미로_탐색/02178-미로_탐색-seungjoo.py
+++ BOJ/bfs/02178-미로_탐색/02178-미로_탐색-seungjoo.py
@@ -1,1 +1,27 @@
-# git commit -m "code: Solve boj 02178 미로 탐색 (seungjoo)"
+import sys
+input = sys.stdin.readline
+from collections import deque
+
+
+def BFS(x,y):
+    queue = deque()
+    queue.append([x,y])
+    matrix[x][y] = 1
+    while queue:
+        x,y = queue.popleft()
+        for k in range(4):
+            nx = x+dx[k]
+            ny = y+dy[k]
+            if 0<=nx<N and 0<=ny<M and matrix[nx][ny]==1:
+                matrix[nx][ny] = matrix[x][y]+1
+                queue.append([nx,ny])
+
+
+N,M = map(int,input().split())
+matrix = [list(map(int, input().rstrip())) for _ in range(N)]
+
+dx = [-1,1,0,0]
+dy = [0,0,1,-1]
+
+BFS(0,0)
+print(matrix[N-1][M-1])

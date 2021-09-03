56cc1ace4cd9a273cc4dcfde1ae6a8d3810e1ab8
seokzin <seokzin95@gmail.com>
2021-09-04 00:12:27 +0900

    code: Solve boj 01992 쿼드 트리 (seokzin)

--- BOJ/divide_and_conquer/01992-쿼드_트리/01992-쿼드_트리-seokzin.py
+++ BOJ/divide_and_conquer/01992-쿼드_트리/01992-쿼드_트리-seokzin.py
@@ -1,1 +1,27 @@
-# git commit -m "code: Solve boj 01992 쿼드 트리 (seokzin)"
+def div(x, y, n):
+    global res
+
+    start = arr[y][x]
+
+    for i in range(x, x+n):
+        for j in range(y, y+n):
+            if arr[j][i] != start: # 하나라도 틀리면 DnC
+
+                print('(', end="")
+                div(x, y, n//2)
+                div(x+n//2, y, n//2)
+                div(x, y+n//2, n//2)
+                div(x+n//2, y+n//2, n//2)
+                print(')', end="")
+
+                return
+    
+    print(start, end="")
+
+
+n = int(input())
+arr = [list(map(int, input())) for _ in range(n)]
+
+div(0, 0, n)
+
+# str.extend를 도입했다가 18줄 return none 때문에 꼬였습니다.

3a32b1e5a51f518af64e45d875902f170cecc462
서울_4반_정지웅 <j2woong1@gmail.com>
2021-09-03 23:58:25 +0900

    code: solve 2579 계단_오르기 (jiwoong)

--- BOJ/dp/02579-계단_오르기/02579-계단_오르기-jiwoong.py
+++ BOJ/dp/02579-계단_오르기/02579-계단_오르기-jiwoong.py
@@ -1,1 +1,18 @@
-# git commit -m "code: Solve boj 02579 계단 오르기 (jiwoong)"
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+num = [int(input()) for _ in range(N)]
+num.reverse()
+
+dp = [0] * N
+dp[0] = num[0]
+
+for i in range(1, N):
+    if i < 3:
+        dp[i] = num[0] + num[i]
+    else:
+        dp[i] = max(dp[i - 2] + num[i], dp[i - 3] + num[i - 1] + num[i])
+
+print(max(dp[N - 2], dp[N - 1]))

9358882e28c967810022009f9f5c328924e1dc34
YoonBaek <royal729@naver.com>
2021-09-04 15:46:20 +0900

    code: Solve boj 2178 미로탐색 2667 단지번호 (yoonbaek)

--- BOJ/bfs/02178-미로_탐색/02178-미로_탐색-yoonbaek.py
+++ BOJ/bfs/02178-미로_탐색/02178-미로_탐색-yoonbaek.py
@@ -1,1 +1,46 @@
+from sys import stdin
+from collections import deque
+
+# first-class function
+ints = lambda x : list(map(int, x))
+
+# move_directions
+MOVES = ((0, -1), (0, 1), (-1, 0), (1, 0))
+
+# functions
+def search(start):
+    moves = []
+    for x, y in MOVES:
+        next_x, next_y = start[0]+x, start[1]+y
+        if 0 <= next_x < M and 0 <= next_y < N:
+            if maze[next_y][next_x] == 1:
+                moves.append((next_x, next_y))
+
+    return moves
+                
+
+def BFS(start):
+    q = deque([start])
+
+    while q:
+        now = q.popleft()
+
+        moves = search(now)
+        for x, y in moves:
+            if maze[y][x] == 1:
+                # 최초점부터 끝점까지의 거리를 메모
+                maze[y][x] = maze[now[1]][now[0]]+1
+                q.append((x, y))
+
+
+if __name__ == "__main__":
+    N, M = ints(input().split())
+
+    maze = [ints(input()) for _ in range(N)]
+
+    BFS((0,0))
+    # 끝점의 거리를 출력
+    print(maze[N-1][M-1])
+
+
 # git commit -m "code: Solve boj 02178 미로 탐색 (yoonbaek)"
--- BOJ/bfs/02667-단지번호붙이기/02667-단지번호붙이기-yoonbaek.py
+++ BOJ/bfs/02667-단지번호붙이기/02667-단지번호붙이기-yoonbaek.py
@@ -1,1 +1,55 @@
+import sys
+from collections import deque
+
+read = sys.stdin.read
+
+MOVES = ((-1, 0), (1, 0), (0, -1), (0, 1))
+
+def search(start):
+    moves = []
+    for x, y in MOVES:
+        next_x, next_y = start[0]+x, start[1]+y
+        if 0 <= next_x < N and 0 <= next_y < N:
+            if 지도[next_y][next_x] == 1:
+                moves.append((next_x, next_y))
+
+    return moves
+
+def BFS(start):
+    q = deque([start])
+    # check visited
+    지도[start[1]][start[0]] = 0
+    단지수 = 0
+
+    while q:
+        now_x, now_y = q.popleft()
+        단지수 += 1
+        
+        moves = search((now_x, now_y))
+        for next_x, next_y in moves:
+            if 지도[next_y][next_x]:
+                지도[next_y][next_x] = 0
+                q.append((next_x, next_y))
+
+    return 단지수
+
+if __name__ == "__main__":
+    rd = read().split()
+    
+    N = int(rd[0])
+    지도 = [list(map(int, r)) for r in rd[1:]]
+
+    단지수들 = []
+
+    for 행 in range(N):
+        for 열 in range(N):
+            if 지도[행][열]:
+                단지수들.append(BFS((열, 행)))
+    
+    단지수들.sort()
+
+    print(len(단지수들))
+    print("\n".join(map(str, 단지수들)))
+                
+
 # git commit -m "code: Solve boj 02667 단지번호붙이기 (yoonbaek)"

6f233aa046f8a0d4bcdf8a80af1aff73661eb0c7
hing9u <yeonheechae425@gmail.com>
2021-09-05 09:37:21 +0900

    code: Solve boj 01780 종이의 개수 (yeonhee)

--- BOJ/divide_and_conquer/01780-종이의_개수/01780-종이의_개수-yeonhee.py
+++ BOJ/divide_and_conquer/01780-종이의_개수/01780-종이의_개수-yeonhee.py
@@ -1,1 +1,32 @@
-# git commit -m "code: Solve boj 01780 종이의 개수 (yeonhee)"
+import sys
+input = sys.stdin.readline
+
+
+def cut_out_paper(x, y, n):
+    global minus, zero, plus
+
+    tmp = paper[x][y]
+    for i in range(x, x+n):
+        for j in range(y, y+n):
+            if paper[i][j] != tmp:
+                for k in range(3):
+                    for l in range(3):
+                        cut_out_paper(x + k*n//3, y + l*n//3, n//3)
+                return
+
+    if tmp == -1:
+        minus += 1
+    elif tmp == 0:
+        zero += 1
+    else:
+        plus += 1
+
+
+N = int(input())
+paper = [list(map(int, input().split())) for _ in range(N)]
+
+minus, zero, plus = 0, 0, 0
+
+cut_out_paper(0, 0, N)
+
+print(minus, zero, plus, sep='\n')

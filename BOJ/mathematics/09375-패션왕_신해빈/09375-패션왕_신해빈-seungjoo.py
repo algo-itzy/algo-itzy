b67c2e593f49a7d8ed5a2c8d07df86f41fab8378
chaselover <gkstmdwn234@gmail.com>
2021-09-07 03:50:30 +0900

    code: Solve boj 09375 패션왕 신해빈 (seungjoo)

--- BOJ/mathematics/09375-패션왕_신해빈/09375-패션왕_신해빈-seungjoo.py
+++ BOJ/mathematics/09375-패션왕_신해빈/09375-패션왕_신해빈-seungjoo.py
@@ -1,1 +1,15 @@
 # git commit -m "code: Solve boj 09375 패션왕 신해빈 (seungjoo)"
+import sys
+input = sys.stdin.readline
+from collections import defaultdict
+
+for _ in range(int(input())):
+    n = int(input())
+    clothes = defaultdict(int)
+    ans = 1
+    for _ in range(n):
+        name, type_c = input().split()
+        clothes[type_c] += 1
+    for cloth in clothes:
+        ans *= clothes[cloth]+1
+    print(ans-1)

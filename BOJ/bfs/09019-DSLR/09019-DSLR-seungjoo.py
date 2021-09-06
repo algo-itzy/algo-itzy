b54ebb8f69f23dceee619d5f05eedc83777b27b3
chaselover <gkstmdwn234@gmail.com>
2021-09-07 03:30:32 +0900

    code: Solve boj 09019 DSLR (seungjoo)

--- BOJ/bfs/09019-DSLR/09019-DSLR-seungjoo.py
+++ BOJ/bfs/09019-DSLR/09019-DSLR-seungjoo.py
@@ -1,1 +1,32 @@
 # git commit -m "code: Solve boj 09019 DSLR (seungjoo)"
+import sys
+input = sys.stdin.readline
+from collections import deque
+
+def bfs(start):
+    visited[start] = True
+    q = deque()
+    q.append([start,''])
+    while q:
+        cur_register, commands = q.popleft()
+        if cur_register==target:
+            return commands
+        for command in 'DSLR':
+            if command=='D':
+                next_register = (cur_register*2)%10000
+            elif command=='S':
+                next_register = (cur_register-1)%10000
+            elif command=='L':
+                next_register = (cur_register%1000)*10 + cur_register//1000
+            else:
+                next_register = cur_register//10 + (cur_register%10)*1000
+            if not visited[next_register]:
+                visited[next_register] = True
+                q.append([next_register,commands+command])
+
+
+
+for _ in range(int(input())):
+    start, target = map(int, input().split())
+    visited = {i: False for i in range(10000)}
+    print(bfs(start))

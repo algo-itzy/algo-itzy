4d704a70c8a85b30ec6e1a42aea843fff3daee71
chaselover <gkstmdwn234@gmail.com>
2021-09-07 03:58:09 +0900

    code: Solve boj 11047 동전 0 (seungjoo)

--- BOJ/greedy/11047-동전_0/11047-동전_0-seungjoo.py
+++ BOJ/greedy/11047-동전_0/11047-동전_0-seungjoo.py
@@ -1,1 +1,11 @@
 # git commit -m "code: Solve boj 11047 동전 0 (seungjoo)"
+import sys
+input = sys.stdin.readline
+
+N, K = map(int, input().split())
+coins = [int(input()) for _  in range(N)]
+cnt = 0
+for i in range(1,N+1):
+    cnt += K // coins[-i]
+    K = K % coins[-i]
+print(cnt)

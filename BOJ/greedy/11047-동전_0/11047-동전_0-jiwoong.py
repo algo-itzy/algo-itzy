89e64a4a36637685be27a38001baad2c0d638db9
서울_4반_정지웅 <j2woong1@gmail.com>
2021-09-07 21:19:41 +0900

    code: solve 11047 동전_0 (jiwoong)

--- BOJ/greedy/11047-동전_0/11047-동전_0-jiwoong.py
+++ BOJ/greedy/11047-동전_0/11047-동전_0-jiwoong.py
@@ -1,1 +1,16 @@
-# git commit -m "code: Solve boj 11047 동전 0 (jiwoong)"
+import sys
+
+input = sys.stdin.readline
+
+N, K = map(int, input().split())
+A = []
+for _ in range(N):
+    A.append(int(input()))
+ans = 0
+for i in range(N - 1, -1, -1):
+    if K == 0:
+        break
+    if K >= A[i]:
+        ans = ans + K // A[i]
+        K = K % A[i]
+print(ans)

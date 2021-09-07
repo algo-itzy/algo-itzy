7b97462e017c02f0e9886ff9fc3be3e294bd726e
서울_4반_정지웅 <j2woong1@gmail.com>
2021-09-07 21:15:29 +0900

    code: solve 6064 카잉_달력 (jiwoong)

--- BOJ/mathematics/06064-카잉_달력/06064-카잉_달력-jiwoong.py
+++ BOJ/mathematics/06064-카잉_달력/06064-카잉_달력-jiwoong.py
@@ -1,1 +1,13 @@
-# git commit -m "code: Solve boj 06064 카잉 달력 (jiwoong)"
+from math import gcd
+
+T = int(input())
+
+for _ in range(T):
+    M, N, x, y = map(int, input().split())
+    for i in range(int(N / gcd(M, N))):
+        ans = x + M * i
+        if ans % N == y % N:
+            break
+        else:
+            ans = -1
+    print(ans)

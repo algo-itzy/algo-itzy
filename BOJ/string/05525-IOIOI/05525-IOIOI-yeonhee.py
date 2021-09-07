21dd7d57bfeeaf8a42347905ff3764638f3767da
devpla <chaeyeonhee@kakao.com>
2021-09-07 23:18:52 +0900

    code: Solve boj 05525 IOIOI (yeonhee)

--- BOJ/string/05525-IOIOI/05525-IOIOI-yeonhee.py
+++ BOJ/string/05525-IOIOI/05525-IOIOI-yeonhee.py
@@ -1,1 +1,20 @@
-# git commit -m "code: Solve boj 05525 IOIOI (yeonhee)"
+N = int(input())
+M = int(input())
+S = input()
+
+pattern = 0
+cnt = 0
+i = 1
+
+while i < M - 1:
+    if S[i - 1:i + 2] == 'IOI':
+        pattern += 1
+        if pattern == N:
+            cnt += 1
+            pattern -= 1
+        i += 2
+    else:
+        pattern = 0
+        i += 1
+
+print(cnt)

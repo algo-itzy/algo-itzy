2ec21239b2a1a80eb2f24881d4c2a948679334d1
서울_4반_정지웅 <j2woong1@gmail.com>
2021-09-07 21:14:44 +0900

    code: solve boj 5525 IOIOI (jiwoong)

--- BOJ/string/05525-IOIOI/05525-IOIOI-jiwoong.py
+++ BOJ/string/05525-IOIOI/05525-IOIOI-jiwoong.py
@@ -1,1 +1,24 @@
-# git commit -m "code: Solve boj 05525 IOIOI (jiwoong)"
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+M = int(input())
+S = list(input())
+
+cnt = 0
+ans = 0
+idx = 1
+
+while idx < M-1:
+    if S[idx-1] == 'I' and S[idx] == 'O' and S[idx+1] == 'I':
+        cnt += 1
+        if cnt == N:
+            cnt -= 1
+            ans += 1
+        idx += 1
+    else:
+        cnt = 0
+    idx += 1
+
+print(ans)

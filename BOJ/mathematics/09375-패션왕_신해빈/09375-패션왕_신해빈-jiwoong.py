b6fd439195a47b8da6685d56ae74939626695233
서울_4반_정지웅 <j2woong1@gmail.com>
2021-09-07 21:17:18 +0900

    code: solve 9375 패션왕_신해빈 (jiwoong)

--- BOJ/mathematics/09375-패션왕_신해빈/09375-패션왕_신해빈-jiwoong.py
+++ BOJ/mathematics/09375-패션왕_신해빈/09375-패션왕_신해빈-jiwoong.py
@@ -1,1 +1,24 @@
-# git commit -m "code: Solve boj 09375 패션왕 신해빈 (jiwoong)"
+import sys
+
+input = sys.stdin.readline
+
+tc = int(input())
+
+for _ in range(tc):
+    N = int(input())  # 의상 수
+    kinds = []  # 종류
+    cnt = []  # 종류별 개수
+
+    for _ in range(N):
+        name, kind = input().split()
+        if kind in kinds:  # 이미 cnt 했으면
+            cnt[kinds.index(kind)] += 1
+        else:  # 안했으면
+            kinds.append(kind)
+            cnt.append(1)
+
+    ans = 1
+    for i in cnt:
+        ans *= i + 1
+    ans -= 1
+    print(ans)

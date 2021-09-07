f9a7daf357e6a512a37a175100cd60fab04550e7
서울_4반_정지웅 <j2woong1@gmail.com>
2021-09-07 21:18:04 +0900

    code: solve 9461 파도반_수열 (jiwoong)

--- BOJ/dp/09461-파도반_수열/09461-파도반_수열-jiwoong.py
+++ BOJ/dp/09461-파도반_수열/09461-파도반_수열-jiwoong.py
@@ -1,1 +1,12 @@
-# git commit -m "code: Solve boj 09461 파도반 수열 (jiwoong)"
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    dp = [0, 1, 1, 1, 2, 2]
+
+    for i in range(6, N + 1):
+        dp.append(dp[i - 1] + dp[i - 5])
+
+    print(dp[N])

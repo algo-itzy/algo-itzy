ee5aea3ec7705df7b0e66b573a2ada55d23e2268
YoonBaek <royal729@naver.com>
2021-09-09 17:33:28 +0900

    code: Solve boj 11047 동전 0 (yoonbaek)

--- BOJ/greedy/11047-동전_0/11047-동전_0-yoonbaek.py
+++ BOJ/greedy/11047-동전_0/11047-동전_0-yoonbaek.py
@@ -1,1 +1,18 @@
+from sys import stdin
+
+rd = stdin.readline
+
+if __name__ == "__main__":
+    N, K = map(int, rd().split())
+    coins = [int(rd()) for _ in range(N)]
+
+    cnt = 0
+    for i in range(N-1, -1, -1):
+        now = coins[i]
+        if now > K:
+            continue
+        cnt += K // now
+        K %= now
+
+    print(cnt)
 # git commit -m "code: Solve boj 11047 동전 0 (yoonbaek)"

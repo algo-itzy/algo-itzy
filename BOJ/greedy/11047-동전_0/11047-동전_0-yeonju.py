9df4acc9aeeb3535abece4c68c6c4b29c2c549e7
위연주 <thizzyzz@naver.com>
2021-09-09 17:19:35 +0900

    code: Solve boj 11047 동전 0 (yeonju)

--- BOJ/greedy/11047-동전_0/11047-동전_0-yeonju.py
+++ BOJ/greedy/11047-동전_0/11047-동전_0-yeonju.py
@@ -1,1 +1,20 @@
+import sys
+
+input = sys.stdin.readline
+
+n,k = map(int, input().split())     # k원 만들기
+
+a =[0]*(n+1)
+amount = k
+cnt = 0
+for i in range(1, n+1):
+    a[i]=int(input())
+
+for i in range(n,0,-1):             # 큰 단위부터 빼주기 
+    if amount >= a[i]:
+       cnt += amount//a[i]
+       amount -= (a[i]* (amount//a[i]))
+       
+print(cnt)
+
 # git commit -m "code: Solve boj 11047 동전 0 (yeonju)"

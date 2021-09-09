a8a0f6fd3b867f264c1af238e4e2838d79b8548f
위연주 <thizzyzz@naver.com>
2021-09-09 17:13:41 +0900

    code: Solve boj 05525 IOIOI (yeonju)

--- BOJ/string/05525-IOIOI/05525-IOIOI-yeonju.py
+++ BOJ/string/05525-IOIOI/05525-IOIOI-yeonju.py
@@ -1,1 +1,19 @@
+# 50점- 시간 초과
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+m = int(input()) # s 의 길이
+s = input()
+
+pn = 'IO'*n + 'I'
+cnt = 0
+
+for i in range(m-len(pn)+1):
+    if pn == s[i:i+len(pn)]:
+        cnt += 1
+
+print(cnt)
+
 # git commit -m "code: Solve boj 05525 IOIOI (yeonju)"

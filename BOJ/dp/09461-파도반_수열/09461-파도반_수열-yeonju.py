c94d0990b82c0931b52b73b316d36086943cd887
위연주 <thizzyzz@naver.com>
2021-09-09 17:16:27 +0900

    code: Solve boj 10026 적록색약 (yeonju)

--- BOJ/dp/09461-파도반_수열/09461-파도반_수열-yeonju.py
+++ BOJ/dp/09461-파도반_수열/09461-파도반_수열-yeonju.py
@@ -1,1 +1,15 @@
+t = int(input())
+
+for tc in range(t):
+    n = int(input())
+
+    dp = [1, 1, 1, 2, 2]
+
+    if n>5:
+        for i in range(5,n):
+            dp.append(dp[i-5] + dp[i-1])
+
+    print(dp[n-1])
+
+
 # git commit -m "code: Solve boj 09461 파도반 수열 (yeonju)"

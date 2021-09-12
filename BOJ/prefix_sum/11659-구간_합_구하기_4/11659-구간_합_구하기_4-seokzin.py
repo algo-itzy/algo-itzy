dc576c4e8927d9ef3e3c9f220bf94fea777d6beb
seokzin <seokzin95@gmail.com>
2021-09-12 19:08:33 +0900

    code: Solve boj 11659 구간 합 구하기 4 (seokzin)

--- BOJ/prefix_sum/11659-구간_합_구하기_4/11659-구간_합_구하기_4-seokzin.py
+++ BOJ/prefix_sum/11659-구간_합_구하기_4/11659-구간_합_구하기_4-seokzin.py
@@ -1,1 +1,13 @@
-# git commit -m "code: Solve boj 11659 구간 합 구하기 4 (seokzin)"
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+presum = [0] * (n+1)  # 부분합
+
+for i in range(n):
+    presum[i+1] = presum[i] + arr[i]
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    print(presum[b] - presum[a-1])

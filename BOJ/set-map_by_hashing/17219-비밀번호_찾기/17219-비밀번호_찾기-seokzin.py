685e43394c8ae020e49871101900719f2b1f0111
seokzin <seokzin95@gmail.com>
2021-09-12 19:12:00 +0900

    code: Solve boj 17219 비밀번호 찾기 (seokzin)

--- BOJ/set-map_by_hashing/17219-비밀번호_찾기/17219-비밀번호_찾기-seokzin.py
+++ BOJ/set-map_by_hashing/17219-비밀번호_찾기/17219-비밀번호_찾기-seokzin.py
@@ -1,1 +1,8 @@
-# git commit -m "code: Solve boj 17219 비밀번호 찾기 (seokzin)"
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+dic = dict(input().split() for _ in range(n))  # arr -> dict
+
+for _ in range(m):
+    print(dic[input().rstrip()])

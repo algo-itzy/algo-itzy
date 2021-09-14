6466c6f089f76fad4542c4b3cd28496e10e89bbc
chaselover <gkstmdwn234@gmail.com>
2021-09-15 00:52:34 +0900

    code: Solve boj 11286 11403 11659 14500 16928 17219 17626

--- BOJ/bfs/11403-경로_찾기/11403-경로_찾기-seungjoo.py
+++ BOJ/bfs/11403-경로_찾기/11403-경로_찾기-seungjoo.py
@@ -1,1 +1,16 @@
 # git commit -m "code: Solve boj 11403 경로 찾기 (seungjoo)"
+import sys
+input = sys.stdin.readline
+
+def dfs(v):
+    for i in range(n):
+        if visited[i] == 0 and matrix[v][i] == 1:
+            visited[i] = 1
+            dfs(i)
+
+n = int(input())
+matrix = [list(map(int, input().split())) for _ in range(n)]
+for i in range(n):
+    visited = [0 for _ in range(n)]
+    dfs(i)
+    print(*visited)
--- BOJ/set-map_by_hashing/17219-비밀번호_찾기/17219-비밀번호_찾기-seungjoo.py
+++ BOJ/set-map_by_hashing/17219-비밀번호_찾기/17219-비밀번호_찾기-seungjoo.py
@@ -1,1 +1,11 @@
 # git commit -m "code: Solve boj 17219 비밀번호 찾기 (seungjoo)"
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+dic = {}
+for _ in range(N):
+    address, pw = input().split()
+    dic[address] = pw
+for _ in range(M):
+    print(dic[input().rstrip()])

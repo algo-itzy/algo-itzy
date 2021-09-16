72197500ed0f6e070882a0910de5d664af00c809
위연주 <thizzyzz@naver.com>
2021-09-16 18:34:53 +0900

    code: Solve boj 11403 경로 찾기 (yeonju)

--- BOJ/bfs/11403-경로_찾기/11403-경로_찾기-yeonju.py
+++ BOJ/bfs/11403-경로_찾기/11403-경로_찾기-yeonju.py
@@ -1,1 +1,23 @@
+import sys
+input = sys.stdin.readline
+
+def dfs(v):
+    for i in range(n):
+        if res[i] == 0 and graph[v][i] == 1: # 연결된 정점을 res 에 방문처리 
+            res[i] = 1
+            dfs(i)
+
+n = int(input())
+
+graph = [list(map(int, input().split())) for _ in range(n)]
+
+res = [0 for _ in range(n)]
+
+for i in range(n):
+    dfs(i)
+    print(*res)
+
+    res = [0 for _ in range(n)]
+
+
 # git commit -m "code: Solve boj 11403 경로 찾기 (yeonju)"

66073c828671185699af7dc4970f196f2c5c3f9a
YoonBaek <royal729@naver.com>
2021-09-16 18:28:09 +0900

    code: Solve boj 11286 절댓값 힙 (yoonbaek)

--- BOJ/priority_queue/11286-절댓값_힙/11286-절댓값_힙-yoonbaek.py
+++ BOJ/priority_queue/11286-절댓값_힙/11286-절댓값_힙-yoonbaek.py
@@ -1,1 +1,21 @@
+from sys import stdin
+import heapq
+
+rd = lambda : int(stdin.readline())
+
+min_heap = []
+
+if __name__ == "__main__":
+    N = rd()
+    for _ in range(N):
+        x = rd()
+
+        if x:            
+            heapq.heappush(min_heap, (abs(x), x))
+        else:
+            if not min_heap:
+                print(0)
+                continue
+            print(heapq.heappop(min_heap)[1])
+
 # git commit -m "code: Solve boj 11286 절댓값 힙 (yoonbaek)"

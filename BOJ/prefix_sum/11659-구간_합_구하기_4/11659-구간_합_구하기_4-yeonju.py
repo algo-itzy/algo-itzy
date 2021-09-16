8c4c7c5181995187a0de0243d57e8a04811853d6
위연주 <thizzyzz@naver.com>
2021-09-16 17:51:56 +0900

    code: Solve boj 11659 구간 합 구하기 4 (yeonju)

--- BOJ/prefix_sum/11659-구간_합_구하기_4/11659-구간_합_구하기_4-yeonju.py
+++ BOJ/prefix_sum/11659-구간_합_구하기_4/11659-구간_합_구하기_4-yeonju.py
@@ -1,1 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+nums = list(map(int, input().split()))
+nums_add =[0]                               # 누적 합 리스트
+
+for i in range(n):
+    nums_add.append(nums_add[-1] + nums[i])
+
+for i in range(m):
+    i, j = map(int, input().split())
+
+    print(n
+
 # git commit -m "code: Solve boj 11659 구간 합 구하기 4 (yeonju)"

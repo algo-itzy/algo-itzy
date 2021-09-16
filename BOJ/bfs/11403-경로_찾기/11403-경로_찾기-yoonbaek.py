4c63ae88131f9036636a24ece8778ab1f03343ec
YoonBaek <royal729@naver.com>
2021-09-16 18:25:37 +0900

    code: Solve boj 11403 경로 찾기 (yoonbaek)

--- BOJ/bfs/11403-경로_찾기/11403-경로_찾기-yoonbaek.py
+++ BOJ/bfs/11403-경로_찾기/11403-경로_찾기-yoonbaek.py
@@ -1,1 +1,22 @@
+from sys import stdin
+
+rd = stdin.readline
+
+
+def FW():
+    for mid in range(N):
+        for start in range(N):
+            for end in range(N):
+                if mat[start][mid] and mat[mid][end]:
+                    mat[start][end] = 1
+
+
+if __name__ == "__main__":
+    N = int(rd())
+    mat = [list(map(int, rd().split())) for _ in range(N)]
+
+    FW()
+
+    for row in mat:
+        print(*row)
 # git commit -m "code: Solve boj 11403 경로 찾기 (yoonbaek)"

72fe890d2fb3882e9d0c32dea6c00bb68e6c5920
chaselover <gkstmdwn234@gmail.com>
2021-09-19 20:35:57 +0900

    code: Solve swea L5185 L5186 (seungjoo)

--- SWEA/implementation/L5185-이진수/L5185-이진수-seungjoo.py
+++ SWEA/implementation/L5185-이진수/L5185-이진수-seungjoo.py
@@ -1,1 +1,7 @@
 # git commit -m "code: Solve swea L5185 이진수 (seungjoo)"
+for test in range(1,int(input())+1):
+    N, s = input().split()
+    ans = ''
+    for i in range(int(N)):
+        ans += bin(int(s[i],16))[2:].zfill(4)
+    print(f"#{test} {ans}")
--- SWEA/implementation/L5186-이진수2/L5186-이진수2-seungjoo.py
+++ SWEA/implementation/L5186-이진수2/L5186-이진수2-seungjoo.py
@@ -1,1 +1,18 @@
 # git commit -m "code: Solve swea L5186 이진수2 (seungjoo)"
+def binary(n,depth):
+    if depth>13:
+        return '2'
+    if not n:
+        return ''
+    if n >= 2**(-depth):
+        n -= 2**(-depth)
+        return '1' + binary(n,depth+1)
+    return '0' + binary(n,depth+1)
+
+
+for test in range(1,int(input())+1):
+    N = float(input())
+    ans = binary(N,1)
+    if ans[-1]=='2':
+        ans = 'overflow'
+    print(f'#{test} {ans}')

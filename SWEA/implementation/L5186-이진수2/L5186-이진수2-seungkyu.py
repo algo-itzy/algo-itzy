249a6dc4175c10f0357be9107822aa66b8514783
ed-kyu <seungkyukim.dev@gmail.com>
2021-09-21 22:12:22 +0900

    code: Solve swea L5186 이진수2 (seungkyu)

--- SWEA/implementation/L5186-이진수2/L5186-이진수2-seungkyu.py
+++ SWEA/implementation/L5186-이진수2/L5186-이진수2-seungkyu.py
@@ -1,1 +1,20 @@
 # git commit -m "code: Solve swea L5186 이진수2 (seungkyu)"
+T = int(input())
+
+for tc in range(1, T+1):
+    num = float(input())
+    ans = ''
+    n = 1
+    flag = True
+    while num:
+        ans += str(int(num//(2**(-n)))) 
+        num = num % (2**(-n))
+        n += 1
+        if len(ans) > 13:
+            flag = False
+            break
+
+    if flag:
+        print(f'#{tc} {ans}')
+    else:
+        print(f'#{tc} overflow')

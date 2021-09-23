b0f91d04d2988c82e8b7fc4e2505a459cb233457
devpla <chaeyeonhee@kakao.com>
2021-09-23 10:47:14 +0900

    code: Solve swea L5186 이진수2 (yeonhee)

--- SWEA/implementation/L5186-이진수2/L5186-이진수2-yeonhee.py
+++ SWEA/implementation/L5186-이진수2/L5186-이진수2-yeonhee.py
@@ -1,1 +1,16 @@
-# git commit -m "code: Solve swea L5186 이진수2 (yeonhee)"
+import sys
+sys.stdin = open('input.txt')
+
+for t in range(1, int(input())+1):
+    n = float(input())
+    result = ''
+
+    for i in range(-1, -14, -1):
+        result += str(int(n // 2**i))
+        n %= 2**i
+        if n == 0:
+            break
+    else:
+        result = 'overflow'
+
+    print(f'#{t} {result}')

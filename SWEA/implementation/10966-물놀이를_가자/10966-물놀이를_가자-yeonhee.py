6e0969624c34a50387f4aa6cdf36016eaf21047b
devpla <chaeyeonhee@kakao.com>
2021-09-24 15:11:40 +0900

    code: Solve swea 10966 물놀이를 가자 (yeonhee)

--- SWEA/implementation/10966-물놀이를_가자/10966-물놀이를_가자-yeonhee.py
+++ SWEA/implementation/10966-물놀이를_가자/10966-물놀이를_가자-yeonhee.py
@@ -1,1 +1,31 @@
-# git commit -m "code: Solve swea 10966 물놀이를 가자 (yeonhee)"
+import sys
+from collections import deque
+
+sys.stdin = open('input.txt')
+
+DIRECTIONS = ((1, 0), (-1, 0), (0, 1), (0, -1))
+
+for t in range(1, int(input())+1):
+    N, M = map(int, input().split())
+    visited = [[-1]*M for _ in range(N)]
+    q = deque()
+
+    for r in range(N):
+        row = input()
+        for c in range(M):
+            if row[c] == 'W':
+                q.append((r, c))
+                visited[r][c] = 0
+
+    result = 0
+
+    while q:
+        r, c = q.popleft()
+        for dr, dc in DIRECTIONS:
+            nr, nc = r + dr, c + dc
+            if 0 <= nr < N and 0 <= nc < M and visited[nr][nc] == -1:
+                visited[nr][nc] = visited[r][c] + 1
+                result += visited[nr][nc]
+                q.append((nr, nc))
+
+    print(f'#{t} {result}')

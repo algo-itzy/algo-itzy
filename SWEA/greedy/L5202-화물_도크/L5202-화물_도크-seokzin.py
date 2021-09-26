573443b486290474ea11278783646a35df2fb09e
seokzin <seokzin95@gmail.com>
2021-09-27 00:03:02 +0900

    code: Solve swea L5202 화물 도크 (seokzin)

--- SWEA/greedy/L5202-화물_도크/L5202-화물_도크-seokzin.py
+++ SWEA/greedy/L5202-화물_도크/L5202-화물_도크-seokzin.py
@@ -1,1 +1,15 @@
-# git commit -m "code: Solve swea L5202 화물 도크 (seokzin)"
+for tc in range(1, int(input())+1):
+    n = int(input())
+    arr = [list(map(int, input().split())) for _ in range(n)]
+    arr.sort(key=lambda x: (x[1], x[0]))
+    res = 0
+    last = 0  # 마지막 회의 종료 시간
+
+    for s, e in arr:
+        if s >= last:
+            res += 1
+            last = e
+
+    print(f"#{tc}", res)
+
+# 회의실 배정 문제랑 똑같음

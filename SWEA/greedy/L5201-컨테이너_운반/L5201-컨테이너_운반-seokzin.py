e04600f7c79eb4b20b099e3fc50d470286de5284
seokzin <seokzin95@gmail.com>
2021-09-27 00:02:48 +0900

    code: Solve swea L5201 컨테이너 운반 (seokzin)

--- SWEA/greedy/L5201-컨테이너_운반/L5201-컨테이너_운반-seokzin.py
+++ SWEA/greedy/L5201-컨테이너_운반/L5201-컨테이너_운반-seokzin.py
@@ -1,1 +1,16 @@
-# git commit -m "code: Solve swea L5201 컨테이너 운반 (seokzin)"
+for tc in range(1, int(input())+1):
+    n, m = map(int, input().split())
+    w = sorted(list(map(int, input().split())))
+    t = sorted(list(map(int, input().split())))
+    res = 0
+    
+    for _ in range(min(n, m)):  # 최소 횟수로 해야 pop() = null 오류 없어짐 
+        if t[-1] >= w[-1]:
+            res += w[-1]
+            t.pop()
+        
+        w.pop()
+
+    print(f"#{tc}", res)
+
+# 전형적 배낭 문제

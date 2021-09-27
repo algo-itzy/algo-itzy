8eca5beac38436a7e932fe86b0730b52355d67f5
YoonBaek <royal729@naver.com>
2021-09-27 22:37:19 +0900

    code: Solve swea 1953 탈주범 검거 (yoonbaek)

--- SWEA/bfs/1953-탈주범_검거/1953-탈주범_검거-yoonbaek.py
+++ SWEA/bfs/1953-탈주범_검거/1953-탈주범_검거-yoonbaek.py
@@ -9,25 +9,32 @@ pipe_dict = {
     6: (down, left), 7: (up, left)
 }
 
+connections = {
+    up: down, down:up, left:right, right:left
+}
+
 def BFS(start):
     q = deque([start])
-    possibles = 0
+    possibles = 1
     visited[start[1]][start[0]] = 1
 
     while q:
         c, r = q.popleft()
 
         for x, y in pipe_dict[tunnel[r][c]]:
+            con = (x, y)
             x += c; y += r
             if 0 <= x < M and 0 <= y < N:
                 if not visited[y][x] and tunnel[y][x]:
-                    visited[y][x] = visited[r][c] + 1
-                    if visited[y][x] <= L+1:
-                        possibles += 1
-                    q.append((x, y))
-    
-    return possibles
-                    
+                    if connections[con] in pipe_dict[tunnel[y][x]]: # 연결부 맞는지 확인하기
+                        visited[y][x] = visited[r][c] + 1
+                        if visited[y][x] <= L:
+                            possibles += 1
+                        else:
+                            return possibles
+                        q.append((x, y))       
+
+    return possibles             
 
 
 if __name__ == "__main__":
@@ -38,7 +45,6 @@ if __name__ == "__main__":
 
         tunnel = [list(map(int, input().split())) for _ in range(N)]
         visited = [[0 for _ in range(M)]for _ in range(N)]
-
         ans = BFS((C, R))
 
         print(f"#{tc} {ans}")

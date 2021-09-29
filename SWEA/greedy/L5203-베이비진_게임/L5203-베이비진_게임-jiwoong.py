5a1cf6a1bccc7fad2209e84ca9e01554db02e855
서울_4반_정지웅 <j2woong1@gmail.com>
2021-09-29 23:07:55 +0900

    code: Solve L5203 베이비진_게임 (jiwoong)

--- SWEA/greedy/L5203-베이비진_게임/L5203-베이비진_게임-jiwoong.py
+++ SWEA/greedy/L5203-베이비진_게임/L5203-베이비진_게임-jiwoong.py
@@ -1,1 +1,33 @@
 # git commit -m "code: Solve swea L5203 베이비진 게임 (jiwoong)"
+def babygjn(num, cnt):  # 카드숫자, 카운팅리스트
+    cnt[num] += 1
+    tmp = 0
+    i = 0
+    while i < 8:
+        if cnt[i] >= 3:  # babygjn check
+            tmp = 1
+            break
+        if cnt[i] and cnt[i + 1] and cnt[i + 2]:  # run check
+            tmp = 1
+            break
+        i += 1
+    if tmp == 1:
+        return True
+
+
+T = int(input())
+
+for tc in range(1, T + 1):
+    cards = list(map(int, input().split()))
+    p1, p2 = [0] * 10, [0] * 10
+
+    ans = 0
+    for i in range(0, len(cards), 2):
+        if babygjn(cards[i], p1):
+            ans = 1
+            break
+        if babygjn(cards[i + 1], p2):
+            ans = 2
+            break
+
+    print("#{} {}".format(tc, ans))

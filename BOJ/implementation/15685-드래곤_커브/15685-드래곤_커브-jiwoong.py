be0d3174e1ba1acb93c082b9731126c58d7c71c0
서울_4반_정지웅 <j2woong1@gmail.com>
2021-09-29 23:08:53 +0900

    code: Solve 15685 드래곤_커브 (jiwoong)

--- BOJ/implementation/15685-드래곤_커브/15685-드래곤_커브-jiwoong.py
+++ BOJ/implementation/15685-드래곤_커브/15685-드래곤_커브-jiwoong.py
@@ -1,1 +1,30 @@
 # git commit -m "code: Solve boj 15685 드래곤 커브 (jiwoong)"
+N = int(input())
+
+dx, dy = (1, 0, -1, 0), (0, -1, 0, 1)
+
+start = []
+dragon = [list() for _ in range(N)]
+visited = [[0] * 101 for _ in range(101)]
+
+for i in range(N):
+    x, y, d, g = map(int, input().split())
+    curve = 0
+    dragon[i].append(d)
+    while curve < g:
+        n = 2 ** curve
+        for j in dragon[i][::-1]:
+            dragon[i].append((j + 1) % 4)
+        curve += 1
+    visited[y][x] = 1
+    for d in dragon[i]:
+        y += dy[d]
+        x += dx[d]
+        visited[y][x] = 1
+
+ans = 0
+for i in range(100):
+    for j in range(100):
+        if visited[i][j] and visited[i + 1][j] and visited[i][j + 1] and visited[i + 1][j + 1]:
+            ans += 1
+print(ans)

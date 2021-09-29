23baa3f0f1024ed8797e35b5dcdcbc4ae8aa6ff1
서울_4반_정지웅 <j2woong1@gmail.com>
2021-09-29 23:06:11 +0900

    code: Solve L5201 컨테이너_운반 (jiwoong)

--- SWEA/greedy/L5201-컨테이너_운반/L5201-컨테이너_운반-jiwoong.py
+++ SWEA/greedy/L5201-컨테이너_운반/L5201-컨테이너_운반-jiwoong.py
@@ -1,1 +1,23 @@
 # git commit -m "code: Solve swea L5201 컨테이너 운반 (jiwoong)"
+T = int(input())
+
+for tc in range(1, T + 1):
+    N, M = map(int, input().split())  # N: 컨테이너수, M: 트럭수
+    weight = sorted(list(map(int, input().split())), reverse=True)  # N개의 화물 무게
+    truck = sorted(list(map(int, input().split())), reverse=True)  # M개 트럭의 적재용량
+
+    ans = 0
+    cnt = 0
+    i, j = 0, 0  # truck, weight
+    while True:
+        if cnt == min(N, M):
+            break
+        if truck[i] >= weight[j]:
+            ans += weight[j]
+            i += 1
+            j += 1
+        else:
+            j += 1
+        cnt += 1
+
+    print("#{} {}".format(tc, ans))

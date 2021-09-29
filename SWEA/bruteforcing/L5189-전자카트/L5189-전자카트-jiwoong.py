212c03a22974e733ad3c794641ceadd8db3874e1
서울_4반_정지웅 <j2woong1@gmail.com>
2021-09-29 23:04:52 +0900

    code: Solve L5189 전자카트 (jiwoong)

--- SWEA/bruteforcing/L5189-전자카트/L5189-전자카트-jiwoong.py
+++ SWEA/bruteforcing/L5189-전자카트/L5189-전자카트-jiwoong.py
@@ -1,1 +1,28 @@
 # git commit -m "code: Solve swea L5189 전자카트 (jiwoong)"
+def check(t, s):
+    if 0 not in visited:
+        s += nums[t][0]
+        global ans
+        if ans == 0 or ans > s:
+            ans = s
+        pass
+    else:
+        for i in range(len(visited)):
+            if visited[i] == 1:
+                continue
+            visited[i] = 1
+            check(i, s + nums[t][i])
+            visited[i] = 0
+
+
+T = int(input())
+for tc in range(1, T + 1):
+    N = int(input())
+    nums = [list(map(int, input().split())) for _ in range(N)]
+
+    ans = 0
+    visited = [0] * N
+
+    visited[0] = 1
+    check(0, 0)
+    print('#{} {}'.format(tc, ans))

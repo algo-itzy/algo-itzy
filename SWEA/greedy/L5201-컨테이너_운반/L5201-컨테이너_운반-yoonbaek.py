77d45fb9e445c15a8316114a0520d44c07fd4620
YoonBaek <royal729@naver.com>
2021-09-30 17:18:59 +0900

    code: Solve swea L5201 컨테이너 운반 (yoonbaek)

--- SWEA/greedy/L5201-컨테이너_운반/L5201-컨테이너_운반-yoonbaek.py
+++ SWEA/greedy/L5201-컨테이너_운반/L5201-컨테이너_운반-yoonbaek.py
@@ -1,1 +1,18 @@
+if __name__ == "__main__":
+    T = int(input())
+
+    for tc in range(1, T+1):
+        N, M = map(int, input().split())
+        containers = sorted(list(map(int, input().split())), reverse=True)
+        trucks = sorted(list(map(int, input().split())), reverse=True)
+
+        total, c, t = 0, 0, 0
+        while c < N and t < M:
+            if containers[c] <= trucks[t]:
+                total += containers[c]
+                t += 1
+            c += 1
+    
+        print(f"#{tc} {total}")
+
 # git commit -m "code: Solve swea L5201 컨테이너 운반 (yoonbaek)"

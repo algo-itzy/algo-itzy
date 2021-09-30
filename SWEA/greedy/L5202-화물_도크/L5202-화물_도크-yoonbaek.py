3e78391ddab6968c9515bf171b2febbf2ec5cfe6
YoonBaek <royal729@naver.com>
2021-09-30 19:00:34 +0900

    code: Solve swea L5202 화물 도크 (yoonbaek)

--- SWEA/greedy/L5202-화물_도크/L5202-화물_도크-yoonbaek.py
+++ SWEA/greedy/L5202-화물_도크/L5202-화물_도크-yoonbaek.py
@@ -1,1 +1,20 @@
+key = lambda x: (x[1], x[0])
+
+if __name__ == "__main__":
+    T = int(input())
+
+    for tc in range(1, T+1):
+        N = int(input())
+    
+        schedules = sorted([tuple(map(int, input().split())) for _ in range(N)], key=key)
+        
+        cnt = 0
+        prev = 0
+        for start, end in schedules:
+            if prev <= start:
+                cnt += 1
+                prev = end
+
+        print(f"#{tc} {cnt}")
+
 # git commit -m "code: Solve swea L5202 화물 도크 (yoonbaek)"

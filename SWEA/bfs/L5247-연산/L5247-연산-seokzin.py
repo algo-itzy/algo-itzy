ca3358c93232645ce5c2efe9a73c56447bb1b52f
seokzin <seokzin95@gmail.com>
2021-10-04 23:45:24 +0900

    code: Solve swea L5247 연산 (seokzin)

--- SWEA/bfs/L5247-연산/L5247-연산-seokzin.py
+++ SWEA/bfs/L5247-연산/L5247-연산-seokzin.py
@@ -1,1 +1,26 @@
-# git commit -m "code: Solve swea L5247 연산 (seokzin)"
+from collections import deque
+
+
+def bfs():
+    q = deque([(n, 0)])
+    visit = [0] * 1000001
+
+    while q:
+        x, cnt = q.popleft()
+        
+        if x == m: 
+            return cnt
+
+        if visit[x]: 
+            continue
+
+        visit[x] = 1
+
+        for nx in (x+1, x-1, x*2, x-10):
+            if 0 < nx <= 1000000:
+                q.append((nx, cnt+1))
+
+
+for tc in range(1, int(input())+1):
+    n, m = map(int, input().split())
+    print(f'#{tc}', bfs())

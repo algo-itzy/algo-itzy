92c2435a05f885dc6f0f68c0e59f49392b08d1a9
seokzin <seokzin95@gmail.com>
2021-10-04 23:46:56 +0900

    code: Solve swea L5248 그룹 나누기 (seokzin)

--- SWEA/data_structure/L5248-그룹_나누기/L5248-그룹_나누기-seokzin.py
+++ SWEA/data_structure/L5248-그룹_나누기/L5248-그룹_나누기-seokzin.py
@@ -1,1 +1,23 @@
-# git commit -m "code: Solve swea L5248 그룹 나누기 (seokzin)"
+def find(x):
+    if x == parent[x]:
+        return x
+    else:
+        return find(parent[x])
+
+
+def union(x,y):
+    parent[find(y)] = find(x)
+
+
+for tc in range(1, int(input())+1):
+    n, m = map(int, input().split())
+    parent = list(range(n+1))
+    arr = list(map(int, input().split()))
+
+    for i in range(m):
+        a, b = arr[2*i: 2*i+2]
+        union(a, b)
+
+    res = [find(i) for i in range(n+1)]
+    
+    print(f'#{tc}', len(set(res))-1)

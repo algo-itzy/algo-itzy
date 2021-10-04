2a099cacf37794e132788bceea69d545bab5414b
seokzin <seokzin95@gmail.com>
2021-10-04 23:45:57 +0900

    code: Solve swea L5208 전기버스2 (seokzin)

--- SWEA/dfs/L5208-전기버스2/L5208-전기버스2-seokzin.py
+++ SWEA/dfs/L5208-전기버스2/L5208-전기버스2-seokzin.py
@@ -1,1 +1,21 @@
-# git commit -m "code: Solve swea L5208 전기버스2 (seokzin)"
+def dfs(x, cnt):
+    global res
+
+    if res < cnt:
+        return
+
+    if x >= n:
+        res = cnt
+        return
+
+
+    for i in range(arr[x-1], 0, -1):  # 역탐색 중요
+        dfs(x+i, cnt+1)
+
+
+for tc in range(1, int(input())+1):
+    n, *arr = list(map(int, input().split()))
+    res = float('inf')
+    dfs(1, -1)  # 출발은 카운팅 X라서 -1 시작
+
+    print(f'#{tc}', res)

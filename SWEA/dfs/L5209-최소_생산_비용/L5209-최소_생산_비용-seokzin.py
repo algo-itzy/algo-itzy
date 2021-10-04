f041d3e3c9c71ea7362cdd8e78f1ec603780320f
seokzin <seokzin95@gmail.com>
2021-10-04 23:46:28 +0900

    code: Solve swea L5209 최소 생산 비용 (seokzin)

--- SWEA/dfs/L5209-최소_생산_비용/L5209-최소_생산_비용-seokzin.py
+++ SWEA/dfs/L5209-최소_생산_비용/L5209-최소_생산_비용-seokzin.py
@@ -1,1 +1,23 @@
-# git commit -m "code: Solve swea L5209 최소 생산 비용 (seokzin)"
+def dfs(x, s, visit):
+    global res
+
+    if res < s:
+        return
+
+    if visit == (1<<n) - 1:
+        res = s
+        return
+
+
+    for i in range(n):
+        if not visit & (1<<i):
+            dfs(x+1, s+arr[x][i], visit|(1<<i))
+
+
+for tc in range(1, int(input())+1):
+    n = int(input())
+    arr = [list(map(int, input().split())) for _ in range(n)]
+    res = float('inf')
+
+    dfs(0, 0, 0)
+    print(f'#{tc}', res)

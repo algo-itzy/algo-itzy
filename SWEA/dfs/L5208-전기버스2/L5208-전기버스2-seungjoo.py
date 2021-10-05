4348d6ba5d168d8d81ea5b63861e312aa576a6a5
chaselover <gkstmdwn234@gmail.com>
2021-10-06 06:12:21 +0900

    code: Solve swea L5208 전기버스2 (seungjoo)

--- SWEA/dfs/L5208-전기버스2/L5208-전기버스2-seungjoo.py
+++ SWEA/dfs/L5208-전기버스2/L5208-전기버스2-seungjoo.py
@@ -1,1 +1,10 @@
 # git commit -m "code: Solve swea L5208 전기버스2 (seungjoo)"
+for test in range(1, int(input()) + 1):
+    N, *battery = map(int, input().split())
+    dp = [float('inf')] * N
+    dp[N - 1] = 0
+    for i in range(N - 1, -1, -1):
+        for j in range(i - 1, -1, -1):
+            if j + battery[j] >= i:
+                dp[j] = min(dp[j], dp[i] + 1)
+    print(f'#{test} {dp[0] - 1}')

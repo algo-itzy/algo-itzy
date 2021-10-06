d6d5b6598ae3390e742c3a8742b52b44349a200f
devpla <chaeyeonhee@kakao.com>
2021-10-06 15:55:44 +0900

    code: Solve swea L5247 연산 (yeonhee)

--- SWEA/bfs/L5247-연산/L5247-연산-yeonhee.py
+++ SWEA/bfs/L5247-연산/L5247-연산-yeonhee.py
@@ -1,1 +1,36 @@
-# git commit -m "code: Solve swea L5247 연산 (yeonhee)"
+import sys
+from collections import deque
+
+sys.stdin = open('input.txt')
+
+
+def calculation(num, calc):
+    if calc == 0:
+        return num + 1
+    elif calc == 1:
+        return num - 1
+    elif calc == 2:
+        return num * 2
+    elif calc == 3:
+        return num - 10
+
+
+def bfs(n, m):
+    q = deque([(n, 0)])
+    visited[n] = t
+
+    while q:
+        num, cnt = q.popleft()
+        for i in range(4):
+            num_next = calculation(num, i)
+            if num_next == m:
+                return cnt + 1
+            elif 1 <= num_next <= 1000000 and visited[num_next] != t:
+                q.append((num_next, cnt + 1))
+                visited[num_next] = t
+
+
+visited = [0] * 1000001
+for t in range(1, int(input())+1):
+    N, M = map(int, input().split())
+    print(f'#{t} {bfs(N, M)}')

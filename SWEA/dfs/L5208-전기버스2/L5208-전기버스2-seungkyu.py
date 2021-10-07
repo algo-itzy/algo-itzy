ba7bd49fe060eac23118c369e110f6cc197403c0
ed-kyu <seungkyukim.dev@gmail.com>
2021-10-07 13:40:44 +0900

    code: Solve swea L5208 전기버스2 (seungkyu)

--- SWEA/dfs/L5208-전기버스2/L5208-전기버스2-seungkyu.py
+++ SWEA/dfs/L5208-전기버스2/L5208-전기버스2-seungkyu.py
@@ -1,1 +1,29 @@
 # git commit -m "code: Solve swea L5208 전기버스2 (seungkyu)"
+import sys
+sys.stdin = open('input.txt')
+
+
+def dfs(x, cnt):
+    global ans
+    
+    if cnt > ans:
+        return
+    
+    if x >= m[0]:
+        ans = cnt
+        return
+
+    for i in range(x+m[x], x, -1):
+        cnt += 1
+        dfs(i, cnt)
+        cnt -= 1
+
+T = int(input())
+for tc in range(1, T+1):
+
+    m = list(map(int, input().split()))
+    
+    ans = 100
+    dfs(1, 0)
+
+    print(f'#{tc} {ans-1}')

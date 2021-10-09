9712b3c4c73af4f06d3687f4901718e68d2d97f0
devpla <chaeyeonhee@kakao.com>
2021-10-09 23:49:00 +0900

    code: Solve swea L5251 최소 이동 거리 (yeonhee)

--- SWEA/dijkstra/L5251-최소_이동_거리/L5251-최소_이동_거리-yeonhee.py
+++ SWEA/dijkstra/L5251-최소_이동_거리/L5251-최소_이동_거리-yeonhee.py
@@ -1,1 +1,31 @@
-# git commit -m "code: Solve swea L5251 최소 이동 거리 (yeonhee)"
+import sys
+from heapq import heappop, heappush
+from collections import defaultdict
+
+sys.stdin = open('input.txt')
+
+
+def dijkstra():
+    heap = []
+    heappush(heap, (0, 0))
+
+    while heap:
+        idx, w = heappop(heap)
+        for nidx, nw in graph[idx]:
+            tmp = w + nw
+            if dist[nidx] > tmp:
+                dist[nidx] = tmp
+                heappush(heap, [nidx, tmp])
+
+
+for t in range(1, int(input())+1):
+    N, E = map(int, input().split())
+    dist = [0] + [float('inf')]*N
+    graph = defaultdict(list)
+
+    for _ in range(E):
+        s, e, w = map(int, input().split())
+        graph[s].append((e, w))
+
+    dijkstra()
+    print(f'#{t} {dist[N]}')

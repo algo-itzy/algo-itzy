3f70606e64f67f7e24526aa2c804660bf630b580
chaselover <gkstmdwn234@gmail.com>
2021-10-10 03:29:46 +0900

    code: Solve swea L5249 L5250 L 5251 (seungjoo)

--- SWEA/dijkstra/L5251-최소_이동_거리/L5251-최소_이동_거리-seungjoo.py
+++ SWEA/dijkstra/L5251-최소_이동_거리/L5251-최소_이동_거리-seungjoo.py
@@ -1,1 +1,27 @@
 # git commit -m "code: Solve swea L5251 최소 이동 거리 (seungjoo)"
+from heapq import heappush, heappop
+
+def dijkstra(start):
+    heap = []
+    dists[start] = 0
+    heappush(heap, (0, start))
+    while heap:
+        cur_dist, cur_node = heappop(heap)
+        for next_node, next_dist in graph[cur_node]:
+            dist = cur_dist + next_dist
+            if dists[next_node] > dist:
+                dists[next_node] = dist
+                heappush(heap, (dist, next_node))
+
+
+for test in range(1, int(input()) + 1):
+    N, E = map(int, input().split())
+    graph = {i: [] for i in range(N + 1)}
+    for __ in range(E):
+        a, b, c = map(int, input().split())
+        graph[a].append((b, c))
+    dists = {i : float('inf') for i in range(N + 1)}
+
+    dijkstra(0)
+    
+    print(f'#{test} {dists[N]}')
--- SWEA/tree/L5249-최소_신장_트리/L5249-최소_신장_트리-seungjoo.py
+++ SWEA/tree/L5249-최소_신장_트리/L5249-최소_신장_트리-seungjoo.py
@@ -1,1 +1,35 @@
 # git commit -m "code: Solve swea L5249 최소 신장 트리 (seungjoo)"
+from heapq import heappop, heappush
+
+
+def find(x):
+    if parents[x] == x:
+        return x
+    parents[x] = find(parents[x])
+    return parents[x]
+
+
+def union(a, b):
+    a = find(a)
+    b = find(b)
+
+    if a > b:
+        a, b = b, a
+    parents[b] = a
+
+
+for test in range(1, int(input()) + 1):
+    V, E = map(int, input().split())
+    parents = {i: i for i in range(V + 1)}
+    edges = []
+    for __ in range(E):
+        a, b, w = map(int, input().split())
+        heappush(edges, (w, a, b))
+
+    answer = 0
+    while edges:
+        w, a, b = heappop(edges)
+        if find(a) != find(b):
+            union(a, b)
+            answer += w
+    print(f'#{test} {answer}')

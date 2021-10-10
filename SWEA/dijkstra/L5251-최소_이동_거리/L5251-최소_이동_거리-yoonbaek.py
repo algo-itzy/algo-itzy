bec9ca4a90e8939eb59da6125e3d9e83911854f1
YoonBaek <royal729@naver.com>
2021-10-10 11:26:03 +0900

    code: Solve swea L5251 최소 이동 거리 (yoonbaek)

--- SWEA/dijkstra/L5251-최소_이동_거리/L5251-최소_이동_거리-yoonbaek.py
+++ SWEA/dijkstra/L5251-최소_이동_거리/L5251-최소_이동_거리-yoonbaek.py
@@ -1,1 +1,33 @@
+import heapq 
+
+def dijkstra(start):
+    pq = [start]
+    while pq:
+        now_dist, now = heapq.heappop(pq)
+
+        if distances[now] < now_dist:
+            continue
+        for next, next_dist in graph[now]:
+            next_dist += now_dist 
+            if distances[next] > next_dist:
+                distances[next] = next_dist
+                heapq.heappush(pq, (next_dist, next))
+            
+
+if __name__ == "__main__":
+    T = int(input())
+
+    for tc in range(1, T+1):
+        N, E = map(int, input().split())
+        distances = [0]+ [100000000] * (N)
+        graph = [[] for _ in range(N+1)]
+
+        for _ in range(E):
+            s, e, w = map(int, input().split())
+            graph[s].append((e,w))
+
+        dijkstra((0, 0))
+        
+        print(f"#{tc} {distances[-1]}")
+        
 # git commit -m "code: Solve swea L5251 최소 이동 거리 (yoonbaek)"

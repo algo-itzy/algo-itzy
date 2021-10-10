c7707bbd314175e3924d26c152063b7c44f40446
YoonBaek <royal729@naver.com>
2021-10-10 10:56:08 +0900

    code: Solve swea L5209 최소 생산 비용 (yoonbaek)

--- SWEA/dfs/L5209-최소_생산_비용/L5209-최소_생산_비용-yoonbaek.py
+++ SWEA/dfs/L5209-최소_생산_비용/L5209-최소_생산_비용-yoonbaek.py
@@ -1,1 +1,36 @@
+def DFS(acc, recur):
+    global min_cost
+
+    if acc >= min_cost:
+        return
+
+    # 재귀횟수가 N에 도달하면 합격
+    if recur == N:
+        min_cost = acc
+        return
+
+    for i in range(N):
+        if not visited[i]:
+            visited[i] = 1
+            DFS(acc+costs[recur][i], recur+1)
+            visited[i] = 0
+            
+
+if __name__ == "__main__":
+    T = int(input())
+
+    for tc in range(1, T+1):
+        N = int(input())
+        min_cost = 99*(N**2)+1
+
+        costs = []
+        for _ in range(N):
+            costs.append(list(map(int, input().split())))
+
+        visited = [0] * N
+        
+        DFS(0,0)
+
+        print(f"#{tc} {min_cost}")
+
 # git commit -m "code: Solve swea L5209 최소 생산 비용 (yoonbaek)"

cea16e4b27fcc2d8088868c8d7103524be5266c6
seokzin <seokzin95@gmail.com>
2021-10-18 15:19:43 +0900

    code: Solve programmers 신규 아이디 추천 (seokzin)

--- Programmers/신규_아이디_추천/신규_아이디_추천-seokzin.py
+++ Programmers/신규_아이디_추천/신규_아이디_추천-seokzin.py
@@ -1,1 +1,29 @@
-# git commit -m "code: Solve programmers 신규 아이디 추천 (seokzin)"
+def solution(new_id):
+    new_id = new_id.lower()  # 1단계
+
+    res = ''  # 2단계
+    for x in new_id:
+        if x.isalnum() or x in '-_.':
+            res += x
+
+    while '..' in res:  # 3단계
+        res = res.replace('..', '.')  
+
+    if res[0] == '.' and len(res) > 1:  # 4단계
+        res = res[1:]
+    if res[-1] == '.':
+        res = res[:-1]
+
+    if not res:  # 5단계
+        res = 'a'
+
+    if len(res) >= 16:  # 6단계
+        res = res[:15]
+
+        if res[-1] == '.':
+            res = res[:-1]
+
+    if len(res) <= 3:  # 7단계
+        res = res + res[-1] * (3-len(res))
+
+    return res

329095eac7ce48f551d69c0f7572861c2917dcf2
서울_4반_정지웅 <j2woong1@gmail.com>
2021-10-21 22:18:03 +0900

    code: solve programmers 메뉴 리뉴얼 (jiwoong)

--- Programmers/메뉴_리뉴얼/메뉴_리뉴얼-jiwoong.py
+++ Programmers/메뉴_리뉴얼/메뉴_리뉴얼-jiwoong.py
@@ -1,1 +1,33 @@
 # git commit -m "code: Solve programmers 메뉴 리뉴얼 (jiwoong)"
+import itertools
+
+
+def solution(menus, course):
+    ans = []
+    for i in course:
+        com = set()
+        for j in menus:
+            menu = list(set(j))
+            menu.sort()
+            tmp = set(itertools.combinations(menu, i))
+            com = com | tmp
+        res = []
+        max_cnt = 2
+        for j in com:  
+            j = ''.join(j)
+            cnt = 0
+            for k in menus:  
+                tmp = 0
+                for x in j:  
+                    if x in k:
+                        tmp += 1
+                if tmp == len(j):
+                    cnt += 1
+            if cnt > max_cnt:
+                res = [j]
+                max_cnt = cnt
+            elif cnt == max_cnt:
+                res.append(j)
+        ans.extend(res)
+    ans.sort()
+    return ans

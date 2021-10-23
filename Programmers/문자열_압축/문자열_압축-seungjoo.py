cd82297c9d57ce544d4efcc4534da50c5dd42412
chaselover <gkstmdwn234@gmail.com>
2021-10-24 02:30:40 +0900

    code: Solve programmers 문자열 압축 (seungjoo)

--- Programmers/문자열_압축/문자열_압축-seungjoo.py
+++ Programmers/문자열_압축/문자열_압축-seungjoo.py
@@ -1,1 +1,20 @@
 # git commit -m "code: Solve programmers 문자열 압축 (seungjoo)"
+def solution(s):
+    n = len(s)
+    answer = n
+    for unit in range(1, n//2 + 1):
+        cnt = 1
+        now = s[:unit]
+        tmp_answer = ''
+        for i in range(unit, n + unit, unit):
+            if s[i:i + unit] == now:
+                cnt += 1
+            else:
+                if cnt > 1:
+                    tmp_answer += str(cnt) + now
+                else:
+                    tmp_answer += now
+                now = s[i:i + unit]
+                cnt = 1
+        answer = min(answer, len(tmp_answer))
+    return answer

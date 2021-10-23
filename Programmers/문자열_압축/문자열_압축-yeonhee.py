b448a2a04f1abf483fc02c4d8a09d2d71adecc91
devpla <chaeyeonhee@kakao.com>
2021-10-24 00:14:41 +0900

    code: Solve programmers 문자열 압축 (yeonhee)

--- Programmers/문자열_압축/문자열_압축-yeonhee.py
+++ Programmers/문자열_압축/문자열_압축-yeonhee.py
@@ -1,1 +1,20 @@
-# git commit -m "code: Solve programmers 문자열 압축 (yeonhee)"
+def solution(s):
+    answer = float('inf')
+    for i in range(1, len(s)//2+2):
+        result = ''
+        cnt = 1
+        tmp = s[:i]
+
+        for j in range(i, len(s)+i, i):
+            if tmp == s[j:j+i]:
+                cnt += 1
+            else:
+                if cnt == 1:
+                    result += tmp
+                else:
+                    result += str(cnt) + tmp
+                tmp = s[j:j+i]
+                cnt = 1
+
+        answer = min(answer, len(result))
+    return answer

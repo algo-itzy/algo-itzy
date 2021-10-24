ba98300ef8f0a609c39573dffcde1943969cc579
서울_4반_정지웅 <j2woong1@gmail.com>
2021-10-24 10:38:25 +0900

    code: Solve programmers 문자열 압축 (jiwoong)

--- Programmers/문자열_압축/문자열_압축-jiwoong.py
+++ Programmers/문자열_압축/문자열_압축-jiwoong.py
@@ -1,1 +1,17 @@
 # git commit -m "code: Solve programmers 문자열 압축 (jiwoong)"
+def solution(s):
+    ans = [len(s)]
+    for i in range(1, len(s)):
+        word = ""
+        arr = [s[j:j + i] for j in range(0, len(s), i)]
+        stack = [[arr[0], 1]]
+
+        for k in arr[1:]:
+            if stack[-1][0] != k:
+                stack.append([k, 1])
+            else:
+                stack[-1][1] += 1
+        word += ''.join([str(cnt) + word if cnt > 1 else word for word, cnt in stack])
+        ans.append(len(word))
+    print(ans)
+    return min(ans)

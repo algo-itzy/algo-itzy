c55f67b0dc305a32e77ae81ce40952b49396f2a4
서울_4반_정지웅 <j2woong1@gmail.com>
2021-10-24 10:37:27 +0900

    code: Solve programmers 괄호 변환 (jiwoong)

--- Programmers/괄호_변환/괄호_변환-jiwoong.py
+++ Programmers/괄호_변환/괄호_변환-jiwoong.py
@@ -1,1 +1,22 @@
 # git commit -m "code: Solve programmers 괄호 변환 (jiwoong)"
+def solution(p):
+    if p == '':
+        return ''
+    check = True
+    if p[0] == ')':
+        check = False
+    left = 0  # '('
+    right = 0  # ')'
+    idx = 0
+    for sign in p:
+        idx += 1
+        if sign == '(':
+            left += 1
+        else:
+            right += 1
+        if left == right:
+            break
+    if check:
+        return p[:idx] + solution(p[idx:])
+    else:
+        return '(' + solution(p[idx:]) + ')' + ''.join([')' if x == '(' else '(' for x in p[1:(idx - 1)]])

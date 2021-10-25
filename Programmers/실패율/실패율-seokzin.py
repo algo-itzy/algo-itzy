897e770a6c8d33704e6a755bd4e42ca11f844532
seokzin <seokzin95@gmail.com>
2021-10-25 13:17:40 +0900

    code: Solve programmers 실패율 (seokzin)

--- Programmers/실패율/실패율-seokzin.py
+++ Programmers/실패율/실패율-seokzin.py
@@ -1,1 +1,22 @@
-# git commit -m "code: Solve programmers 실패율 (seokzin)"
+from collections import Counter
+
+def solution(N, stages):
+    counter = Counter(stages)
+    fail = []
+    answer = []
+    tot = len(stages)  # 총인원
+    
+
+    for i in range(1, N+1):
+        if tot:
+            fail.append([i, counter[i]/tot])
+            tot -= counter[i]
+        else:
+            fail.append([i, 0])
+
+    fail.sort(key = lambda x: -x[1])
+
+    for x in fail:
+        answer.append(x[0])
+    
+    return answer

07139c4d4fc2a323d0a768e629f353056c7175e6
devpla <chaeyeonhee@kakao.com>
2021-10-27 22:29:57 +0900

    code: Solve programmers 후보키 (yeonhee)

--- Programmers/후보키/후보키-yeonhee.py
+++ Programmers/후보키/후보키-yeonhee.py
@@ -1,1 +1,23 @@
-# git commit -m "code: Solve programmers 후보키 (yeonhee)"
+from itertools import combinations
+
+
+def solution(relation):
+    row = len(relation)
+    col = len(relation[0])
+    candidates = []
+
+    for i in range(1, col+1):
+        for comb in combinations(range(col), i):
+            tmp = set([tuple([r[idx] for idx in comb]) for r in relation])
+
+            if len(tmp) == row:
+                candidates.append(comb)
+
+    answer = set(candidates[:])
+
+    for i in range(len(candidates)):
+        for j in range(i+1, len(candidates)):
+            if set(candidates[i]).issubset(set(candidates[j])):
+                answer.discard(candidates[j])
+
+    return len(answer)

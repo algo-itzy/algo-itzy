0a7a77f4cebe1938eb1460b535f1da1205d7fb6a
서울_4반_정지웅 <j2woong1@gmail.com>
2021-10-28 12:49:31 +0900

    code: Solve programmers 오픈채팅방 (jiwoong)

--- Programmers/오픈채팅방/오픈채팅방-jiwoong.py
+++ Programmers/오픈채팅방/오픈채팅방-jiwoong.py
@@ -1,1 +1,17 @@
 # git commit -m "code: Solve programmers 오픈채팅방 (jiwoong)"
+def solution(record):
+    dict = {}
+    arr = []
+    for user in record:
+        word = user.split(" ")
+        if word[0] == "Enter":
+            dict[word[1]] = word[2]
+            arr.append([word[1], "님이 들어왔습니다."])
+        elif word[0] == "Leave":
+            arr.append([word[1], "님이 나갔습니다."])
+        elif word[0] == "Change":
+            dict[word[1]] = word[2]
+    result = []
+    for i, j in arr:
+        result.append(dict[i] + j)
+    return result

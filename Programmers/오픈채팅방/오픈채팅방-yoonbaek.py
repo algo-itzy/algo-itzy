e29243ede013fcf08a7cac6b22a60df4883f46c8
YoonBaek <royal729@naver.com>
2021-10-28 19:04:57 +0900

    code: Solve programmers 오픈채팅방 (yoonbaek)

--- Programmers/오픈채팅방/오픈채팅방-yoonbaek.py
+++ Programmers/오픈채팅방/오픈채팅방-yoonbaek.py
@@ -1,1 +1,25 @@
+def solution(records):
+    answer = []
+    members = {}
+    cmds = []
+
+    for record in records:
+        record = record.split()
+
+        if record[0] == "Leave":
+            cmd, pk = record
+
+        else:
+            cmd, pk, nickname = record
+            members[pk] = nickname
+
+        if cmd != 'Change':
+            cmds.append((cmd, pk))
+
+    for cmd, pk in cmds:
+        message = "님이 들어왔습니다." if cmd == "Enter" else "님이 나갔습니다."
+        answer.append(f"{members[pk]}{message}")
+    
+    return answer
+    
 # git commit -m "code: Solve programmers 오픈채팅방 (yoonbaek)"

bc66b25e786bfe6a48a0cb7fd46e2bba9bfbaf92
chaselover <gkstmdwn234@gmail.com>
2021-10-31 02:20:19 +0900

    code: Solve programmers 뉴스 클러스터링 캐시 브렌즈 4블록 (seungjoo)

--- Programmers/캐시/캐시-seungjoo.py
+++ Programmers/캐시/캐시-seungjoo.py
@@ -1,1 +1,15 @@
 # git commit -m "code: Solve programmers 캐시 (seungjoo)"
+def solution(cacheSize, cities):
+    cache = []
+    total_cost = 0
+    for city in cities:
+        lo_city = city.lower()
+        if lo_city not in cache:
+            cache.append(lo_city)
+            total_cost += 5
+            if len(cache) > cacheSize:
+                cache.pop(0)
+        else:
+            cache.append(cache.pop(cache.index(lo_city)))
+            total_cost += 1
+    return total_cost
--- Programmers/프렌즈4블록/프렌즈4블록-seungjoo.py
+++ Programmers/프렌즈4블록/프렌즈4블록-seungjoo.py
@@ -1,1 +1,32 @@
 # git commit -m "code: Solve programmers 프렌즈4블록 (seungjoo)"
+
+def bomb_block(b, m, n):
+    block = set()
+    for i in range(m - 1):
+        for j in range(n - 1):
+            if b[i][j] and b[i][j] == b[i][j + 1] == b[i + 1][j] == b[i + 1][j + 1]:
+                for dx, dy in [(0, 0), (1, 0), (0, 1), (1, 1)]:
+                    nx, ny = i + dx, j + dy
+                    block.add((nx, ny))
+    if not block:
+        return 0, 0
+    for x, y in block:
+        b[x][y] = 0
+    new_board = [[0] * n for _ in range(m)]
+    for j in range(n):
+        idx = m - 1
+        for i in range(m - 1, -1, -1):
+            if b[i][j]:
+                new_board[idx][j] = b[i][j]
+                idx -= 1
+    return new_board, len(block)
+     
+def solution(m, n, board):
+    board = [list(row) for row in board]
+    count = 0
+    while board:
+        board, cnt = bomb_block(board, m, n)
+        count += cnt
+    return count
+
+print(solution(4, 5, ["CCBDE", "AAADE", "AAABF", "CCBBF"]))

ba071b3665f1a48a729a594041f98c0fda9ceea1
seokzin <seokzin95@gmail.com>
2021-10-30 16:53:50 +0900

    code: Solve programmers 캐시 (seokzin)

--- Programmers/캐시/캐시-seokzin.py
+++ Programmers/캐시/캐시-seokzin.py
@@ -1,1 +1,26 @@
-# git commit -m "code: Solve programmers 캐시 (seokzin)"
+from collections import deque
+
+
+def solution(cacheSize, cities):
+    answer = 0
+    cache = deque()
+
+    if cacheSize == 0: 
+        return len(cities) * 5
+    else: 
+        for c in cities: 
+            c = c.lower()
+
+            if c in cache:
+                cache.remove(c)
+                cache.append(c)
+                answer += 1
+            else: 
+                if len(cache) < cacheSize:
+                    cache.append(c)
+                else:
+                    cache.popleft()
+                    cache.append(c)
+                answer += 5
+
+    return answer

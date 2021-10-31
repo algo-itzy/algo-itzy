d508d9a38a1bd998997fdac5bb8bd7193fed3491
YoonBaek <royal729@naver.com>
2021-10-31 10:53:58 +0900

    code: Solve programmers 뉴스 클러스터링 (yoonbaek

--- Programmers/뉴스_클러스터링/뉴스_클러스터링-yoonbaek.py
+++ Programmers/뉴스_클러스터링/뉴스_클러스터링-yoonbaek.py
@@ -1,1 +1,29 @@
+def get_set(word: list):
+    alpha = 'abcdefghijklmnopqrstuvwxyz'
+    alpha = set(alpha)
+
+    res, prev = set(), ''
+
+    for char in word:
+        if prev:
+            cnt = 0
+            target = prev+char
+            if target.isalpha():
+                while (cnt, target) in res:
+                    cnt += 1
+                res.add((cnt, target))
+        prev = char
+    
+    return res
+
+def solution(str1: str, str2: str):
+    answer = 65536
+    str1, str2 = str1.lower(), str2.lower()
+
+    setA, setB = get_set(str1), get_set(str2)
+    if setA or setB:
+        answer = int(65536 * len(setA.intersection(setB)) / len(setA.union(setB)))
+
+    return answer
+
 # git commit -m "code: Solve programmers 뉴스 클러스터링 (yoonbaek)"

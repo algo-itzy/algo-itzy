552eeacc80134e7c687a2a93f7f18f1a71205835
chaselover <gkstmdwn234@gmail.com>
2021-11-04 02:47:23 +0900

    code: Solve programmers 방금그곡 (seungjoo)

--- Programmers/압축/압축-seungjoo.py
+++ Programmers/압축/압축-seungjoo.py
@@ -1,1 +1,17 @@
 # git commit -m "code: Solve programmers 압축 (seungjoo)"
+def solution(msg):
+    msg += '.'
+    answer = []
+    dic = {chr(i + ord('A')): i + 1 for i in range(26)}
+    idx, last_idx = 0, len(msg) - 1
+    new_hash = 27
+    while idx < last_idx:
+        start = msg[idx]
+        while idx < last_idx and start in dic:
+            idx += 1
+            start += msg[idx]
+        answer.append(dic[start[:-1]])
+        dic[start] = new_hash
+        new_hash += 1
+                
+    return answer

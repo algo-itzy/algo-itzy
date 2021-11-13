8d31e14e8add640f97f26b1af91ca7fb056a2f44
서울_4반_정지웅 <j2woong1@gmail.com>
2021-11-14 00:07:31 +0900

    code: Solve programmers 거리두기 확인하기 (jiwoong)

--- Programmers/거리두기_확인하기/거리두기_확인하기-jiwoong.py
+++ Programmers/거리두기_확인하기/거리두기_확인하기-jiwoong.py
@@ -1,1 +1,39 @@
 # git commit -m "code: Solve programmers 거리두기 확인하기 (jiwoong)"
+def solution(places):
+    answer = []
+
+    for p in places:
+        check = True
+        for i in range(5):
+            for j in range(5):
+                if p[i][j] == 'P':
+                    if j + 1 < 5:
+                        if p[i][j + 1] == 'P':
+                            check = False
+                            break
+                        elif j + 2 < 5:
+                            if p[i][j + 1] == 'O' and p[i][j + 2] == 'P':
+                                check = False
+                                break
+                        if i + 1 < 5:
+                            if (p[i][j + 1] == 'O' or p[i + 1][j] == 'O') and p[i + 1][j + 1] == 'P':
+                                check = False
+                                break
+                    if i + 1 < 5:
+                        if p[i + 1][j] == 'P':
+                            check = False
+                            break
+                        elif i + 2 < 5:
+                            if p[i + 1][j] == 'O' and p[i + 2][j] == 'P':
+                                check = False
+                                break
+                        if j - 1 >= 0:
+                            if (p[i][j - 1] == 'O' or p[i + 1][j] == 'O') and p[i + 1][j - 1] == 'P':
+                                check = False
+                                break
+        if check:
+            answer.append(1)
+        else:
+            answer.append(0)
+
+    return answer

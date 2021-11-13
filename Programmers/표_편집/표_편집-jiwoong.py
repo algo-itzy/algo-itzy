f3253c65886f7be3ea3a20d08dd67f6a6523f479
서울_4반_정지웅 <j2woong1@gmail.com>
2021-11-14 00:08:12 +0900

    code: Solve programmers 표 편집 (jiwoong)

--- Programmers/표_편집/표_편집-jiwoong.py
+++ Programmers/표_편집/표_편집-jiwoong.py
@@ -1,1 +1,39 @@
 # git commit -m "code: Solve programmers 표 편집 (jiwoong)"
+from collections import deque
+
+
+def solution(n, k, cmd):
+    arr = deque([i for i in range(n)])
+    tmp = deque()
+
+    for i in cmd:
+        s_arr = i.split()
+
+        if len(s_arr) > 1:
+            word, x = s_arr[0], int(s_arr[1])
+            if word == "D":
+                k += x
+            elif word == "U":
+                k -= x
+        else:
+            word = s_arr[0]
+            if word == "C":
+                tmp.append((k, arr[k]))
+                arr.remove(arr[k])
+                if k == len(arr):
+                    k -= 1
+            elif word == "Z":
+                ok, ox = tmp.pop()
+                if ok <= k:
+                    k += 1
+                arr.insert(ok, ox)
+
+    words = set(arr)
+    result = ""
+
+    for i in range(n):
+        if i in words:
+            result += 'O'
+        else:
+            result += 'X'
+    return result

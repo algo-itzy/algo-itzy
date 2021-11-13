6e2ad7ec5322058eb8aa8ce43675efe0ab91d053
chaselover <gkstmdwn234@gmail.com>
2021-11-14 06:15:25 +0900

    code: Solve programmers 거리두기 확인하기, 표편집 (seungjoo)

--- Programmers/표_편집/표_편집-seungjoo.py
+++ Programmers/표_편집/표_편집-seungjoo.py
@@ -1,1 +1,40 @@
 # git commit -m "code: Solve programmers 표 편집 (seungjoo)"
+from heapq import heappop, heappush
+
+def solution(n, k, cmd):
+    left, right, delete = [-i for i in range(k-1, -1, -1)], [i for i in range(k, n)], []
+    for command in cmd:
+        c, *num = command.split()
+        if c == 'U':
+            cnt = int(num.pop())
+            for _ in range(cnt):
+                heappush(right, -heappop(left))
+        elif c == 'D':
+            cnt = int(num.pop())
+            for _ in range(cnt):
+                heappush(left, -heappop(right))
+        elif c == 'C':
+            delete.append(heappop(right))
+            if not right:
+                heappush(right, -heappop(left))
+        else:
+            now = delete.pop()
+            if now > right[0]:
+                heappush(right, now)
+            else:
+                heappush(left, -now)
+    answer, idx = '', 0
+    while left:
+        heappush(right, -heappop(left))
+    while right:
+        now = heappop(right)
+        while idx != now:
+            idx += 1
+            answer += 'X'
+        answer += 'O'
+        idx += 1
+    while idx != n:
+        answer += 'X'
+        idx += 1
+    return answer
+            

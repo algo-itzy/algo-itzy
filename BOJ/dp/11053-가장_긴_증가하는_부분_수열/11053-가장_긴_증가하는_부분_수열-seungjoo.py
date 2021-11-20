01cb189b5c948ac845bc6b1dd05945f1aa1d5cd5
chaselover <gkstmdwn234@gmail.com>
2021-11-21 08:18:22 +0900

    code: Solve boj 11053 가장 긴 증가하는 부분 수열 (seungjoo)

--- BOJ/dp/11053-가장_긴_증가하는_부분_수열/11053-가장_긴_증가하는_부분_수열-seungjoo.py
+++ BOJ/dp/11053-가장_긴_증가하는_부분_수열/11053-가장_긴_증가하는_부분_수열-seungjoo.py
@@ -1,1 +1,16 @@
 # git commit -m "code: Solve boj 11053 가장 긴 증가하는 부분 수열 (seungjoo)"
+import sys
+from bisect import bisect_left
+input = sys.stdin.readline
+
+n = int(input())
+a = list(map(int,input().split()))
+
+q=[]
+for x in a:
+    if not q or x > q[-1]:
+        q.append(x)
+    else:
+        q[bisect_left(q,x)] = x
+
+print(len(q))

cd9be8473c66e5f2605926c8c3335ceba35765a5
devpla <chaeyeonhee@kakao.com>
2021-11-21 11:16:29 +0900

    code: Solve boj 11053 가장 긴 증가하는 부분 수열 (yeonhee)

--- BOJ/dp/11053-가장_긴_증가하는_부분_수열/11053-가장_긴_증가하는_부분_수열-yeonhee.py
+++ BOJ/dp/11053-가장_긴_증가하는_부분_수열/11053-가장_긴_증가하는_부분_수열-yeonhee.py
@@ -1,1 +1,14 @@
-# git commit -m "code: Solve boj 11053 가장 긴 증가하는 부분 수열 (yeonhee)"
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+A = list(map(int, input().split()))
+
+dp = [1] * N
+
+for i in range(N):
+    for j in range(i):
+        if A[i] > A[j]:
+            dp[i] = max(dp[i], dp[j]+1)
+
+print(max(dp))

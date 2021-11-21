50722f8aefca8341171d636a7b2ceaeb7923f16e
YoonBaek <royal729@naver.com>
2021-11-21 11:44:04 +0900

    code: Solve boj 11053 가장 긴 증가하는 부분 수열 (yoonbaek)

--- BOJ/dp/11053-가장_긴_증가하는_부분_수열/11053-가장_긴_증가하는_부분_수열-yoonbaek.py
+++ BOJ/dp/11053-가장_긴_증가하는_부분_수열/11053-가장_긴_증가하는_부분_수열-yoonbaek.py
@@ -1,1 +1,14 @@
 # git commit -m "code: Solve boj 11053 가장 긴 증가하는 부분 수열 (yoonbaek)"
+
+if __name__ == "__main__":
+    N = int(input())
+    As = list(map(int, input().split()))
+    dp = [1] * N
+
+    for i in range(0, N):
+        for j in range(i):
+            if As[i] > As[j]:
+                if dp[i] < dp[j]+1:
+                    dp[i] = dp[j]+1
+
+    print(max(dp))

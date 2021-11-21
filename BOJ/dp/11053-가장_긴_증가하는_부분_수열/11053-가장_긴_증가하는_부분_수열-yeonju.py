90a67eca34769a255af27bcb295040a368a7f978
위연주 <thizzyzz@naver.com>
2021-11-21 11:51:39 +0900

    code: Solve boj 11053 가장 긴 증가하는 부분 수열 (yeonju)

--- BOJ/dp/11053-가장_긴_증가하는_부분_수열/11053-가장_긴_증가하는_부분_수열-yeonju.py
+++ BOJ/dp/11053-가장_긴_증가하는_부분_수열/11053-가장_긴_증가하는_부분_수열-yeonju.py
@@ -1,1 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+nums = list(map(int, input().split()))
+dp = [1] * n
+
+for i in range(n):
+    for j in range(i):
+        if nums[i] > nums[j]:
+            dp[i] = max(dp[i], dp[j]+1)
+
+print(max(dp))
+
+
 # git commit -m "code: Solve boj 11053 가장 긴 증가하는 부분 수열 (yeonju)"

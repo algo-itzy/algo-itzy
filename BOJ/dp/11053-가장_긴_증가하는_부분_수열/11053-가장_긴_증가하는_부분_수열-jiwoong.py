e31e45756d68c5288f94219251c67e49e98716d0
서울_4반_정지웅 <j2woong1@gmail.com>
2021-11-21 11:25:28 +0900

    code: Solve boj 11053 가장 긴 증가하는 부분 수열 (jiwoong)

--- BOJ/dp/11053-가장_긴_증가하는_부분_수열/11053-가장_긴_증가하는_부분_수열-jiwoong.py
+++ BOJ/dp/11053-가장_긴_증가하는_부분_수열/11053-가장_긴_증가하는_부분_수열-jiwoong.py
@@ -1,1 +1,17 @@
 # git commit -m "code: Solve boj 11053 가장 긴 증가하는 부분 수열 (jiwoong)"
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+
+arr = list(map(int, input().split()))
+
+seq = [1 for _ in range(N)]
+
+for i in range(1, N):
+    for j in range(i):
+        if arr[i] > arr[j]:
+            seq[i] = max(seq[i], seq[j] + 1)
+
+print(max(seq))

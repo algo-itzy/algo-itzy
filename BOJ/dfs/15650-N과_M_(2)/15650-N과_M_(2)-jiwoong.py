023d35dcda668aa8061d10e4972ae86a05ea115c
서울_4반_정지웅 <j2woong1@gmail.com>
2021-11-28 11:42:28 +0900

    code: Solve boj 15650 N과 M (2) (jiwoong)

--- BOJ/dfs/15650-N과_M_(2)/15650-N과_M_(2)-jiwoong.py
+++ BOJ/dfs/15650-N과_M_(2)/15650-N과_M_(2)-jiwoong.py
@@ -1,1 +1,11 @@
 # git commit -m "code: Solve boj 15650 N과 M (2) (jiwoong)"
+import sys
+from itertools import combinations
+
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+num = [i for i in range(1, N + 1)]
+comb = combinations(num, M)
+for i in comb:
+    print(' '.join(map(str, i)))

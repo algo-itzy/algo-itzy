4fb58dbb313cf510188394e70c37840cb5974e75
서울_4반_정지웅 <j2woong1@gmail.com>
2021-12-02 14:48:05 +0900

    code: Solve boj 01167 트리의 지름 (jiwoong)

--- BOJ/dfs/01167-트리의_지름/01167-트리의_지름-jiwoong.py
+++ BOJ/dfs/01167-트리의_지름/01167-트리의_지름-jiwoong.py
@@ -1,1 +1,33 @@
 # git commit -m "code: Solve boj 01167 트리의 지름 (jiwoong)"
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+
+def bfs(v):
+    dp = [-1 for _ in range(V + 1)]
+    dp[v] = 0
+    q = deque()
+    q.append(v)
+    while q:
+        tmp = q.popleft()
+        for nc, nv in arr[tmp]:
+            if dp[nv] == -1:  
+                dp[nv] = dp[tmp] + nc
+                q.append(nv)
+    return dp
+
+
+V = int(input())
+arr = [[] for _ in range(V + 1)]
+for _ in range(V):
+    nums = list(map(int, input().split()))
+    tmp = nums[0]
+    for i in range(1, len(nums), 2):
+        if nums[i] == -1:
+            break
+        arr[tmp].append((nums[i + 1], nums[i]))  
+dist = bfs(1)  
+v = dist.index(max(dist))  
+print(max(bfs(v)))  

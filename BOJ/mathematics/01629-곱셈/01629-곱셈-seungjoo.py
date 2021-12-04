b5fc504c7db685c024af43cdb95a5993010d23ad
chaselover <gkstmdwn234@gmail.com>
2021-12-05 03:11:49 +0900

    code: Solve boj 1629 1753 (seungjoo)

--- BOJ/mathematics/01629-곱셈/01629-곱셈-seungjoo.py
+++ BOJ/mathematics/01629-곱셈/01629-곱셈-seungjoo.py
@@ -1,1 +1,14 @@
 # git commit -m "code: Solve boj 01629 곱셈 (seungjoo)"
+import sys
+input = sys.stdin.readline
+
+def pow(n):
+    if n == 1:
+        return a % c
+    left = pow(n // 2)
+    if not n & 1:
+        return left * left % c
+    return left * left * a % c
+
+a, b, c = map(int, input().split())
+print(pow(b))

ed14b50867b6b99a432475ab247ea3f321026653
ed-kyu <seungkyukim.dev@gmail.com>
2021-12-05 11:25:29 +0900

    code: Solve boj 01629 곱셈 (seungkyu)

--- BOJ/mathematics/01629-곱셈/01629-곱셈-seungkyu.py
+++ BOJ/mathematics/01629-곱셈/01629-곱셈-seungkyu.py
@@ -1,1 +1,15 @@
 # git commit -m "code: Solve boj 01629 곱셈 (seungkyu)"
+def powpow(b):
+    if b == 1:
+        return a%c
+
+    tmp = powpow(b//2)
+    tmp = tmp*tmp%c
+
+    if b % 2 == 0:
+        return tmp
+    else:
+        return a*tmp%c
+
+a, b, c = map(int, input().split())
+print(powpow(b))

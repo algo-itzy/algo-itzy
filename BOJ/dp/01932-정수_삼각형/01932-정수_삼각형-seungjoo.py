8778eacecaa906bb75991557021d12ab64f0078a
chaselover <gkstmdwn234@gmail.com>
2021-12-09 03:32:43 +0900

    code: Solve boj 1865 1918 1932 (seungjoo)

--- BOJ/dp/01932-정수_삼각형/01932-정수_삼각형-seungjoo.py
+++ BOJ/dp/01932-정수_삼각형/01932-정수_삼각형-seungjoo.py
@@ -1,1 +1,17 @@
 # git commit -m "code: Solve boj 01932 정수 삼각형 (seungjoo)"
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+triangle = [list(map(int, input().split())) for _ in range(N)]
+
+for row in range(1, N):
+    for col in range(row + 1):
+        if col == 0:
+            triangle[row][col] = triangle[row - 1][col] + triangle[row][col]
+        elif col == row:
+            triangle[row][col] = triangle[row - 1][row - 1] + triangle[row][col]
+        else:
+            triangle[row][col] = max(triangle[row - 1][col], triangle[row - 1][col - 1]) + triangle[row][col]
+
+print(max(triangle[N - 1]))

67145484c7cedf67a91f7dc046d6a0874d77953e
ed-kyu <seungkyukim.dev@gmail.com>
2021-12-09 18:36:05 +0900

    code: Solve boj 01932 정수 삼각형 (seungkyu)

--- BOJ/dp/01932-정수_삼각형/01932-정수_삼각형-seungkyu.py
+++ BOJ/dp/01932-정수_삼각형/01932-정수_삼각형-seungkyu.py
@@ -1,1 +1,14 @@
 # git commit -m "code: Solve boj 01932 정수 삼각형 (seungkyu)"
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+numbers = []
+for _ in range(n):
+    numbers.append([0] + list(map(int, input().split())) + [0])
+
+for i in range(1, n):
+    for j in range(1, i+2):
+        numbers[i][j] += max(numbers[i-1][j-1], numbers[i-1][j])
+
+print(max(numbers[n-1]))

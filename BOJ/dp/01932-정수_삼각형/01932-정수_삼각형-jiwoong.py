699419cc08a14234804547d918a53dba8ac1b1b2
서울_4반_정지웅 <j2woong1@gmail.com>
2021-12-09 11:18:34 +0900

    code: Solve boj 01932 정수 삼각형 (jiwoong)

--- BOJ/dp/01932-정수_삼각형/01932-정수_삼각형-jiwoong.py
+++ BOJ/dp/01932-정수_삼각형/01932-정수_삼각형-jiwoong.py
@@ -1,1 +1,18 @@
 # git commit -m "code: Solve boj 01932 정수 삼각형 (jiwoong)"
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+tri = [list(map(int, input().split())) for _ in range(n)]
+
+for i in range(1, n):
+    num = len(tri[i])
+    for j in range(num):
+        if j == 0:
+            tri[i][j] += tri[i - 1][j]
+        elif j == num - 1:
+            tri[i][j] += tri[i - 1][j - 1]
+        else:
+            tri[i][j] += max(tri[i - 1][j - 1], tri[i - 1][j])
+print(max(tri[n - 1]))

1910a5fb696165c8dd45d896c9efa20b1b97488a
devpla <chaeyeonhee@kakao.com>
2021-12-09 17:06:13 +0900

    code: Solve boj 01918 후위 표기식 (yeonhee)

--- BOJ/stack/01918-후위_표기식/01918-후위_표기식-yeonhee.py
+++ BOJ/stack/01918-후위_표기식/01918-후위_표기식-yeonhee.py
@@ -1,1 +1,27 @@
-# git commit -m "code: Solve boj 01918 후위 표기식 (yeonhee)"
+a = input()
+stack = []
+result = ''
+
+for x in a:
+    if x.isalpha():
+        result += x
+    else:
+        if x == '(':
+            stack.append(x)
+        elif x == ')':
+            while stack and stack[-1] != '(':
+                result += stack.pop()
+            stack.pop()
+        elif x in ('*', '/'):
+            while stack and (stack[-1] in ('*', '/')):
+                result += stack.pop()
+            stack.append(x)
+        elif x in ('+', '-'):
+            while stack and stack[-1] != '(':
+                result += stack.pop()
+            stack.append(x)
+
+while stack:
+    result += stack.pop()
+
+print(result)

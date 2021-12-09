930b4abe1dff196b05ce2d82d794a6902a5b08c9
서울_4반_정지웅 <j2woong1@gmail.com>
2021-12-09 11:17:51 +0900

    code: Solve boj 01918 후위 표기식 (jiwoong)

--- BOJ/stack/01918-후위_표기식/01918-후위_표기식-jiwoong.py
+++ BOJ/stack/01918-후위_표기식/01918-후위_표기식-jiwoong.py
@@ -1,1 +1,30 @@
 # git commit -m "code: Solve boj 01918 후위 표기식 (jiwoong)"
+import sys
+
+input = sys.stdin.readline
+
+infix = input().rstrip('\n')
+postfix = ''
+stack = []
+
+for i in infix:
+    if i.isalpha():
+        postfix += i
+    elif i == '(':
+        stack.append(i)
+    elif i == ')':
+        while stack and stack[-1] != '(':
+            postfix += stack.pop()
+        stack.pop()
+    elif i == '+' or i == '-':
+        while stack and stack[-1] != '(':
+            postfix += stack.pop()
+        stack.append(i)
+    elif i == '*' or i == '/':
+        while stack and (stack[-1] == '*' or stack[-1] == '/'):
+            postfix += stack.pop()
+        stack.append(i)
+while stack:
+    postfix += stack.pop()
+
+print(postfix)

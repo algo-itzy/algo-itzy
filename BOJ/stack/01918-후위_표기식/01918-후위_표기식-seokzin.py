d2287431b974780dbad2dfcaa0a7dbffa5f2b5af
seokzin <seokzin95@gmail.com>
2021-12-09 17:57:43 +0900

    code: Solve boj 01918 후위 표기식 (seokzin)

--- BOJ/stack/01918-후위_표기식/01918-후위_표기식-seokzin.py
+++ BOJ/stack/01918-후위_표기식/01918-후위_표기식-seokzin.py
@@ -1,1 +1,27 @@
-# git commit -m "code: Solve boj 01918 후위 표기식 (seokzin)"
+s = input()
+stack = []
+res = ""
+
+for c in s:
+    if c.isalpha():
+        res += c
+    else:
+        if c == '(':
+            stack.append(c)
+        elif c == '*' or c =='/':
+            while stack and stack[-1] in '*/':
+                res += stack.pop()
+            stack.append(c)
+        elif c == '+' or c == '-':
+            while stack and stack[-1] != '(':
+                res += stack.pop()
+            stack.append(c)
+        elif c == ')':
+            while stack and stack[-1] != '(':
+                res += stack.pop()
+            stack.pop() # '(' 제거
+
+while stack:
+    res += stack.pop()
+
+print(res)

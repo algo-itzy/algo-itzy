c1e5ca445447dd313d0aef2759b522fc13b8b0ea
위연주 <thizzyzz@naver.com>
2021-12-09 19:40:50 +0900

    code: Solve boj 01918 후위 표기식 (yeonju)

--- BOJ/stack/01918-후위_표기식/01918-후위_표기식-yeonju.py
+++ BOJ/stack/01918-후위_표기식/01918-후위_표기식-yeonju.py
@@ -1,1 +1,34 @@
+expression = input()
+
+stack = []
+res = ''
+
+for i in expression:
+    if i.isalpha():
+        res += i
+
+    else:
+        if i == '(':
+            stack.append(i)
+        elif i == '*' or i == '/':
+            while stack and (stack[-1] == '*' or stack[-1] == '/'):
+                res += stack.pop()
+            stack.append(i)
+
+        elif i == '+' or i == '-':
+            while stack and stack[-1] != '(':
+                res += stack.pop()
+            stack.append(i)
+
+        elif i == ')':
+            while stack and stack[-1] != '(':
+                res += stack.pop()
+            stack.pop()
+
+while stack:
+    res += stack.pop()
+
+print(res)
+
+
 # git commit -m "code: Solve boj 01918 후위 표기식 (yeonju)"

e1b5896e4aa1db4f6b83d33cf3bdfeb6fe9d318b
Ji Woong <j2woong1@gmail.com>
2021-12-12 12:29:48 +0900

    21.12.12 (jiwoong)  (#183)
    * code: Solve boj 15650 N과 M (2) (jiwoong)
    
    * code: Solve boj 01149 RGB거리 (jiwoong)
    
    * code: Solve boj 01167 트리의 지름 (jiwoong)
    
    * code: Solve boj 01865 웜홀 (jiwoong)
    
    * code: Solve boj 01918 후위 표기식 (jiwoong)
    
    * code: Solve boj 01932 정수 삼각형 (jiwoong)
    
    * code: Solve boj 01238 파티 (jiwoong)
    
    * code: Solve boj 02206 벽 부수고 이동하기 (jiwoong)

--- BOJ/bfs/02206-벽_부수고_이동하기/02206-벽_부수고_이동하기-jiwoong.py
+++ BOJ/bfs/02206-벽_부수고_이동하기/02206-벽_부수고_이동하기-jiwoong.py
@@ -1,1 +1,39 @@
 # git commit -m "code: Solve boj 02206 벽 부수고 이동하기 (jiwoong)"
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+dir = [(0, 1), (0, -1), (-1, 0), (1, 0)]
+
+
+def bfs(x, y, broken):
+    q = deque()
+    q.append((x, y, broken))
+    visited[x][y][broken] = 1
+
+    while q:
+        cx, cy, br = q.popleft()
+        if cx == N - 1 and cy == M - 1:
+            return visited[N - 1][M - 1][br]
+        for dx, dy in dir:
+            nx, ny = cx + dx, cy + dy
+            if 0 <= nx < N and 0 <= ny < M:
+                # 0이라면 벽을 부실 필요 없음
+                if arr[nx][ny] == 0 and not visited[nx][ny][br]:
+                    q.append((nx, ny, br))
+                    visited[nx][ny][br] = visited[cx][cy][br] + 1
+                # 1이고 벽을 부시지 않은 상태라면 벽을 부시고 지나감
+                if arr[nx][ny] == 1 and not br:
+                    q.append((nx, ny, 1))
+                    visited[nx][ny][1] = visited[cx][cy][0] + 1
+    return -1
+
+
+N, M = map(int, input().split())
+arr = []
+for _ in range(N):
+    arr.append(list(map(int, list(input().rstrip()))))
+# 벽을 부신 상태인지 아닌지를 나타내도록 함
+visited = [[[0] * 2 for _ in range(M)] for _ in range(N)]
+print(bfs(0, 0, 0))

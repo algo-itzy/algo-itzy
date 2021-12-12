a968f17d327ba53ef39491c26e859b5352190f0b
서울_4반_정지웅 <j2woong1@gmail.com>
2021-12-12 11:24:27 +0900

    code: Solve boj 01238 파티 (jiwoong)

--- BOJ/dijkstra/01238-파티/01238-파티-jiwoong.py
+++ BOJ/dijkstra/01238-파티/01238-파티-jiwoong.py
@@ -1,1 +1,47 @@
 # git commit -m "code: Solve boj 01238 파티 (jiwoong)"
+import heapq
+import sys
+
+INF = int(1e9)
+input = sys.stdin.readline
+
+N, M, X = map(int, input().split())
+
+arr = [[] for _ in range(N + 1)]
+
+dist = [[INF for _ in range(N + 1)] for _ in range(N + 1)]
+
+for i in range(M):
+    s, e, t = map(int, input().split())
+    arr[s].append((e, t))
+
+
+def dijkstra(s):
+    q = []
+    heapq.heappush(q, (0, s))
+    dist[s][s] = 0
+
+    while q:
+        tmp = heapq.heappop(q)
+        t, n = tmp[0], tmp[1]
+        if dist[s][n] < t:
+            continue
+
+        for i in arr[n]:
+            e, t = i[0], i[1]
+            data = dist[s][n] + t
+            if dist[s][e] > data:
+                dist[s][e] = data
+                heapq.heappush(q, (t, e))
+
+
+for i in range(1, N + 1):
+    dijkstra(i)
+
+ans = -1
+for i in range(1, N + 1):
+    tmp = dist[i][X] + dist[X][i]
+    if ans < tmp:
+        ans = tmp
+
+print(ans)

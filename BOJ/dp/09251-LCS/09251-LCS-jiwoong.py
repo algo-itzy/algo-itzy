0e818b0d726d083d57759964bc58db2494b8e409
서울_4반_정지웅 <j2woong1@gmail.com>
2021-12-16 19:49:28 +0900

    code: Solve boj 09251 LCS (jiwoong)

--- BOJ/dp/09251-LCS/09251-LCS-jiwoong.py
+++ BOJ/dp/09251-LCS/09251-LCS-jiwoong.py
@@ -1,1 +1,16 @@
 # git commit -m "code: Solve boj 09251 LCS (jiwoong)"
+def lcs():
+    str1, str2 = input(), input()
+    arr = [[0 for _ in range(len(str2) + 1)] for _ in range(len(str1) + 1)]
+
+    for i in range(1, len(str1) + 1):
+        for j in range(1, len(str2) + 1):
+            if str1[i - 1] == str2[j - 1]:  # 1번 문자열 = 2번 문자열 같으면
+                arr[i][j] = arr[i - 1][j - 1] + 1  # 이전 lcs 길이 + 1
+            else:  # 다르면
+                arr[i][j] = max(arr[i - 1][j], arr[i][j - 1])  # 따로 한 글자씩 추가되었을 때 기준 큰 값
+    print(arr[-1][-1])
+
+
+if __name__ == '__main__':
+    lcs()

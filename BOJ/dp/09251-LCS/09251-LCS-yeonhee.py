8249c996df4f1b24b8bf5a0ed9182567bf6e57a5
devpla <chaeyeonhee@kakao.com>
2021-12-16 17:01:18 +0900

    code: Solve boj 09251 LCS (yeonhee)

--- BOJ/dp/09251-LCS/09251-LCS-yeonhee.py
+++ BOJ/dp/09251-LCS/09251-LCS-yeonhee.py
@@ -1,1 +1,16 @@
-# git commit -m "code: Solve boj 09251 LCS (yeonhee)"
+import sys
+input = sys.stdin.readline
+
+a = input().strip()
+b = input().strip()
+
+dp = [[0] * (len(b)+1) for _ in range(len(a)+1)]
+
+for i in range(1, len(a)+1):
+    for j in range(1, len(b)+1):
+        if a[i-1] == b[j-1]:
+            dp[i][j] = dp[i-1][j-1] + 1
+        else:
+            dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+
+print(dp[-1][-1])

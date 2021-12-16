aae7257ad1d29728f9987c0d846681f8b8a1cbf3
위연주 <thizzyzz@naver.com>
2021-12-16 18:29:52 +0900

    code: Solve boj 09251 LCS (yeonju)

--- BOJ/dp/09251-LCS/09251-LCS-yeonju.py
+++ BOJ/dp/09251-LCS/09251-LCS-yeonju.py
@@ -1,1 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+a, b = input().strip(), input().strip()
+n, m = len(a), len(b)
+
+dp = [[0] * (m+1) for _ in range(n+1)]
+
+for i in range(n):
+    for j in range(m):
+        if a[i] == b[j]:
+            dp[i+1][j+1] = dp[i][j]+1
+        else:
+            dp[i+1][j+1] = max(dp[i+1][j], dp[i][j+1])
+
+print(dp[-1][-1])
+
+
 # git commit -m "code: Solve boj 09251 LCS (yeonju)"

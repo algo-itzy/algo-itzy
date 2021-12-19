c402688fb40487782fc619e04cafbc7a885c5289
devpla <chaeyeonhee@kakao.com>
2021-12-19 11:45:34 +0900

    code: Solve boj 11444 피보나치 수 6 (yeonhee)

--- BOJ/mathematics/11444-피보나치_수_6/11444-피보나치_수_6-yeonhee.py
+++ BOJ/mathematics/11444-피보나치_수_6/11444-피보나치_수_6-yeonhee.py
@@ -1,1 +1,35 @@
-# git commit -m "code: Solve boj 11444 피보나치 수 6 (yeonhee)"
+import sys
+input = sys.stdin.readline
+
+c = 1000000007
+
+
+def multiply(mtx1, mtx2):
+    result = []
+
+    for i in range(len(mtx1)):
+        result.append([])
+        for j in range(len(mtx2[0])):
+            tmp = 0
+            for k in range(len(mtx1[0])):
+                tmp += mtx1[i][k] * mtx2[k][j]
+            result[i].append(tmp % c)
+
+    return result
+
+
+def power(mtx, p):
+    if p == 1:
+        return mtx
+
+    tmp = power(mtx, p//2)
+
+    if p % 2:
+        return multiply(multiply(tmp, tmp), mtx)
+    else:
+        return multiply(tmp, tmp)
+
+
+n = int(input())
+m = [[1, 1], [1, 0]]
+print(power(m, n)[0][1])

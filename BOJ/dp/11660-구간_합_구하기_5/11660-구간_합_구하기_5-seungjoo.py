316073dc8e585a601f3b36094659eb19f6e76e4b
chaselover <gkstmdwn234@gmail.com>
2021-12-23 01:42:28 +0900

    code: Solve boj 11660 12865 (seungjoo)

--- BOJ/dp/11660-구간_합_구하기_5/11660-구간_합_구하기_5-seungjoo.py
+++ BOJ/dp/11660-구간_합_구하기_5/11660-구간_합_구하기_5-seungjoo.py
@@ -1,1 +1,17 @@
 # git commit -m "code: Solve boj 11660 구간 합 구하기 5 (seungjoo)"
+import sys
+input = sys.stdin.readline
+
+N,M = map(int, input().split())
+
+dp = [[0] + list(map(int, input().split())) for _ in range(N)]
+dp = [[0] * (N + 1)] + dp
+for i in range(1, N + 1):
+    for j in range(1, N + 1):
+            dp[i][j] += dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1]
+
+
+for _ in range(M):
+    x1, y1, x2, y2 = map(int,input().split())
+    answer = dp[x2][y2] - dp[x2][y1 - 1] - dp[x1 - 1][y2] + dp[x1 - 1][y1 - 1]
+    print(answer)

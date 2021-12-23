34c95f399aa4845d221a24c270e691d0dad9ef02
서울_4반_정지웅 <j2woong1@gmail.com>
2021-12-23 15:40:39 +0900

    code: Solve boj 11660 구간 합 구하기 5 (jiwoong)

--- BOJ/dp/11660-구간_합_구하기_5/11660-구간_합_구하기_5-jiwoong.py
+++ BOJ/dp/11660-구간_합_구하기_5/11660-구간_합_구하기_5-jiwoong.py
@@ -1,1 +1,16 @@
 # git commit -m "code: Solve boj 11660 구간 합 구하기 5 (jiwoong)"
+import sys
+
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+num = [list(map(int, input().split())) for _ in range(N)]
+arr = [[0 for _ in range(N+1)] for _ in range(N+1)]
+
+for i in range(N):
+    for j in range(N):
+        arr[i+1][j+1] = arr[i][j+1] + arr[i+1][j] - arr[i][j] + num[i][j]
+
+for _ in range(M):
+    x1, y1, x2, y2 = map(int, input().split())
+    print(arr[x2][y2] - arr[x2][y1 - 1] - arr[x1 - 1][y2] + arr[x1 - 1][y1 - 1])

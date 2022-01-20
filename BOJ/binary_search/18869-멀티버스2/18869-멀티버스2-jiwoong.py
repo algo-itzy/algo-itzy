82211d868ef9cf9b72a374e5339a698d4a2ae2a0
jiwoong <j2woong1@gmail.com>
2022-01-20 16:24:55 +0900

    code: Solve boj 18869 멀티버스2 (jiwoong)

--- BOJ/binary_search/18869-멀티버스2/18869-멀티버스2-jiwoong.py
+++ BOJ/binary_search/18869-멀티버스2/18869-멀티버스2-jiwoong.py
@@ -1,1 +1,29 @@
 # git commit -m "code: Solve boj 18869 멀티버스2 (jiwoong)"
+import sys
+
+input = sys.stdin.readline
+
+M, N = map(int, input().split())
+
+planet = [list(map(int, input().split())) for _ in range(M)]
+
+arr = [[] for _ in range(M)]
+dict = {}
+for i in range(M):
+    multi = sorted(list(set(planet[i])))
+    for j in range(len(multi)):
+        dict[multi[j]] = j
+    for x in (planet[i]):
+        arr[i].append(dict[x])
+
+arr.sort()
+cnt, ans = 1, 0
+for i in range(1, M):
+    if arr[i] == arr[i - 1]:
+        cnt += 1
+    else:
+        ans += cnt * (cnt - 1) // 2
+        cnt = 1
+
+ans += cnt * (cnt - 1) // 2
+print(ans)
